f0e5c6e16f8fb70af356cccd85925f067de86e04
Akshay Soni <akshaysoni460@gmail.com>
2022-06-14 09:57:43 +0530

    Time: 7 ms (97.54%), Space: 12.1 MB (8.07%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -4,15 +4,18 @@ public:
         
         int n = arr.size();
         vector<int> result;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(arr[i]+arr[j]==targetSum){
-                    result.push_back(i);
-                    result.push_back(j);
-                    break;
-                }
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++){
+            int req_num = targetSum-arr[i];
+            if(m.count(req_num)){
+                result.push_back(i);
+                result.push_back(m[req_num]);
+            }
+            else{
+                m[arr[i]] = i;
             }
         }
+
         return result;
         
     }
