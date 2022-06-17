0b6370b964ea1eb9de150427357ed823abe04806
Akshay Soni <akshaysoni460@gmail.com>
2022-06-17 11:17:31 +0530

    Time: 129 ms (77.04%), Space: 67.7 MB (53.72%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,22 +1,61 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& arr) {
-        int n = arr.size();
-	vector<int> arr_sum(n,0);
-	int sumTillNow = 0;
-	for(int i=0;i<n;i++){
-		sumTillNow += arr[i];
-		if(sumTillNow < 0){
-			arr_sum[i]=arr[i];
-			sumTillNow=0;
-		}
-		else
-		{
-			arr_sum[i]=sumTillNow;
-		}
-
-	}
-	int x = *max_element(arr_sum.begin(),arr_sum.end()); 
-	return x;
+        int n=arr.size();
+        int cs=0,ms=INT_MIN;
+        for(int i=0;i<n;i++){
+            cs += arr[i];
+            ms = max(cs,ms);
+            if(cs<0){ cs = 0; }
+        }
+        return ms;
     }
 };
+
+
+
+// My All other different approches
+// int maxSubArraySum(vector<int> arr){
+// 	int n = arr.size();
+// 	vector<int> arr_sum(n,0);
+// 	int sumTillNow = 0;
+// 	for(int i=0;i<n;i++){
+// 		sumTillNow += arr[i];
+// 		if(sumTillNow < 0){
+// 			arr_sum[i]=0;
+// 			sumTillNow=0;
+// 		}
+// 		else
+// 		{
+// 			arr_sum[i]=sumTillNow;
+// 		}
+
+// 	}
+// 	int x = *max_element(arr_sum.begin(),arr_sum.end()); 
+// 	return x;
+
+// }
+
+
+// int maxSubArraySum_2(vector<int> arr){
+
+// 	int n=arr.size();
+// 	for(int i=1;i<n;i++){
+// 		if (arr[i-1] > 0){
+// 			arr[i]+=arr[i-1];
+// 		}
+// 	}
+// 	return *max_element(arr.begin(),arr.end());
+// }
+
+// int maxSubArraySum_3(vector<int> arr){
+
+// 	int n=arr.size();
+// int cs=0,ms=INT_MIN;
+// for(int i=1;i<n;i++){
+//     cs += arr[i];
+//     ms = max(cs,ms);
+//     if(cs<0){ cs = 0; }
+// }
+// return ms;
+// }
