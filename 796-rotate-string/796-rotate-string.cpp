16febcea2a3cb7d44437d140c998fcb899ef9e41
Akshay Soni <akshaysoni460@gmail.com>
2022-06-23 12:06:38 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (64.33%) - LeetHub

--- 796-rotate-string/796-rotate-string.cpp
+++ 796-rotate-string/796-rotate-string.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int n = s.length();
-        int k=n;
-        while(k>0){
-            char ch = s[0];
-            for(int i=0;i<n-1;i++){
-                s[i] = s[i+1];
-            }
-            s[n-1]=ch;
-            cout<<s<<endl;
-            if(s==goal){ return true; }
-            k--;
-        }
+    
+    if(s.length()!=goal.length()) { return false; }
+    
+    int n=s.length();
+    for(int i=0;i<n;i++){
+        char ch = s[0];
+        s.erase(0,1);
+        s.push_back(ch);
+        if(s==goal){ return true; }
+    }
         return false;
-        
     }
 };
+
+
+// BurteForce O(n2)
+// int n = s.length();
+//         int k=n;
+//         while(k>0){
+//             char ch = s[0];
+//             for(int i=0;i<n-1;i++){
+//                 s[i] = s[i+1];
+//             }
+//             s[n-1]=ch;
+//             cout<<s<<endl;
+//             if(s==goal){ return true; }
+//             k--;
+//         }
+//         return false;
+        
