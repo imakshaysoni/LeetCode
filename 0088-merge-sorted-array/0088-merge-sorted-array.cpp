dbb9390d501318fa7efd0077a81a9385361145d8
Akshay Soni <akshaysoni460@gmail.com>
2023-03-31 09:36:54 +0530

    Time: 4 ms (30.26%), Space: 9.1 MB (76.70%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,36 +2,73 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
      
-
-        vector<int> v;
-        int i=0;
-        int j=0;
-        while(i<m and j<n){
-            if(nums1[i]<=nums2[j]){
-                v.push_back(nums1[i]);
-                i++;
+//         Alternate Solution without extra space
+        if(n==0){
+            return;
+        }
+        if(m==0){
+            nums1=nums2;
+            return;
+        }
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0 and j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+                k--;
             }
             else{
-                v.push_back(nums2[j]);
-                j++;
+                nums1[k]=nums2[j];
+                j--;
+                k--;
             }
         }
-            while(i<m){
-                v.push_back(nums1[i]);
-                i++;
-            }
-            while(j<n){
-                v.push_back(nums2[j]);
-                j++;
-            }
+        for(int z=0;z<nums1.size();z++){
+            cout<<nums1[z];
+        }
+        cout<<endl<<i<<","<<j<<","<<k;
+        while(i>=0){
+            nums1[k]=nums1[i];
+            i--;
+            k--;
+        }
+        while(j>=0){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
+        }
+
+//         vector<int> v;
+//         int i=0;
+//         int j=0;
+//         while(i<m and j<n){
+//             if(nums1[i]<=nums2[j]){
+//                 v.push_back(nums1[i]);
+//                 i++;
+//             }
+//             else{
+//                 v.push_back(nums2[j]);
+//                 j++;
+//             }
+//         }
+//             while(i<m){
+//                 v.push_back(nums1[i]);
+//                 i++;
+//             }
+//             while(j<n){
+//                 v.push_back(nums2[j]);
+//                 j++;
+//             }
         
-        for(int i=0;i<nums1.size();i++){
-            cout<<v[i];
-        }
-        cout<<endl;
-        for(int k=0;k<nums1.size();k++){
-            nums1[k]=v[k];
-        }
+//         for(int i=0;i<nums1.size();i++){
+//             cout<<v[i];
+//         }
+//         cout<<endl;
+//         for(int k=0;k<nums1.size();k++){
+//             nums1[k]=v[k];
+//         }
         
     }
 };
