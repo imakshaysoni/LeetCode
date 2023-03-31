3ac83bdc4feaac7c826961576081124a28ed3990
Akshay Soni <akshaysoni460@gmail.com>
2023-03-31 21:37:16 +0530

    Time: 19 ms (92.76%), Space: 31.2 MB (65.89%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-    
-        vector<int> arr1;
-        vector<int> arr2;
-        int ans=-1;
-        int l_sum=0;
+        
         int r_sum=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            l_sum+=nums[i];
-            r_sum+=nums[n-i-1];
-            arr1.push_back(l_sum);
-            arr2.push_back(r_sum);
+        int l_sum=0;
+        for(int i=0;i<nums.size();i++){
+            r_sum+=nums[i];
         }
-        for(int i=0;i<n;i++){
-            if(arr1[i]==arr2[n-i-1]){
-                ans=i;
-                break;
+        for(int i=0;i<nums.size();i++){
+            r_sum -= nums[i];
+            if(r_sum==l_sum){
+                return i;
             }
+            l_sum+=nums[i];
         }
+        return -1;
+//         vector<int> arr1;
+//         vector<int> arr2;
+//         int ans=-1;
+//         int l_sum=0;
+//         int r_sum=0;
+//         int n=nums.size();
+//         for(int i=0;i<n;i++){
+//             l_sum+=nums[i];
+//             r_sum+=nums[n-i-1];
+//             arr1.push_back(l_sum);
+//             arr2.push_back(r_sum);
+//         }
+//         for(int i=0;i<n;i++){
+//             if(arr1[i]==arr2[n-i-1]){
+//                 ans=i;
+//                 break;
+//             }
+//         }  
             
-        return ans;
+//         return ans;
     }
 };
