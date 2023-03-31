1f739a7f4b7e0778f4ec5c43167df439b68a5b88
Akshay Soni <akshaysoni460@gmail.com>
2023-03-31 21:59:47 +0530

    Time: 112 ms (6.45%), Space: 19.6 MB (38.41%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size()/2;
+        sort(nums.begin(),nums.end());
+        int count=1;
+        for(int i=0;i<nums.size()-1;i++){
+            cout<<nums[i]<<endl;
+            if(nums[i]==nums[i+1]){
+                count++;
+            }
+            cout<<count<<endl;
+            if(count>n){
+                return nums[i];
+            }
+        }
+        return 1;
+    }
+};
