d4cc01be875abc14abbc14b6264f1a47a3373633
Akshay Soni <akshaysoni460@gmail.com>
2023-04-01 19:24:21 +0530

    Time: 29 ms (12.62%), Space: 19.6 MB (38.32%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,22 +3,35 @@ public:
     int majorityElement(vector<int>& nums) {
         
         
-        
-        
-        
-        int n=nums.size()/2;
-        sort(nums.begin(),nums.end());
         int count=1;
-        for(int i=0;i<nums.size()-1;i++){
-            cout<<nums[i]<<endl;
-            if(nums[i]==nums[i+1]){
+        int max = nums[0];
+        for(int i=1; i<nums.size();i++){
+            if(nums[i]==max){
                 count++;
             }
-            cout<<count<<endl;
-            if(count>n){
-                return nums[i];
+            else if(count==0){
+                count++;
+                max=nums[i];
             }
+            else{count--;}
+            
         }
-        return nums[0];
+        return max;
+        
+        
+        // int n=nums.size()/2;
+        // sort(nums.begin(),nums.end());
+        // int count=1;
+        // for(int i=0;i<nums.size()-1;i++){
+        //     cout<<nums[i]<<endl;
+        //     if(nums[i]==nums[i+1]){
+        //         count++;
+        //     }
+        //     cout<<count<<endl;
+        //     if(count>n){
+        //         return nums[i];
+        //     }
+        // }
+        // return nums[0];
     }
 };
