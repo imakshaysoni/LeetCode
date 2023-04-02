10a56e83a2e8924018f48b40738957c3deb1fda1
Akshay Soni <akshaysoni460@gmail.com>
2023-04-02 19:31:36 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (52.82%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>> v;
+        for(int i=0;i<numRows;i++){
+            vector<int> r(i+1,1);
+            for(int j=1;j<i;j++){
+                r[j] = v[i-1][j] + v[i-1][j-1];
+            }
+            v.push_back(r);
+        }
+        return v;
+        
+    }
+};
