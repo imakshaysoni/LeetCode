3b62da45eb80866bcc737988559df0f331f17f90
Akshay Soni <akshaysoni460@gmail.com>
2023-04-04 10:45:43 +0530

    Time: 26 ms (64.81%), Space: 24.1 MB (55.88%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,23 +2,40 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         
-        int mul=1;
-        vector<int> r;
-        r.push_back(1);
-        int l_sum=1;
-        int n = nums.size()-1;
-        for(int i=n;i>0;i--){
-            mul*=nums[i];
-            r.push_back(mul);
+        
+        int n= nums.size();
+        vector<int> ans(n,1);
+        int p=1;
+        for(int i=0;i<n;i++){
+            ans[i]=p;
+            p*=nums[i];
         }
-        // for(auto a: r)cout<<a<<" ";
-        for(int i=0;i<=n;i++){
-            int temp=nums[i];
-            nums[i]=r[n-i]*l_sum;
-            l_sum*=temp;                        
+        p=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i]*=p;
+            p*=nums[i];
         }
         
-        return nums;      
+        return ans;
+        
+        
+        // int mul=1;
+//         vector<int> r;
+//         r.push_back(1);
+//         int l_sum=1;
+//         int n = nums.size()-1;
+//         for(int i=n;i>0;i--){
+//             mul*=nums[i];
+//             r.push_back(mul);
+//         }
+//         // for(auto a: r)cout<<a<<" ";
+//         for(int i=0;i<=n;i++){
+//             int temp=nums[i];
+//             nums[i]=r[n-i]*l_sum;
+//             l_sum*=temp;                        
+//         }
+        
+//         return nums;      
         
     }
 };
