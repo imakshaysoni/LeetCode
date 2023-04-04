4e874bd69fc600b22da02ab139ccd14637c57934
Akshay Soni <akshaysoni460@gmail.com>
2023-04-04 10:28:55 +0530

    Time: 135 ms (5.45%), Space: 25.6 MB (11.93%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -7,10 +7,11 @@ public:
         r.push_back(1);
         int l_sum=1;
         int n = nums.size()-1;
-        for(int i=n;i>=0;i--){
+        for(int i=n;i>0;i--){
             mul*=nums[i];
             r.push_back(mul);
         }
+        for(auto a: r)cout<<a<<" ";
         for(int i=0;i<=n;i++){
             int temp=nums[i];
             nums[i]=r[n-i]*l_sum;
