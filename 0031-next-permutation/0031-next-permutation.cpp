28f019c71f681fcf951914988ae2bcd536f46bd7
Akshay Soni <akshaysoni460@gmail.com>
2023-04-05 10:11:25 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (41.71%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,28 +1,59 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
+    void nextPermutation(vector<int>& num) {
+   
+        int n=num.size();
+    if(n<2)
+        return;
+    int index=n-1;        
+    while(index>0){
+        if(num[index-1]<num[index])
+            break;
+        index--;
+    }
+    if(index==0){
+        reverse(num.begin(),num.end());
+        return;
+    }
+    else{
+        int val=num[index-1];
+        int j=n-1;
+        while(j>=index){
+            if(num[j]>val)
+                break;
+            j--;
+        }
+        swap(num[j],num[index-1]);
+        reverse(num.begin()+index,num.end());
+        return;
         
-     int n=nums.size();
-    int i=n-1;
-    while(i>0){
-        if(nums[i-1] < nums[i])break;
-        i--;
     }
-    int k=n-1;
-    --i;
-    while(k>i and i>=0){
-     if(nums[k] > nums[i]){
-         swap(nums[k],nums[i]);
-         break;
-     }
-        k--;
-    }
-    int j=i+1;
-    k=n-1;
-    while(j<k){
-        swap(nums[j],nums[k]);
-        j++;k--;
-    }
+        
+        
+        
+//         StriverSolution & Intution
+//      int n=nums.size();
+//     int i=n-1;
+//     while(i>0){
+//         if(nums[i-1] < nums[i])break;
+//         i--;
+//     }
+//     int k=n-1;
+//     --i;
+//     while(k>i and i>=0){
+//      if(nums[k] > nums[i]){
+//          swap(nums[k],nums[i]);
+//          break;
+//      }
+//         k--;
+//     }
+    
+//     int j=i+1;
+//     k=n-1;
+//     while(j<k){
+//         swap(nums[j],nums[k]);
+//         j++;k--;
+//     }
         
         
         
@@ -48,9 +79,6 @@ public:
         
     }
 };
-
-
-
 // 1 6 3 4 2
     
 
