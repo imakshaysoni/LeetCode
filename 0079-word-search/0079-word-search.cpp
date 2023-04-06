17672032c574345a0f9a0a29a8e84443f31a90f2
Akshay Soni <akshaysoni460@gmail.com>
2023-04-06 10:30:26 +0530

    Time: 1053 ms (30.46%), Space: 8 MB (85.92%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
-    bool search(int r, int c, size_t i, const string &word, vector <vector <char>> &board) {
-        if(i == word.size()) {return true;}
-        else if(r < 0 || r >= (int) board.size() || c < 0 || c >= (int) board[0].size()) {return false;}
-        else if(board[r][c] != word[i]) {return false;}
+    bool exist(vector<vector<char>>& b, string word) {
+       int m=b.size();
+        if(m==0) return false;
+        int n = b[0].size();
         
-        board[r][c] = '#';
         
-        bool ret = search(r - 1, c, i + 1, word, board) || search(r + 1, c, i + 1, word, board)
-                || search(r, c - 1, i + 1, word, board) || search(r, c + 1, i + 1, word, board);
+        // #Pruning
         
-        board[r][c] = word[i];
-        return ret;
-    }
-    
-    bool exist(vector<vector<char>>& board, string word) {
-        int R = board.size(), C = board[0].size(), N = word.size();
         
-        // Prune #1: If there aren't even enough letters in the board to fit word, then return false
-        if((int) word.size() > R * C) {return false;}
-        
-        // Prune #2: For each letter, the board must contain at least as many of that letter as word contains
-        int occ[128] = {};
-        for(const auto &v : board) {for(char c : v) {++occ[c];}}
-        for(char c : word) {
-            if(--occ[c] < 0) {return false;}
-        }
-        
-        // Prune #3: Find the longest prefix/suffix of the same character. If the longest suffix is longer
-        // than the longest prefix, swap the strigns (so we are less likely to have a long prefix with a lot
-        // of the same character)
-        int left_pref = word.find_first_not_of(word[0]);
-        int right_pref = word.size() - word.find_last_not_of(word.back());
-        if(left_pref > right_pref) {
-            reverse(word.begin(), word.end());
-        }
-        
-        // for every starting point, see if we can find word from there
-        for(int r = 0; r < R; ++r) {
-            for(int c = 0; c < C; ++c) {
-                if(search(r, c, 0, word, board)) {return true;}
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                
+                if(b[i][j]==word[0]){
+                    if(helper(b, i, j, m, n, 0, word)) return true;
+                }
             }
         }
-        return false;
+     return false;
+    }
+    
+    
+    bool helper(vector<vector<char>> &b, int i, int j, int m, int n, int k, string word){
+        if(k >= word.size() ) return true;
+        if( i<0 || j<0 || i>=m || j>=n || b[i][j]=='.' || b[i][j]!=word[k]) return false;
+        if(word.size()==1 && word[k]==b[i][j]) return true;
+        
+        b[i][j]='.';
+        int temp=false;
+        
+        // int x[4] = {0,0,-1,1};
+        // int y[4] = {-1,1,0,0};
+        // for(int index=0;index<4;index++){
+        //     temp = temp || helper(b,i+x[index],j+y[index],m,n,k+1,word);
+        // }
+        
+        temp = temp || helper(b, i-1, j, m, n , k+1, word);
+        temp = temp || helper(b, i+1, j, m, n , k+1, word);
+        temp = temp || helper(b, i, j-1, m, n , k+1, word);
+        temp = temp || helper(b, i, j+1, m, n , k+1, word);
+        
+        b[i][j]=word[k];
+        return temp;
     }
 };
