54a86d0113fa2263b7d6b7d2d41eabf7e66facff
Akshay Soni <akshaysoni460@gmail.com>
2023-04-06 10:22:36 +0530

    Time: 400 ms (83.98%), Space: 8 MB (62.71%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& b, string word) {
-     
-        
-        int m=b.size();
-        if(m==0) return false;
-        int n = b[0].size();
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                
-                if(b[i][j]==word[0]){
-                    if(helper(b, i, j, m, n, 0, word)) return true;
+    bool exist(vector<vector<char> > &board, string word) {
+    		 m=board.size();
+    		 n=board[0].size();
+            for(int x=0;x<m;x++)
+                for(int y=0;y<n;y++)
+                {
+    				if(isFound(board,word.c_str(),x,y))
+    					return true;
                 }
-            }
+            return false;
         }
-     return false;
-    }
+    private:
+    	int m;
+    	int n;
+        bool isFound(vector<vector<char> > &board, const char* w, int x, int y)
+        {
+    		if(x<0||y<0||x>=m||y>=n||board[x][y]=='\0'||*w!=board[x][y])
+    			return false;
+            if(*(w+1)=='\0')
+                return true;
+    		char t=board[x][y];
+    		board[x][y]='\0';
+    		if(isFound(board,w+1,x-1,y)||isFound(board,w+1,x+1,y)||isFound(board,w+1,x,y-1)||isFound(board,w+1,x,y+1))
+    			return true; 
+    		board[x][y]=t;
+            return false; 
     
-    
-    bool helper(vector<vector<char>> &b, int i, int j, int m, int n, int k, string word){
-        if(k >= word.size() ) return true;
-        if( i<0 || j<0 || i>=m || j>=n || b[i][j]=='.' || b[i][j]!=word[k]) return false;
-        if(word.size()==1 && word[k]==b[i][j]) return true;
-        
-        b[i][j]='.';
-        int temp=false;
-        
-        // int x[4] = {0,0,-1,1};
-        // int y[4] = {-1,1,0,0};
-        // for(int index=0;index<4;index++){
-        //     temp = temp || helper(b,i+x[index],j+y[index],m,n,k+1,word);
-        // }
-        
-        temp = temp || helper(b, i-1, j, m, n , k+1, word);
-        temp = temp || helper(b, i+1, j, m, n , k+1, word);
-        temp = temp || helper(b, i, j-1, m, n , k+1, word);
-        temp = temp || helper(b, i, j+1, m, n , k+1, word);
-        
-        b[i][j]=word[k];
-        return temp;
-        
-    }
+}
 };
