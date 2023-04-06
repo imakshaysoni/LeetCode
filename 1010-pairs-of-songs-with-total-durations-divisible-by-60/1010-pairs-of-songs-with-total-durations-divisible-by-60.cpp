e6c08710bb1062f41dcbc2ed6359ea37d5c4046d
Akshay Soni <akshaysoni460@gmail.com>
2023-04-06 22:05:06 +0530

    Time: 40 ms (67.38%), Space: 28.2 MB (69.96%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -2,22 +2,36 @@ class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& time) {
         
-        int ans=0;
-        int n = time.size();
-        if(time.size()<=1)return 0;
-        unordered_map<int,int> m;
-        for(int i=n-1;i>=0;i--){
-            int x = time[i]%60;
-            int find=60-x;
-            if(m.find(find)!=m.end()){
-                ans+=m[find];
-            }
-            if(x==0)x=find;
-            if(m.find(x)!=m.end())m[x]+=1;
-            else m[x]=1;
+        
+         int count=0;
+        vector<int>v(60,0);
+        for(int i=0;i<time.size();i++)
+        {
+            int a=time[i]%60;
+            if(a==0)
+                count+=v[0];
+            else
+                count+=v[60-a];
+            v[a]++;
+        }
+        return count;
+        
+//         int ans=0;
+//         int n = time.size();
+//         if(time.size()<=1)return 0;
+//         unordered_map<int,int> m;
+//         for(int i=n-1;i>=0;i--){
+//             int x = time[i]%60;
+//             int find=60-x;
+//             if(m.find(find)!=m.end()){
+//                 ans+=m[find];
+//             }
+//             if(x==0)x=find;
+//             if(m.find(x)!=m.end())m[x]+=1;
+//             else m[x]=1;
             
-        }        
-        return ans;
+//         }        
+//         return ans;
     }
 };
 
