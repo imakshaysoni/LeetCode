354035ed03abffb90b3009d4e57a186c69793d24
Akshay Soni <akshaysoni460@gmail.com>
2023-04-06 17:39:15 +0530

    Time: 0 ms (100.00%), Space: 7 MB (65.90%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        
-        int m = board.size();
-        int n = board[0].size();
-        int count=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                count = check(board,i,j,m,n);
-                if(board[i][j]==0){
-                    if(count==3)board[i][j]=3;
-                }
-                else if(board[i][j]==1){
-                    if(count <2 || count>3)board[i][j]=2;
-                }
-                
+        int n = board.size(), m = board[0].size(); // Size of board
+        int dx[] = {-1,-1,-1,0,0,1,1,1}; 
+        int dy[] = {-1,0,1,-1,1,-1,0,1}; /*Direction vectors to visit  8 neighbours */
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j < m ; j++){ // For each cell of the board
+                int countLive = 0; // Count how many live neighbours it has
+                for(int k = 0; k < 8 ; k++){ // visit its 8 neighbours
+                    int destx = i + dx[k];
+                    int desty = j + dy[k]; // current neighbour is destx and desty
+                    if(destx < 0 || desty < 0 || destx >= n || desty >= m) continue; // neighbour should be within boundary of the board
+                    if(board[destx][desty] == 1 || board[destx][desty] == 2) countLive++; // 1 or 2 means it was alive in previous state
+                } 
+                if((countLive < 2 || countLive > 3) && board[i][j] == 1) board[i][j] = 2; //conditions for a live cell to die
+                if(board[i][j] == 0 && countLive == 3) board[i][j] = 3; // conditions for a dead cell to come alive
             }
         }
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]==2 or board[i][j]==3){
-                if(board[i][j]%2==0)board[i][j]=0;
-                else board[i][j]=1;
-                }
+        for(int i = 0 ; i < n;  i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == 2) board[i][j] = 0;
+                if(board[i][j] == 3) board[i][j] = 1;
+                // mark each newly alive and dead cell as 0 and 1.
             }
         }
-        
-        
-        return;
-        
-    }
-    
-    int check(vector<vector<int>> &board, int i, int j, int m, int n){
-        int count=0;        
-        vector<int> x = {0,0,1,1,-1,-1,-1,1};
-        vector<int> y = {-1,1,-1,1,0,-1,1,0};
-        for(int index=0;index<x.size();index++){
-            if(i+x[index] < 0 || i+x[index] >= m) continue;
-            if(j+y[index] < 0 || j+y[index] >= n ) continue;
-            if(board[i+x[index]][j+y[index]] == 1 or board[i+x[index]][j+y[index]] == 2){
-                count++;
-            }
-        }
-        return count;    
-        
-       
-        
     }
 };
