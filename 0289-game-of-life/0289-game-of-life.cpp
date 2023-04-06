3c37d00f95115d630966d1e2f5a3616e403cfcee
Akshay Soni <akshaysoni460@gmail.com>
2023-04-06 17:26:09 +0530

    Time: 2 ms (49.10%), Space: 7.1 MB (23.95%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -7,9 +7,7 @@ public:
         int count=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int count =0;
                 count = check(board,i,j,m,n);
-                cout<<"count: "<<count<<endl;
                 if(board[i][j]==0){
                     if(count==3)board[i][j]=3;
                 }
@@ -35,18 +33,14 @@ public:
     }
     
     int check(vector<vector<int>> &board, int i, int j, int m, int n){
-        int count=0;
-        
+        int count=0;        
         vector<int> x = {0,0,1,1,-1,-1,-1,1};
         vector<int> y = {-1,1,-1,1,0,-1,1,0};
         for(int index=0;index<x.size();index++){
             if(i+x[index] < 0 || i+x[index] >= m) continue;
             if(j+y[index] < 0 || j+y[index] >= n ) continue;
-            cout<<i+x[index]<<","<<j+y[index]<<endl;
-            cout<<board[i+x[index]][j+y[index]]<<endl;
             if(board[i+x[index]][j+y[index]] == 1 or board[i+x[index]][j+y[index]] == 2){
-                // cout<<i<<","<<j<<endl;
-                count+=1;
+                count++;
             }
         }
         return count;    
