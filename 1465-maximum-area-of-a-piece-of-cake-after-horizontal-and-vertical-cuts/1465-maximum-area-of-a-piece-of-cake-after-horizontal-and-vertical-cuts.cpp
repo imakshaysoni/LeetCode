c7efc6cbaa29b9a7514ea142f153a17ba83976a7
Akshay Soni <akshaysoni460@gmail.com>
2023-04-07 21:13:53 +0530

    Time: 81 ms (44.74%), Space: 32.3 MB (84.33%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
-    int maxArea(int h, int w, vector<int>& HCuts, vector<int>& VCuts) {
+    int maxArea(int h, int w, vector<int>& hC, vector<int>& vC) {
         
-        HCuts.push_back(h);
-        HCuts.push_back(0);
-        sort(HCuts.begin(),HCuts.end());
         
-        int maxh=INT_MIN;
-        for(int i=1;i<HCuts.size();i++){
-            maxh = max(maxh, HCuts[i]-HCuts[i-1]);
+         hC.push_back(h);
+        sort(hC.begin(), hC.end());
+		int maxh = hC[0];
+        for(int i=1; i<hC.size(); i++){
+            maxh = max(maxh, hC[i] - hC[i-1]);
         }
         
-        VCuts.push_back(w);
-        VCuts.push_back(0);
-        sort(VCuts.begin(),VCuts.end());
-        
-        int maxv=INT_MIN;
-        for(int i=1;i<VCuts.size();i++){
-            maxv = max(maxv, VCuts[i]-VCuts[i-1]);
+        vC.push_back(w);
+        sort(vC.begin(), vC.end());
+		int maxv = vC[0];
+        for(int i=1; i<vC.size(); i++){
+            maxv = max(maxv, vC[i] - vC[i-1]);
         }
+		
+        return (1LL*maxh*maxv) % 1000000007; //1LL used to make the product long long or integer sign overflow will occur.
         
-        cout<<maxh<<" "<<maxh;
-        return (1LL*maxh*maxv)%1000000007;
+//         HCuts.push_back(h);
+//         HCuts.push_back(0);
+//         sort(HCuts.begin(),HCuts.end());
+        
+//         int maxh=INT_MIN;
+//         for(int i=1;i<HCuts.size();i++){
+//             maxh = max(maxh, HCuts[i]-HCuts[i-1]);
+//         }
+        
+//         VCuts.push_back(w);
+//         VCuts.push_back(0);
+//         sort(VCuts.begin(),VCuts.end());
+        
+//         int maxv=INT_MIN;
+//         for(int i=1;i<VCuts.size();i++){
+//             maxv = max(maxv, VCuts[i]-VCuts[i-1]);
+//         }
+        
+//         cout<<maxh<<" "<<maxh;
+//         return (1LL*maxh*maxv)%1000000007;
         
     }
 };
