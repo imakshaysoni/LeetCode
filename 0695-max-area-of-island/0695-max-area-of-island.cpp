5e285d9c4412db509197090ba740fe276b7e0ae3
Akshay Soni <akshaysoni460@gmail.com>
2023-04-08 18:44:11 +0530

    Time: 25 ms (40.34%), Space: 23.2 MB (92.51%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,35 +1,32 @@
 class Solution {
-public:    
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-//         FLood FIll Algorithm
-        int m = grid.size();
-        int n = grid[0].size();
-        int count = 0;
-        int maxCount = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]){
-                    getCnt(grid, m,n,i,j,count);
-                }
-                maxCount = max(maxCount,count);
-                count=0;
-            }
-        }        
-        return maxCount;                
-    }
+public:
     
-    void getCnt(vector<vector<int>> &grid, int m, int n, int i, int j,int &count){
+    int utility(vector<vector<int>>& grid, int i, int j)
+    {
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size())
+            return 0;
+        if(grid[i][j]==0)
+            return 0;
+       
+        grid[i][j]=0;
          
-        if( i<0 || i==m || j<0 || j==n || grid[i][j] == 0) return;
-        grid[i][j] = 0;
-        count++;
-        vector<int> x = {0,0,1,-1};
-        vector<int> y = {-1,1,0,0};        
-    
-        for(int index=0;index<4;index++){
-            getCnt(grid, m, n, i+x[index], j+y[index], count);    
-        }
-        return;   
+            
+        return (1+ utility(grid, i+1, j) + utility(grid, i, j+1) + utility(grid, i-1, j) + utility(grid, i, j-1));
     }
     
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int ans = 0, x =0;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j])
+                    x = utility(grid, i, j);
+                
+                ans = max(ans, x);
+            }
+        return ans;
+    }
 };
