68867384f46525d71cb13d000427b4bad2f215af
Akshay Soni <akshaysoni460@gmail.com>
2023-04-08 18:34:05 +0530

    Time: 99 ms (5.01%), Space: 42 MB (5.01%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+      
+    
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+//         FLood FIll Algorithm
+        int m = grid.size();
+        int n = grid[0].size();
+        int count = 0;
+        int maxCount = INT_MIN;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                getCnt(grid, m,n,i,j,count);
+                cout<<count<<" ";
+                maxCount = max(maxCount,count);
+                count=0;
+            }
+        }
+        
+        return maxCount;        
+        
+    }
+    
+    
+    void getCnt(vector<vector<int>> &grid, int m, int n, int i, int j,int &count){
+         
+        if( i<0 || i==m || j<0 || j==n || grid[i][j] == 0 || grid[i][j] == '*') return;
+        
+        int temp = grid[i][j];
+        grid[i][j] = '*';
+        count++;
+        vector<int> x = {0,0,1,-1};
+        vector<int> y = {-1,1,0,0};        
+    
+        for(int index=0;index<4;index++){
+            getCnt(grid, m, n, i+x[index], j+y[index], count);    
+        }
+        // grid[i][j] = temp;
+        return;     
+        
+    }
+    
+};
