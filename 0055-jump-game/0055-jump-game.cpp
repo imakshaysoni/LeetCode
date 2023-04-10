61b62909a2b2776aa52eba1078bd5436febeb8bc
Akshay Soni <akshaysoni460@gmail.com>
2023-04-10 13:18:05 +0530

    Time: 59 ms (90.31%), Space: 48.4 MB (50.99%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -7,6 +7,7 @@ public:
         if(n<2) return true;
         int reach = 0;
         for(int i=0;i<n-1 && i <= reach;i++){
+            if(reach<i) return false;
             int currReach = i+nums[i];
             reach = max(currReach, reach);
         }
