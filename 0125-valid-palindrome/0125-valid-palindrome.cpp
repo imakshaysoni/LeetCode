88754db846e76210897b85eb440529a9666c1b0c
Akshay Soni <akshaysoni460@gmail.com>
2023-04-11 23:44:46 +0530

    Time: 7 ms (49.72%), Space: 7.4 MB (85.72%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -7,10 +7,10 @@ public:
     
     bool isPal(string &s, int l, int r){
         if(l>r) return true;
-         while (isalnum(s[l]) == false && l < r) l++; // Increment left pointer if not alphanumeric
-        while (isalnum(s[r]) == false && l < r) r--; // Decrement right pointer if no alphanumeric
-        // while(!isalnum(s[l]) && l<r)l++;
-        // while(!isalnum(s[r]) && r>l)r--;
+        //  while (isalnum(s[l]) == false && l < r) l++; // Increment left pointer if not alphanumeric
+        // while (isalnum(s[r]) == false && l < r) r--; // Decrement right pointer if no alphanumeric
+        while(!isalnum(s[l]) && l<r)l++;
+        while(!isalnum(s[r]) && r>l)r--;
         if(l==r) return true;
         if(toupper(s[l]) != toupper(s[r])) return false;
         
