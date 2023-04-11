c26b6b7be2402d8b34375076a3a7f7d37c8debfd
Akshay Soni <akshaysoni460@gmail.com>
2023-04-11 21:46:26 +0530

    Time: 0 ms (100.00%), Space: 6 MB (78.58%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,41 +1,33 @@
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+//         vector<int> dp;
+        
+//         if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
+//         if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
+//         if (n< 0 ) return 1.0/pow(x,abs(n));
+//         return pow(x,n);    
+//     }
+    
+//     double pow(double x, int n){     
+//         if(n==0) return 1;     
+//         return x * pow(x,n-1);
+//     }
+// };
+
 class Solution {
 public:
     double myPow(double x, int n) {
-        vector<int> dp;
-        
-        if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
-        if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
-        if (n< 0 ) return 1.0/pow(x,abs(n));
-        return pow(x,n);    
-    }
-    
-    double pow(double x, int n){     
-        if(n==0) return 1;     
-        return x * pow(x,n-1);
+        if(n==0) return 1;
+        if(n<0) {
+            n = abs(n);
+            x = 1/x;
+        }
+        if(n%2==0){
+            return myPow(x*x, n/2);
+        }
+        else{
+            return x*myPow(x, n-1);
+        }
     }
 };
-
-// class Solution {
-// private:
-//     double power(double x, int n){
-//         if(n==0){
-//             return 1;
-//         }
-//         return x * power(x, n-1);
-//     }
-// public:
-//     double myPow(double x, int n) {
-//         if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
-//         if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
-//         double num = 1;
-//         if(n>=0){
-//             num = power(x, n);
-//         }
-//         else{
-//             n = -n;
-//             num = power(x, n);
-//             num = 1.0/num;
-//         }
-//         return num;
-//     }
-// };
