4ceceba5b18ad32fd09fc5c2dd03ed9a95c37a1a
Akshay Soni <akshaysoni460@gmail.com>
2023-04-11 21:45:06 +0530

    Time: 31 ms (5.11%), Space: 35.4 MB (5.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,41 +1,41 @@
-// class Solution {
-// public:
-//     double myPow(double x, int n) {
-//         vector<int> dp;
-//         if (n< 0 ) return 1 / pow(x,abs(n));
-//         return pow(x,n);
-    
-//     }
-    
-//     double pow(double x, int n){     
-//         if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
-//         if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
-//         if(n==0) return 1;     
-//         return x * pow(x,n-1);
-//     }
-// };
-
 class Solution {
-private:
-    double power(double x, int n){
-        if(n==0){
-            return 1;
-        }
-        return x * power(x, n-1);
-    }
 public:
     double myPow(double x, int n) {
+        vector<int> dp;
+        
         if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
         if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
-        double num = 1;
-        if(n>=0){
-            num = power(x, n);
-        }
-        else{
-            n = -n;
-            num = power(x, n);
-            num = 1.0/num;
-        }
-        return num;
+        if (n< 0 ) return 1.0/pow(x,abs(n));
+        return pow(x,n);    
+    }
+    
+    double pow(double x, int n){     
+        if(n==0) return 1;     
+        return x * pow(x,n-1);
     }
 };
+
+// class Solution {
+// private:
+//     double power(double x, int n){
+//         if(n==0){
+//             return 1;
+//         }
+//         return x * power(x, n-1);
+//     }
+// public:
+//     double myPow(double x, int n) {
+//         if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
+//         if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
+//         double num = 1;
+//         if(n>=0){
+//             num = power(x, n);
+//         }
+//         else{
+//             n = -n;
+//             num = power(x, n);
+//             num = 1.0/num;
+//         }
+//         return num;
+//     }
+// };
