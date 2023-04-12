606f76ee13f1a193ffb91fb6db0af7c6dfea1629
Akshay Soni <akshaysoni460@gmail.com>
2023-04-12 11:56:29 +0530

    Time: 343 ms (5.49%), Space: 108.6 MB (10.66%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+       
+        vector<vector<int>> ans;
+        vector<int> subset;
+        return subarray(nums,0,subset,ans);
+        // return ans;
+        
+    }
+    
+    vector<vector<int>> subarray(vector<int> &v, int i, vector<int> &subset, vector<vector<int>> &ans){
+        
+        if(i==v.size()) {
+            ans.push_back(subset);
+            return ans;
+        }
+        
+//         Include first Term
+        subset.push_back(v[i]);
+        subarray(v,i+1,subset,ans);
+        
+//         Ignore first term
+        subset.pop_back();
+        subarray(v,i+1,subset,ans);
+        
+        return ans;
+        
+        
+    }
+};
