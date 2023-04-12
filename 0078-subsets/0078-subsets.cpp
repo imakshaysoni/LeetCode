63e2acc7bfe24150e56a820786210e0550c35bda
Akshay Soni <akshaysoni460@gmail.com>
2023-04-12 18:15:12 +0530

    Time: 7 ms (20.85%), Space: 7.7 MB (48.42%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -4,30 +4,51 @@ public:
        
         vector<vector<int>> ans;
         vector<int> subset;
-        subarray(nums,0,subset,ans);
-        return ans;
+        // subarray(nums,0,subset,ans);
+        // return ans;
+        return SubSets(nums,0);
         
     }
     
-   void subarray(vector<int> &v, int i, vector<int> &subset, vector<vector<int>> &ans){
+    
+    
+    vector<vector<int>> SubSets(vector<int> &v, int i){
         
-        if(i==v.size()) {
-            ans.push_back(subset);
-            return;
+        if(i==v.size()) return {{}};
+        
+        vector<vector<int>> partialAns = SubSets(v,i+1);
+        vector<vector<int> > ans;
+        for(auto x: partialAns){
+            ans.push_back(x);
         }
-       
-//         Include first Term
-        subset.push_back(v[i]);
-        subarray(v,i+1,subset,ans);
         
-//         Ignore first term
-        subset.pop_back();
-        subarray(v,i+1,subset,ans);
-       
-       
+        for(auto x: partialAns){
+            x.push_back(v[i]);
+            ans.push_back(x);
+        }
         
-        return;
-        
-        
+        return ans;
     }
+    
+//    void subarray(vector<int> &v, int i, vector<int> &subset, vector<vector<int>> &ans){
+        
+//         if(i==v.size()) {
+//             ans.push_back(subset);
+//             return;
+//         }
+       
+// //         Include first Term
+//         subset.push_back(v[i]);
+//         subarray(v,i+1,subset,ans);
+        
+// //         Ignore first term
+//         subset.pop_back();
+//         subarray(v,i+1,subset,ans);
+       
+       
+        
+//         return;
+        
+        
+//     }
 };
