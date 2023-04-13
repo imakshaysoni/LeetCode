ff4374b59763c667e06b3820c135212c250cadf4
Akshay Soni <akshaysoni460@gmail.com>
2023-04-13 15:30:36 +0530

    Time: 0 ms (100.00%), Space: 16.6 MB (19.52%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,61 +1,33 @@
 class Solution {
 public:
-    std::vector<std::vector<int> > subsetsWithDup(std::vector<int> &nums) {
-		std::sort(nums.begin(), nums.end());
-        std::vector<std::vector<int> > res;
-		std::vector<int> vec;
-		subsetsWithDup(res, nums, vec, 0);
-		return res;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        vector<int> subset;
+        uniqueSubSets(nums,subset,0, ans);
+        return ans;    
+        
     }
-private:
-	void subsetsWithDup(std::vector<std::vector<int> > &res, std::vector<int> &nums, std::vector<int> &vec, int begin) {
-		res.push_back(vec);
-		for (int i = begin; i != nums.size(); ++i)
-			if (i == begin || nums[i] != nums[i - 1]) { 
-				vec.push_back(nums[i]);
-				subsetsWithDup(res, nums, vec, i + 1);
-				vec.pop_back();
-			}
-	}
+    
+    void uniqueSubSets(vector<int> &v, vector<int> subset, int i, vector<vector<int>> &ans){
+        
+        if(i>=v.size()){
+            ans.push_back(subset);
+            return;
+        }
+        
+        
+        // Take First Term
+        subset.push_back(v[i]);
+        uniqueSubSets(v,subset,i+1,ans);
+        
+        
+        subset.pop_back();
+        
+        while(i<v.size()-1 && v[i]==v[i+1])i++;
+//         Ignore the first term
+        uniqueSubSets(v,subset,i+1,ans);
+    }
+    
 };
-
-
-// class Solution {
-// public:
-//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
-//         vector<vector<int>> ans;
-//         sort(nums.begin(),nums.end());
-//         vector<int> subset;
-//         subSets(nums,0,ans, subset);
-//         return ans;
-        
-//     }
-    
-//     void subSets(vector<int> &v, int i, vector<vector<int>> &ans, vector<int> &subset){
-        
-        
-//        if(i>=v.size()){
-//            ans.push_back(subset);
-//            return;
-//        } 
-        
-// //       Take the first term
-//         subset.push_back(v[i]);
-//         subSets(v,i+1,ans,subset);
-        
-// //      Not taking first value
-//         unordered_set<int> s;
-//         int ignored = subset.back();        
-//         s.insert(ignored);
-//         subset.pop_back();        
-//         // while(i+1<v.size() && v[i]==v[i+1]) i++;
-    
-//         while(s.find(v[i])!=s.end()){
-//             i++;
-//         }        
-        
-//         subSets(v,i+1,ans,subset);
-//         // s.erase(s.find(ignored));
-//     }
-// };
