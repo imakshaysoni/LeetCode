676186bffe367104c6c8bfd1e4990e13275a786b
Akshay Soni <akshaysoni460@gmail.com>
2023-04-13 17:32:21 +0530

    Time: 503 ms (14.10%), Space: 164 MB (17.64%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        
+        vector<int> subset;
+        vector<vector<int>> ans;
+        // vector<int> subset;
+        helper(1,n,k,ans, subset);
+        
+        return ans;
+    }
+    
+    void helper(int i ,int n, int k, vector<vector<int>> &ans, vector<int> subset){
+        
+        
+        if(subset.size()==k){ ans.push_back(subset); return;}
+        
+        for(int j=i;j<n+1;j++){
+            subset.push_back(j);
+            helper(j+1,n,k,ans,subset);
+            subset.pop_back();
+        }
+    } 
+};
