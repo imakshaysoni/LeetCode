ac696abc2497567ef4d726cedf96e8b3ff442e3c
Akshay Soni <akshaysoni460@gmail.com>
2023-04-13 16:14:07 +0530

    Time: 3 ms (74.09%), Space: 7.5 MB (88.50%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,49 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
-        vector<int> subset;
-        uniqueSubSets(nums,subset,0, ans, false);
-        return ans;    
-        
+    std::vector<std::vector<int> > subsetsWithDup(std::vector<int> &nums) {
+		std::sort(nums.begin(), nums.end());
+        std::vector<std::vector<int> > res;
+		std::vector<int> vec;
+		subsetsWithDup(res, nums, vec, 0);
+		return res;
     }
-    
-    void uniqueSubSets(vector<int> &v, vector<int> subset, int i, vector<vector<int>> &ans, bool prev){
-        
-        if(i>=v.size()){
-            ans.push_back(subset);
-            return;
-        }
-        
-        
-        uniqueSubSets(v,subset,i+1,ans,false);
-        //Method 1:  while(i<v.size()-1 && v[i]==v[i+1])i++;
-        
-        //**********************************//
-//         Prev used value Logic
-        
-        if(i>0 && v[i]==v[i-1] && !prev){
-            return;
-        }
-        
-            
-        // Take First Term
-        subset.push_back(v[i]);
-        uniqueSubSets(v,subset,i+1,ans, true);             
-        subset.pop_back();
-        
-        
-        
-        
-        
-        
-        
-        
-        //**********************************//
-//         Ignore the first term
-        
-    }
-    
+private:
+	void subsetsWithDup(std::vector<std::vector<int> > &res, std::vector<int> &nums, std::vector<int> &vec, int begin) {
+		res.push_back(vec);
+		for (int i = begin; i != nums.size(); ++i)
+			if (i == begin || nums[i] != nums[i - 1]) { 
+				vec.push_back(nums[i]);
+				subsetsWithDup(res, nums, vec, i + 1);
+				vec.pop_back();
+			}
+	}
 };
