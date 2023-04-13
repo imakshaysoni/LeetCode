b5f65a0ea03894915da005b38370b1673c26b025
Akshay Soni <akshaysoni460@gmail.com>
2023-04-13 20:46:25 +0530

    Time: 33 ms (39.63%), Space: 18 MB (32.07%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -10,15 +10,41 @@ public:
         return ans;
     }
     
-    void helper(int i ,int n, int k, vector<vector<int>> &ans, vector<int> &subset){
+    
+    void helper(int i, int n, int k, vector<vector<int>> &ans, vector<int> &subSets){
         
         
-        if(subset.size()==k){ ans.push_back(subset); return;}
-        
-        for(int j=i;j<n+1;j++){
-            subset.push_back(j);
-            helper(j+1,n,k,ans,subset);
-            subset.pop_back();
+        if(k==0){
+            ans.push_back(subSets);
+            return;
         }
-    } 
+        if(i>n) return;
+                
+//         Taking the first term
+        subSets.push_back(i);
+        helper(i+1,n,k-1,ans,subSets);
+        
+        
+//         Ignorering the value
+        subSets.pop_back();
+        helper(i+1,n,k,ans,subSets);
+        
+        
+    }
+    
+     
 };
+
+
+//  Mehtod whihc i figure out on my own, 80% correct
+// void helper(int i ,int n, int k, vector<vector<int>> &ans, vector<int> &subset){
+        
+        
+//         if(subset.size()==k){ ans.push_back(subset); return;}
+        
+//         for(int j=i;j<n+1;j++){
+//             subset.push_back(j);
+//             helper(j+1,n,k,ans,subset);
+//             subset.pop_back();
+//         }
+//     }
