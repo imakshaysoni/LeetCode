8ae762915af77ca740e2a5d00f2c5f9d5851d99f
Akshay Soni <akshaysoni460@gmail.com>
2023-04-13 15:42:19 +0530

    Time: 0 ms (100.00%), Space: 16.4 MB (21.89%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -5,12 +5,12 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         vector<int> subset;
-        uniqueSubSets(nums,subset,0, ans);
+        uniqueSubSets(nums,subset,0, ans, false);
         return ans;    
         
     }
     
-    void uniqueSubSets(vector<int> &v, vector<int> subset, int i, vector<vector<int>> &ans){
+    void uniqueSubSets(vector<int> &v, vector<int> subset, int i, vector<vector<int>> &ans, bool prev){
         
         if(i>=v.size()){
             ans.push_back(subset);
@@ -18,16 +18,32 @@ public:
         }
         
         
+        uniqueSubSets(v,subset,i+1,ans,false);
+        //Method 1:  while(i<v.size()-1 && v[i]==v[i+1])i++;
+        
+        //**********************************//
+//         Prev used value Logic
+        
+        if(i>0 && v[i]==v[i-1] && !prev){
+            return;
+        }
+        
+            
         // Take First Term
         subset.push_back(v[i]);
-        uniqueSubSets(v,subset,i+1,ans);
-        
-        
+        uniqueSubSets(v,subset,i+1,ans, true);             
         subset.pop_back();
         
-        while(i<v.size()-1 && v[i]==v[i+1])i++;
+        
+        
+        
+        
+        
+        
+        
+        //**********************************//
 //         Ignore the first term
-        uniqueSubSets(v,subset,i+1,ans);
+        
     }
     
 };
