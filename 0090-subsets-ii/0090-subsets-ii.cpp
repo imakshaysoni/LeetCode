15e9092b6b7cef450c96fdfbffaa14ba82e7c312
Akshay Soni <akshaysoni460@gmail.com>
2023-04-13 11:41:01 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (69.94%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -18,14 +18,8 @@ public:
            return;
        } 
         
-//         unordered_set<int> s;
         
-// //         Take the first term
-//         while(s.find(v[i])!=s.end()){
-//             i++;
-//         }
-//         s.insert(v[i]);
-        
+//         Take the first term
         subset.push_back(v[i]);
         subSets(v,i+1,ans,subset);
         
@@ -33,7 +27,13 @@ public:
         subset.pop_back();
         
         while(i+1<v.size() && v[i]==v[i+1]) i++;
+    
+        // unordered_set<int> s;
+        // s.insert(v[i]);
+        // while(s.find(v[i])!=s.end()){
+        //     i++;
+        // }
+        // s.insert(v[i]);        
         subSets(v,i+1,ans,subset);
-        
     }
 };
