4c9b4ca26f39981552f4677fe0c996be006e97d0
Akshay Soni <akshaysoni460@gmail.com>
2023-04-15 21:40:41 +0530

    Time: 12 ms (99.17%), Space: 9.6 MB (95.13%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,40 +1,53 @@
+
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-     
-        int sum=0;
-        vector<int> subset;
-        for(auto x: nums){
-            sum+=x;
-        }
-        if(sum%2 & 1) return false;
-        int target = sum/2;
-        
-        vector<vector<int>> dp(nums.size(),vector<int>(target+1,-1));
-        
-        return helper(nums,target,0, dp);
-        
+        int totalSum = accumulate(begin(nums), end(nums), 0), halfSum = totalSum / 2;
+        if(totalSum & 1) return false;
+        bitset<10001> dp(1);       // 0 is always achievable => dp[0] = 1
+        for(int num : nums) 
+            dp = dp | dp << num;
+        return dp[halfSum];
     }
-    
-    
-    
-    bool helper(vector<int> &nums,int target, int i, vector<vector<int>> &dp){
+};
+
+// class Solution {
+// public:
+//     bool canPartition(vector<int>& nums) {
+     
+//         int sum=0;
+//         vector<int> subset;
+//         for(auto x: nums){
+//             sum+=x;
+//         }
+//         if(sum%2 & 1) return false;
+//         int target = sum/2;
         
-        if(target==0) return true;
-        if(target<0) return false;
+//         vector<vector<int>> dp(nums.size(),vector<int>(target+1,-1));
         
-        if(i>=nums.size()) return false;
-//          Include ith term
-        if(dp[i][target]!=-1){
-            return dp[i][target];
-        }
-        bool op1 = helper(nums,target-nums[i],i+1, dp);
-        bool op2 = helper(nums, target,i+1, dp);        
-        dp[i][target] = op1 || op2;
-        return op1 || op2;                 
+//         return helper(nums,target,0, dp);
+        
+//     }
+    
+    
+    
+//     bool helper(vector<int> &nums,int target, int i, vector<vector<int>> &dp){
+        
+//         if(target==0) return true;
+//         if(target<0) return false;
+        
+//         if(i>=nums.size()) return false;
+// //          Include ith term
+//         if(dp[i][target]!=-1){
+//             return dp[i][target];
+//         }
+//         bool op1 = helper(nums,target-nums[i],i+1, dp);
+//         bool op2 = helper(nums, target,i+1, dp);        
+//         dp[i][target] = op1 || op2;
+//         return op1 || op2;                 
         
         
            
-    }
+//     }
     
-};
+// };
