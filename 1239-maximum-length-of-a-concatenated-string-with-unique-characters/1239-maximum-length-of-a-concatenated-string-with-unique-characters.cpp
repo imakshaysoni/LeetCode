24ed80f8010e3b68fa15b6de12d3bb58bcb8cf5b
Akshay Soni <akshaysoni460@gmail.com>
2023-04-19 12:06:06 +0530

    Time: 130 ms (43.31%), Space: 87.9 MB (31.81%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,54 +1,52 @@
 class Solution {
 public:
-    int maxLength(vector<string>& arr, string str = "", int index = 0) {
-	    //Use set to check if the string contains all unique characters
-        unordered_set<char>s(str.begin(), str.end());
-        if (s.size() != ((int)str.length())) 
-            return 0;
+    int maxLength(vector<string>& arr) {
         
-        int ret = str.length();
-        for (int i = index; i < arr.size(); i++)
-            ret = max(ret, maxLength(arr, str+arr[i], i+1));
-
-        return ret;
+        unordered_map<char, int> m;
+        int ans=0;
+        int finalAns=0;
+        helper(arr,ans,0,m, finalAns);
+        return finalAns;
+    }
+    
+    
+    
+    void helper(vector<string> &arr, int &ans, int i, unordered_map<char,int> &m, int &finalAns){
+        
+        
+        if(i>=arr.size()) {
+            if(ans>finalAns){
+                finalAns=ans;
+            }
+            return;
+        }
+        
+        
+        helper(arr,ans,i+1,m, finalAns);
+        // for(int j=i;j<arr.size();j++){
+            if(uniqueCheck(arr[i],m)){                
+                for(auto x: arr[i]) m[x]++;
+                ans+=arr[i].size();
+                helper(arr,ans,i+1,m,finalAns);
+                for(auto x: arr[i]) m[x]--;
+                ans-=arr[i].size();
+                    
+            }    
+        // }
+                
+    }
+    
+    bool uniqueCheck(string s, unordered_map<char,int> &m){        
+        unordered_map<char,int> map;
+        for(auto x: s){
+            if(map.find(x)!=map.end()){
+                return false;
+            }
+            if(m[x]>0) return false;
+            map[x]++;
+        }
+        
+        return true;
+        
     }
 };
-
-
-
-
-// class Solution {
-// public:
-//     int maxLength(vector<string>& arr) {
-        
-//         if(arr.size()==1) return 1;
-//         int ans=0;
-//         int temp_ans=0;
-//         unordered_set<char> s;
-//         helper(arr,0,ans, s, temp_ans,"");
-        
-//         return ans;
-//     }
-    
-//     void helper(vector<string> &arr, int i, int ans, unordered_set<char> m, int temp_ans, string temp){
-        
-       
-//         m(temp.begin(),temp.end());
-//         if(m.size()!=temp.size()) return;
-        
-//         if(m.size()==temp.size()){
-//             if(temp_ans>ans){ans=temp_ans; return;}
-//         }
-        
-        
-        
-//         for(int index = i;index<arr.size();index++){
-//             temp +=arr[index];
-//             temp_ans++;
-//             helper(arr,i+1,ans,m,temp_ans,temp);
-//             temp -=arr[index];
-//             temp_ans--;
-//         }
-        
-//     }
-// };
