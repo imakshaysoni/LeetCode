162b121e49c35743a821260397cf145426f25a78
Akshay Soni <akshaysoni460@gmail.com>
2023-04-21 14:32:10 +0530

    Time: 8 ms (55.34%), Space: 7.5 MB (49.94%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    std::vector<std::vector<std::string> > solveNQueens(int n) {
-        std::vector<std::vector<std::string> > res;
-        std::vector<std::string> nQueens(n, std::string(n, '.'));
-        /*
-        flag[0] to flag[n - 1] to indicate if the column had a queen before.
-        flag[n] to flag[3 * n - 2] to indicate if the 45° diagonal had a queen before.
-        flag[3 * n - 1] to flag[5 * n - 3] to indicate if the 135° diagonal had a queen before.
-        */
-        std::vector<int> flag(5 * n - 2, 1);
-        solveNQueens(res, nQueens, flag, 0, n);
-        return res;
+    vector<vector<string> > sols; // 2D vector of strings to store the solutions
+    vector<vector<string>> solveNQueens(int n) {    
+	    vector<string> board(n, string(n, '.'));  // creating an empty board
+	    solve(board, 0); // calling the recursive function
+	    return sols; 
     }
-private:
-    void solveNQueens(std::vector<std::vector<std::string> > &res, std::vector<std::string> &nQueens, std::vector<int> &flag, int row, int &n) {
-        if (row == n) {
-            res.push_back(nQueens);
-            return;
+    bool isSafe(vector<string>& board, int row, int col) {
+	    int n = size(board);
+	    for(int i = 0; i < n; i++) {
+			// checking if there is a queen in the same column
+		    if(board[i][col] == 'Q') return false; 
+		    // checking if there is a queen in the same diagonal (left to right)
+		    if(row - i >= 0 && col - i >= 0 && board[row - i][col - i] == 'Q') return false;
+		    if(row - i >= 0 && col + i <  n && board[row - i][col + i] == 'Q') return false;
+		    // No need to traverse more since the lower rows below current row attribute will always be safe.
+			/* if(row + i <  n && col - i >= 0 && board[row + i][col - i] == 'Q') return false;
+			if(row + i <  n && col + i <  n && board[row + i][col + i] == 'Q') return false; */
+	    }
+	    return true;
+    }
+    // Recursive Function (solve) - It basically tries all possible placement of queen for the current row & recurses for it's next row
+    void solve(vector<string>& board, int row) {
+	// Base condition. 
+    // We reached the last row, so we have a solution so we add it to the solution vector
+	    if(row == size(board)) { 
+		    sols.push_back(board);
+		    return;
+    	}            
+	    // Try placing a queen on each column for a given row. 
+        // Explore next row by placing Q at each valid column for the current row
+	    for(int col = 0; col < size(board); col++){
+		    if(isSafe(board, row, col)) {
+			    board[row][col] = 'Q';    // Queen placed on a valid cell
+			    solve(board, row + 1);    // Exploring next row
+			    board[row][col] = '.';    // Backtracking to get all possible solutions
+		    }
         }
-        for (int col = 0; col != n; ++col)
-            if (flag[col] && flag[n + row + col] && flag[4 * n - 2 + col - row]) {
-                flag[col] = flag[n + row + col] = flag[4 * n - 2 + col - row] = 0;
-                nQueens[row][col] = 'Q';
-                solveNQueens(res, nQueens, flag, row + 1, n);
-                nQueens[row][col] = '.';
-                flag[col] = flag[n + row + col] = flag[4 * n - 2 + col - row] = 1;
-            }
     }
 };
 
