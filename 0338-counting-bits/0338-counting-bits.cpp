09557ae0e9e90af3760a42ba728c10040bcdc058
Akshay Soni <akshaysoni460@gmail.com>
2023-04-24 23:15:38 +0530

    Time: 6 ms (61.11%), Space: 7.9 MB (51.91%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,12 +4,14 @@ public:
         
         vector<int> ans(n+1, 0);
         for(int i=1;i<=n;i++){
-            if(i%2==0){
-                ans[i] = ans[i/2];
-            }
-            else{
-                ans[i] = ans[i-1]+1;
-            }
+            
+            ans[i] = ans[i/2] + i%2;
+            // if(i%2==0){
+            //     ans[i] = ans[i/2];
+            // }
+            // else{
+            //     ans[i] = ans[i-1]+1;
+            // }
           
         }
         
