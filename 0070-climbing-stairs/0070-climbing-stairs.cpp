b221d0810bcec30127823bce7b0fe81987ca8e06
Akshay Soni <akshaysoni460@gmail.com>
2023-04-24 22:12:04 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (23.07%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        int ways=0;
+        vector<int> dp(n*n, -1);
+        ways = helper(n, dp, 0);
+        return ways;
+        
+    }
+    
+    int helper(int n, vector<int> &dp, int i){
+     
+//         TLE Now need to add DP
+        if(i==n){
+            return 1;
+        }
+        if(i>n) return 0;
+        
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        
+        dp[i] = helper(n, dp, i+1) + helper(n, dp, i+2);
+        return dp[i];
+//         Taking one step
+//         helper(n, dp, i+1);
+// //     taking 2 steps
+//         helper(n, dp, i+2);
+        
+    }
+};
