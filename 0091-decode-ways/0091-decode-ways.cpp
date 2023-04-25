77d00b92be0e9a584dc6007b4dfbbf3dd6f38b1e
Akshay Soni <akshaysoni460@gmail.com>
2023-04-25 13:26:21 +0530

    Time: 32 ms (5.05%), Space: 6.1 MB (79.97%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -11,19 +11,36 @@ public:
     }
     
     
-    int helper(string &s, int i, vector<int> &dp){
-        if(i==s.size()) return 1;
-        if(i>s.size()) return 0;
-        int ans=0;
-        
+    int helper(string &s, int i, vector<int> &dp){        
+        if(i>=s.size()) return 1;
+        int ans =0;
+        int op1 = s[i]-'0';
+        int op2;
         if(dp[i]!=0) return dp[i];
-        if(s[i]!='0') dp[i] += helper(s,i+1, dp);
-        
-        if(i+1 < s.size() && s[i] == '1' || s[i] == '2' && s[i+1] <= '6' ){
-            dp[i]+= helper(s,i+2, dp);
+        if(op1 > 0) ans+=helper(s,i+1,dp);
+        if(i < s.size()-1){
+            op2 = op1*10 + s[i+1]-'0';
         }
+        if(op1>0 && op2>0 && op2 <=26) ans+=helper(s,i+2,dp);
         
-        return dp[i];
+        return dp[i]=ans;       
+        
         
     }
+    
+    // int helper(string &s, int i, vector<int> &dp){
+//         if(i==s.size()) return 1;
+//         if(i>s.size()) return 0;
+//         int ans=0;
+        
+//         if(dp[i]!=0) return dp[i];
+//         if(s[i]!='0') dp[i] += helper(s,i+1, dp);
+        
+//         if(i+1 < s.size() && s[i] == '1' || s[i] == '2' && s[i+1] <= '6' ){
+//             dp[i]+= helper(s,i+2, dp);
+//         }
+        
+//         return dp[i];
+        
+//     }
 };
