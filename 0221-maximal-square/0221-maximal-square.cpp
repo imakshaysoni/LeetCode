33dc734fb37823437f729ebdbf7cd3a85ee911b3
Akshay Soni <akshaysoni460@gmail.com>
2023-04-26 19:42:39 +0530

    Time: 91 ms (56.48%), Space: 23.1 MB (7.34%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -4,7 +4,8 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(),-1));
         
-        int x = helper(matrix,0,0, dp);
+        // int x = helper(matrix,0,0, dp);
+        int x = dpBottomUp(matrix);
         return maxAns*maxAns;
     }
     
@@ -25,9 +26,30 @@ public:
         }
         else{
             return dp[i][j]=0;
-        }     
+        }          
         
         
+    }
+    
+    int dpBottomUp(vector<vector<char>> &mat){
+        int row = mat.size();
+        int col = mat[0].size();
+        vector<vector<int>> dp(row+1,vector<int> (col+1,0));
+        for(int i=row-1;i>=0;i--){
+            for(int j=col-1;j>=0;j--){
+                
+                int rightCnt = dp[i][j+1];
+                int digCnt = dp[i+1][j+1];
+                int downCnt = dp[i+1][j];
+                
+                if(mat[i][j]=='1'){
+                    dp[i][j]=1+min(rightCnt,min(digCnt,downCnt));
+                    maxAns = max(maxAns, dp[i][j]);
+                }
+                else dp[i][j]=0;
+            }
+        }
+        return 0;
         
     }
 };
