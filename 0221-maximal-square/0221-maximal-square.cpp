9f24695badd55648c068960c47093ecc25f20ef6
Akshay Soni <akshaysoni460@gmail.com>
2023-04-26 20:36:20 +0530

    Time: 102 ms (26.82%), Space: 23.2 MB (5.90%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,3 +1,65 @@
+// class Solution {
+// public:
+//     int maximalSquare(vector<vector<char>>& m) {
+//         int maxCount = 0;
+//         for(int row=0;row<m.size();row++){
+//             for(int col=0;col<m[0].size();col++){
+//                 if(m[row][col]=='1'){
+//                     int area = 0;
+//                     area = helper(m, row, col);
+//                     maxCount = max(maxCount,area);
+//                 }
+//             }
+//         }
+//         return maxCount*maxCount;        
+//     }
+    
+//     int helper(vector<vector<char>> &m, int row, int col){
+        
+//         if(row<0 || row>=m.size() || col<0 || col>=m[0].size() || m[row][col]=='0') return 0;
+// //         
+//         int count =0;
+//         count++;
+        
+// //         right Count
+//         int rightCnt=0;
+//         for(int j=col;j<m[0].size();j++){
+//             if(m[row][j]=='1') rightCnt++;
+//             else break;
+//         }
+        
+//         int downCnt=0;
+//         for(int i=row;i<m.size();i++){
+//             if(m[i][col]=='1') downCnt++;
+//             else break;
+//         }
+        
+//         int digCnt=0;
+//         for(int i=row,j=col;i<m.size() && j<m[0].size();j++,i++){
+//             if(m[i][j]=='1') digCnt++;
+//             else break;
+//         }
+        
+//         int minCount = min(min(rightCnt, downCnt), digCnt);
+//         int cnt = 0;
+//         for(int i=row+1;i<row+minCount;i++){
+//             for(int j=col+1;j<col+minCount;j++){
+//                 // cout<<i<<" "<<j<<endl;
+//                 if(m[i][j]=='0'){
+//                     // return cnt*cnt;
+//                     return i-row+1;
+//                 }
+//             }
+//         }        
+//         // minCount = min(cnt)
+//         return minCount;
+//         return minCount*minCount;
+        
+//     }
+    
+    
+// };
+
 class Solution {
 public:
     int maxAns = 0;
