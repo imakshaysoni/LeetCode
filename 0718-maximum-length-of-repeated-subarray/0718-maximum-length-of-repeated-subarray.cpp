2e6bb9e61876183623f149bedddcf453508fc363
Akshay Soni <akshaysoni460@gmail.com>
2023-04-27 19:52:23 +0530

    Time: 143 ms (89.37%), Space: 10.8 MB (98.22%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,5 +1,5 @@
-class Solution {
-public:
+// class Solution {
+// public:
 //     int findLength(vector<int>& nums1, vector<int>& nums2) {
      
 //         vector<vector<int>> dp(nums1.size()+1, vector<int> (nums2.size()+1, 0));
@@ -20,25 +20,85 @@ public:
 //     }
     
     
-int findLength(vector<int>& nums1, vector<int>& nums2) {
-    if (nums1.size() < nums2.size())
-        swap(nums1, nums2);
+// int findLength(vector<int>& nums1, vector<int>& nums2) {
+//     if (nums1.size() < nums2.size())
+//         swap(nums1, nums2);
 
-    const int n2 = nums2.size();
-    vector<int> indices2[101];
-    for (int i = 0; i < n2; ++i)
-        indices2[nums2[i]].emplace_back(i);
+//     const int n2 = nums2.size();
+//     vector<int> indices2[101];
+//     for (int i = 0; i < n2; ++i)
+//         indices2[nums2[i]].emplace_back(i);
 
-    vector<int> dp0(n2 + 1), dp1(n2 + 1);
-    vector<int>& prev = dp0;
-    vector<int>& curr = dp1;
-    int answer = 0, count = sizeof(int) * prev.size();
-    for (int num : nums1) {
-        memset(prev.data(), 0, count);
-        swap(prev, curr);
-        for (int i : indices2[num])
-            answer = max(answer, curr[i + 1] = prev[i] + 1);
+//     vector<int> dp0(n2 + 1), dp1(n2 + 1);
+//     vector<int>& prev = dp0;
+//     vector<int>& curr = dp1;
+//     int answer = 0, count = sizeof(int) * prev.size();
+//     for (int num : nums1) {
+//         memset(prev.data(), 0, count);
+//         swap(prev, curr);
+//         for (int i : indices2[num])
+//             answer = max(answer, curr[i + 1] = prev[i] + 1);
+//     }
+//     return answer;
+// }
+// };
+
+
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        
+        
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        //moving num2 on num1
+        
+        int ptr2 = 0;
+        
+        int cnt = 0;
+        
+        int largest  = INT_MIN;
+        
+        for(int i=0;i<n1;i++)
+        {
+            cnt = 0;
+            for(int j=i,ptr2=0;j<n1 && ptr2<n2;j++,ptr2++)
+            {
+                if(nums1[j]==nums2[ptr2])
+                {
+                    cnt++;
+                    
+                }
+                else
+                {
+                    cnt = 0;
+                }
+                largest = max(largest,cnt);
+            }
+        }
+        
+        //moving num1 on num2
+        ptr2 = 0;
+        cnt = 0;
+        for(int i=0;i<n2;i++)
+        {
+            cnt = 0;
+            for(int j=i,ptr2=0;j<n2 && ptr2<n1;j++,ptr2++)
+            {
+                if(nums2[j]==nums1[ptr2])
+                {
+                    cnt++;
+                }
+                else
+                {
+                    cnt = 0;
+                }
+                largest = max(largest,cnt);
+            }
+        }
+        
+        return largest;
+        
     }
-    return answer;
-}
 };
