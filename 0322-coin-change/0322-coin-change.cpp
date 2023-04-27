6dd42cd5fd30e307d96cb5ee8153bf508251551f
Akshay Soni <akshaysoni460@gmail.com>
2023-04-27 08:39:02 +0530

    Time: 77 ms (84.94%), Space: 15.4 MB (51.42%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -12,7 +12,7 @@ public:
         vector<int> dp(amount+1,-1);
         int ans = helper(coins, amount, dp);
         
-        if(ans==INT_MAX-1) return -1;
+        if(ans==INT_MAX) return -1;
         return ans;
         
     }
@@ -41,14 +41,14 @@ public:
     
         int helper(vector<int> &coins, int amount, vector<int> &dp){
             if(amount==0) return 0;
-            if(amount<0) return INT_MAX-1;
+            if(amount<0) return INT_MAX;
             
             if(dp[amount]!=-1) return dp[amount];
             
-            int minAns=INT_MAX-1;
+            int minAns=INT_MAX;
             for(int i=0;i<coins.size();i++){
-                int ans = 1 + helper(coins, amount-coins[i], dp);
-                minAns = min(ans, minAns);
+                int ans = helper(coins, amount-coins[i], dp);
+                if(ans!=INT_MAX) minAns = min(1+ans, minAns);
             }
             dp[amount] = minAns;
             
