6da7e0104a6614bfd50db31f04188c83e89e437d
Akshay Soni <akshaysoni460@gmail.com>
2023-04-27 09:05:30 +0530

    Time: 196 ms (10.62%), Space: 14.5 MB (76.70%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -9,13 +9,18 @@ public:
         // return ans;
         
 // //         Method 2
-        vector<int> dp(amount+1,-1);
-        int ans = helper(coins, amount, dp);
+//         vector<int> dp(amount+1,-1);
+//         int ans = helper(coins, amount, dp);
         
+//         if(ans==INT_MAX) return -1;
+//         return ans;
+      // method 3
+         int ans = helper(coins, amount);
         if(ans==INT_MAX) return -1;
         return ans;
         
-    }
+    }       
+        
     
     
     // Method 1
@@ -39,20 +44,37 @@ public:
     
 // //     Method 2
     
-        int helper(vector<int> &coins, int amount, vector<int> &dp){
-            if(amount==0) return 0;
-            if(amount<0) return INT_MAX;
+//         int helper(vector<int> &coins, int amount, vector<int> &dp){
+//             if(amount==0) return 0;
+//             if(amount<0) return INT_MAX;
             
-            if(dp[amount]!=-1) return dp[amount];
+//             if(dp[amount]!=-1) return dp[amount];
             
-            int minAns=INT_MAX;
-            for(int i=0;i<coins.size();i++){
-                int ans = helper(coins, amount-coins[i], dp);
-                if(ans!=INT_MAX) minAns = min(1+ans, minAns);
+//             int minAns=INT_MAX;
+//             for(int i=0;i<coins.size();i++){
+//                 int ans = helper(coins, amount-coins[i], dp);
+//                 if(ans!=INT_MAX) minAns = min(1+ans, minAns);
+//             }
+//             dp[amount] = minAns;
+            
+//             return dp[amount];
+            
+//         }
+    
+//     Method 3 (Tabulation Bottom Up DP)
+    
+    int helper(vector<int> &coins, int amount){
+        
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0] = 0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if(i-coins[j]>=0 && dp[i-coins[j]] != INT_MAX){
+                    dp[i] = min(dp[i], 1+dp[i-coins[j]]);
+                }
             }
-            dp[amount] = minAns;
-            
-            return dp[amount];
-            
         }
+        return dp[amount];
+    }
+    
 };
