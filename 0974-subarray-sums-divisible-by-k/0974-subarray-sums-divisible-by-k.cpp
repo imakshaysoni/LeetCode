2d01e7db9b7f76de322f0ff0aeff375c96d437b5
Akshay Soni <akshaysoni460@gmail.com>
2023-04-28 19:11:47 +0530

    Time: 122 ms (5.13%), Space: 31.5 MB (70.63%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    int subarraysDivByK(vector<int>& A, int K) {
-     
-       vector<int> counts(K, 0);
-    counts[0] = 1;
-    int count = 0, running_sum = 0;
-    for (int i = 0; i < A.size(); i++) {
-        running_sum = (running_sum + A[i]) % K;
-        if (running_sum < 0) running_sum += K; // fix negative modulo
-        count += counts[running_sum];
-        counts[running_sum]++;
-    }
-    return count;
-//         unordered_map<int,int> map;
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
         
-//         int sum=0;
-//         int count=0;
-//         map[0]=-1;
-//         for(int i=0;i<nums.size();i++){
-//             sum+=nums[i];
-//             if(sum%k==0) count++;
-//             int rem = sum%k;
-//             if(map.find(rem)!=map.end()){
-//                 count++;
-//             }
-//             map[rem]=i;
-//         }
+        int sum=0;
+        int count=0;
+        map[0]=1;
+        for(int i=0;i<nums.size();i++){
+            sum=(sum+nums[i])%k;
+            if(sum<0) sum=sum+k;
+            if(map.find(sum)!=map.end()){
+                count+=map[sum];
+            }
+            map[sum]++;
+        }
         
-//         for(auto x: map) cout<<x.first<<"->"<<x.second<<endl;
-//         return count;
+        for(auto x: map) cout<<x.first<<"->"<<x.second<<endl;
+        return count;
         
     }
 };
