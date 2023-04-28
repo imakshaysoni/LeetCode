3648fa6d8d7d5581c77d8848ea2997c96727d608
Akshay Soni <akshaysoni460@gmail.com>
2023-04-28 10:36:30 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (35.92%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,10 +3,18 @@ public:
     int rob(vector<int>& nums) {
         
        
-        vector<int> dp(nums.size()+1, -1);
-        int ans = helper(nums,0, dp);
-        return ans;
+        // vector<int> dp(nums.size()+1, -1);
+        // int ans = helper(nums,0, dp);
+        // return ans;
         
+        vector<int> dp(nums.size()+2,0);
+        for(int i=nums.size()-1;i>=0;i--){
+            int op1 = nums[i]+dp[i+2];
+            int op2 = dp[i+1];
+            dp[i] = max(op1,op2);
+        }
+        
+        return dp[0];
     }
     
     int helper(vector<int> &nums, int i, vector<int> &dp){
@@ -23,4 +31,6 @@ public:
         return dp[i];      
         
     }
+    
+    
 };
