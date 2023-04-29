d3ffd1f5a828c93d0eb575fa00583554e0854696
Akshay Soni <akshaysoni460@gmail.com>
2023-04-29 18:38:27 +0530

    Time: 2955 ms (5.03%), Space: 349.8 MB (5.03%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    #define mod 1000000007
+    int ans=0;
+    int knightDialer(int n) {
+     
+        vector<vector<int>> mat(4,vector<int>(3,0));
+        vector<vector<vector<int>>> dp(4, vector<vector<int>>(3, vector<int>(n+1,-1)));
+          for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 3; j++) {
+                ans = (ans + helper(n,mat, i, j, dp)%mod) % mod;
+                }
+        }
+        return ans%mod;
+    }
+    
+    int helper(int n, vector<vector<int>> &m, int i, int j, vector<vector<vector<int>>> &dp){
+        
+        
+        if(i<0 || i>=4 || j<0 || j>=3 || (i==3 && j!=1) ) return 0;
+        
+        if(n==1) return 1;
+        
+        if(dp[i][j][n]!=-1) return dp[i][j][n]%mod;
+        
+        vector<int> dx = {-2,-2,-1,-1, 1, 1, 2, 2};
+        vector<int> dy = {-1, 1, 2,-2, 2,-2,-1, 1};
+        dp[i][j][n] = 0;
+        for(int index=0;index<8;index++){
+            dp[i][j][n]  = dp[i][j][n]%mod  + helper(n-1,m, i+dx[index], j+dy[index], dp)%mod;
+        }
+        return dp[i][j][n]%mod;
+        
+    }
+    
+};
