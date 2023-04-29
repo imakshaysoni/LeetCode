6d8ba4e7c86a1c5ebb490e7fd7678652e48f2c1b
Akshay Soni <akshaysoni460@gmail.com>
2023-04-29 20:35:57 +0530

    Time: 184 ms (61.15%), Space: 23.5 MB (62.45%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -4,30 +4,18 @@ public:
     vector<vector<int>> ms = { {4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, {}, {1, 7, 0}, {2, 6}, {1, 3}, {4, 2}};
     int final_ans=0;
     int knightDialer(int n) {
-        unordered_map<int, vector<int>> m;
         vector<vector<int>> dp(10,vector<int>(n+1,-1));
-        
-        m[1] = {6, 8};
-        m[2] = {7, 9};
-        m[3] = {4, 8};
-        m[4] = {0, 3, 9};
-        m[5] = {-1,-1};
-        m[6] = {0, 1, 7};
-        m[7] = {2, 6};
-        m[8] = {1, 3};
-        m[9] = {2, 4};
-        m[0] = {4, 6};
         if(n==1) return 10;
         int ans=0;
         for(int digit=0;digit<10;digit++){
-            ans =  (ans%mod + helper(n, m, digit, dp)%mod)%mod;
+            ans =  (ans%mod + helper(n, digit, dp)%mod)%mod;
         }
         
         return ans%mod;
     }
     
     
-    int helper(int n, unordered_map<int,vector<int>> &m, int digit, vector<vector<int>>&dp){
+    int helper(int n, int digit, vector<vector<int>>&dp){
         
         if(n-1==0) return 1;
         
@@ -42,7 +30,7 @@ public:
         // sum+=op1+op2+op3;
         
         for(int x: ms[digit]){
-            sum= (sum%mod + helper(n-1,m,x,dp) % mod)%mod;
+            sum= (sum%mod + helper(n-1,x,dp) % mod)%mod;
         }
         
         return dp[digit][n] = sum%mod;
