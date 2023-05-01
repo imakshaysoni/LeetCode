ad3089ab8cc770a2263fcacd032e2735e7b4980c
Akshay Soni <akshaysoni460@gmail.com>
2023-05-01 09:58:55 +0530

    Time: 106 ms (9.80%), Space: 26.4 MB (31.85%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -20,13 +20,13 @@ public:
         
         if(i>=m.size() || j>=m[0].size() || m[i][j]==0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int count=0;
+        int count=1;
         int right = helper(m, i, j+1, dp);
         int down  = helper(m, i+1, j, dp);
         int rightDig = helper(m, i+1, j+1, dp);
         
             // cout<<right<<"-"<<down<<"-"<<rightDig<<endl;
-            count += 1 + min(right, min(down, rightDig));
+            count = 1 + min(right, min(down, rightDig));
         return dp[i][j]=count;
     }
 };
