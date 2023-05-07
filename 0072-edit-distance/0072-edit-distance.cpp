9c21cdad88222cabab8a8de71e9d2c4a4c39ba97
Akshay Soni <akshaysoni460@gmail.com>
2023-05-07 12:45:26 +0530

    Time: 11 ms (78.34%), Space: 9 MB (64.40%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -5,8 +5,30 @@ public:
         int m = word1.size();
         int n = word2.size();
         
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return helper(word1, word2, m-1, n-1, dp);
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
+        // return helper(word1, word2, m-1, n-1, dp);
+        
+//         Converting into bottom up DP
+        vector<vector<int>> dp(m+1, vector<int> (n+1,0));
+        
+        for(int j=0;j<=n;j++) dp[0][j] = j;
+        for(int i=0;i<=m;i++) dp[i][0] = i;
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] =  1 + min(dp[i][j-1],
+                                   min(dp[i-1][j],
+                                       dp[i-1][j-1]));
+                }
+            }
+        }
+        
+        return dp[m][n];
+        
         
     }
     
@@ -16,6 +38,7 @@ public:
 //         Base Case
 //         If S1 Get Exsaust
         if(i<0) return j+1;
+//         If S2 Get Exsaust
         if(j<0) return i+1;      
         
         if(dp[i][j]!=-1) return dp[i][j];
