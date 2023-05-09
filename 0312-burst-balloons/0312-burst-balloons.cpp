ef15e80adb7f69b25620d821f6d0fd9e4c553100
Akshay Soni <akshaysoni460@gmail.com>
2023-05-09 12:07:54 +0530

    Time: 439 ms (63.61%), Space: 10.4 MB (58.56%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -4,8 +4,30 @@ public:
         int n=nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
-        return solve(nums,1,n, dp);
+        // vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+        // return solve(nums,1,n, dp);
+        
+//         Tabulation
+//     i -> 1, n & j-> n-0 REvese this in tabulation
+        vector<vector<int>> dp(n+2, vector<int>(n+2,0));
+        
+//         base case when i>j dp[i][j] should be = 0
+        for(int i=n;i>0;i--){
+            for(int j=0;j<=n;j++){
+                if(i>j) continue;
+                int maxi = INT_MIN;
+                for(int index=i;index<=j;index++){
+
+                    int cost = nums[i-1]*nums[index]*nums[j+1]
+                        + dp[i][index-1] + dp[index+1][j];
+                    maxi = max(maxi,cost);            
+                }
+
+                dp[i][j] = maxi;      
+                
+            }
+        }        
+        return dp[1][n];
         
     }
     
