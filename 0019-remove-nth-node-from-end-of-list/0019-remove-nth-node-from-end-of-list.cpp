bf1a8b3842fb879990774a232a4425701ad15d8b
Akshay Soni <akshaysoni460@gmail.com>
2023-05-13 20:57:20 +0530

    Time: 7 ms (39.44%), Space: 10.5 MB (96.61%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,32 +8,45 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+//         if(head->next==NULL and n==1) return NULL;
+        
+//         ListNode * ptr = head;
+//         ListNode * ptr2 = head;
+//         while(n){
+//             ptr2 = ptr2->next;
+//             n--;
+//         }
+//         ListNode * prev = ptr;
+        
+//         if(ptr2==NULL){
+//             head = head->next;
+//             return head;
+//         }
+        
+//         while(ptr2){
+//             ptr2 = ptr2->next;
+//             prev = ptr;
+//             ptr = ptr->next;
+//         }
+//         prev->next = ptr->next;
+//         return head;        
+        
+        
+//     }
+// };
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        
-        if(head->next==NULL and n==1) return NULL;
-        
-        ListNode * ptr = head;
-        ListNode * ptr2 = head;
-        while(n){
-            ptr2 = ptr2->next;
-            n--;
-        }
-        ListNode * prev = ptr;
-        
-        if(ptr2==NULL){
-            head = head->next;
-            return head;
-        }
-        while(ptr2){
-            ptr2 = ptr2->next;
-            prev = ptr;
-            ptr = ptr->next;
-        }
-        prev->next = ptr->next;
-        return head;        
-        
-        
+        ListNode *fast = head, *slow = head;
+        for (int i = 0; i < n; i++) fast = fast->next;
+        if (!fast) return head->next;
+        while (fast->next) fast = fast->next, slow = slow->next;
+        slow->next = slow->next->next;
+        return head;
     }
 };
