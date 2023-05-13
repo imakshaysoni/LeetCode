f699fbb7282643f5b41cb2a0938afe27a9ddafc8
Akshay Soni <akshaysoni460@gmail.com>
2023-05-13 21:15:12 +0530

    Time: 1065 ms (23.59%), Space: 294.7 MB (73.16%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,16 +11,37 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if(head==NULL || head->next==NULL) return NULL;
-        ListNode * slow = head;
-        ListNode * fast = head->next;
-        while(fast->next and fast->next->next){
+        // if(head==NULL || head->next==NULL) return NULL;
+        // ListNode * slow = head;
+        // ListNode * fast = head->next;
+        // while(fast->next and fast->next->next){
+        //     slow = slow->next;
+        //     fast = fast->next->next;
+        // }
+        // // ListNode * temp =slow->next;
+        // slow->next = slow->next->next;
+        // // delete temp;
+        // return head;
+        
+        // edge case conditions
+       if (head == NULL) {
+          return NULL;
+       }
+       if (head->next == NULL) {
+          delete head;
+          return NULL;
+       }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = head; // keeping this prev node to get the previous node of slow pointer when we reach middle
+        while(fast != NULL && fast->next != NULL)
+        {
+            if(slow != head) prev = prev->next; // since we want prev to start one step later than slow node
             slow = slow->next;
             fast = fast->next->next;
         }
-        // ListNode * temp =slow->next;
-        slow->next = slow->next->next;
-        // delete temp;
+        prev->next = slow->next;
+        delete slow;
         return head;
         
     }
