97bef13f603e8084589d17cdae153c9cf73491ab
Akshay Soni <akshaysoni460@gmail.com>
2023-05-13 11:04:26 +0530

    Time: 12 ms (43.35%), Space: 8 MB (77.16%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -10,15 +10,27 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         
-        unordered_set<ListNode*> s;
-        while (head!=NULL){
-            if(s.find(head)!=s.end()){
-                return true;
-            }
-            s.insert(head);
-            head = head->next;           
+        
+//         Hashing Method
+//         unordered_set<ListNode*> s;
+//         while (head!=NULL){
+//             if(s.find(head)!=s.end()){
+//                 return true;
+//             }
+//             s.insert(head);
+//             head = head->next;           
             
             
+//         }
+//         return false;
+        
+//         Slow Pointer /Fast Pointer mehtod( Tortoise Mehtod)
+        ListNode * slow = head;
+        ListNode * fast = head;
+        while(fast!=NULL and fast->next!=NULL){     
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast==slow) return true;      
         }
         return false;
         
