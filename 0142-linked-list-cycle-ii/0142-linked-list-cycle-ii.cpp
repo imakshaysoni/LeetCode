c325f36b7c863b862cc2289f50f865858a7a1d19
Akshay Soni <akshaysoni460@gmail.com>
2023-05-13 11:21:35 +0530

    Time: 44 ms (5.66%), Space: 7.7 MB (62.65%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -10,16 +10,38 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         //         Hashing Method
-        unordered_set<ListNode*> s;
-        while (head!=NULL){
-            if(s.find(head)!=s.end()){
-                return head;
-            }
-            s.insert(head);
-            head = head->next;           
+//         unordered_set<ListNode*> s;
+//         while (head!=NULL){
+//             if(s.find(head)!=s.end()){
+//                 return head;
+//             }
+//             s.insert(head);
+//             head = head->next;           
             
             
-        }
+//         }
+//         return NULL;
+        
+        if (head == NULL || head->next == NULL)
         return NULL;
+    
+    ListNode *slow  = head;
+    ListNode *fast  = head;
+    ListNode *entry = head;
+    
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {                      // there is a cycle
+            while(slow != entry) {               // found the entry location
+                cout<<slow->val<<" "<<entry->val<<endl;
+                slow  = slow->next;
+                cout<<slow->val<<endl;
+                entry = entry->next;
+            }
+            return entry;
+        }
+    }
+    return NULL;              
     }
 };
