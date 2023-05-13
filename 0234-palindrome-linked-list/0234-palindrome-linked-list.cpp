e2a3d1b331eb29770eb70ff5267323ea5de0311e
Akshay Soni <akshaysoni460@gmail.com>
2023-05-13 13:44:01 +0530

    Time: 284 ms (13.81%), Space: 117.9 MB (62.90%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -12,17 +12,51 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) {
 //         Brute Force, Store the values of linked list in str and check it is palindrome or not
-        string s="";
-        ListNode *curr = head;
-        while(curr){
-            s+=to_string(curr->val);
-            curr = curr->next;
+        // string s="";
+        // ListNode *curr = head;
+        // while(curr){
+        //     s+=to_string(curr->val);
+        //     curr = curr->next;
+        // }
+        // // cout<<s;
+        // int n=s.size();
+        // for(int i=0;i<n/2;i++){
+        //     if(s[i]!=s[n-i-1]) return false;
+        // }
+        // return true;
+        
+//         Find Middle & And Reverse 2nd Half and compare second half & first half
+        ListNode * slow = head;
+        ListNode * fast = head;
+        
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        cout<<s;
-        int n=s.size();
-        for(int i=0;i<n/2;i++){
-            if(s[i]!=s[n-i-1]) return false;
+//         Slow is now the 1st middle
+        ListNode * head2 = reverseLinkList(slow->next);
+        slow->next = head2;
+        slow = slow->next;
+        ListNode * dummy = head;
+        while(slow){
+            if(dummy->val != slow->val) return false;
+            slow = slow->next;
+            dummy = dummy->next;
         }
         return true;
+        
+        
+    }
+    
+    ListNode * reverseLinkList(ListNode *head){
+        ListNode * prev = NULL;
+        while(head){
+            ListNode * temp = head->next;
+            head->next = prev;
+            prev = head;
+            head = temp;          
+        }
+        return prev;
+        
     }
 };
