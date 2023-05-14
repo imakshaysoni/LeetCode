d4af16b3069f33d4d5c40e51a7e87a08746bb02f
Akshay Soni <akshaysoni460@gmail.com>
2023-05-14 21:45:29 +0530

    Time: 11 ms (34.95%), Space: 11.6 MB (84.52%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -13,29 +13,56 @@ public:
     ListNode* rotateRight(ListNode* head, int k) {
         if(head==NULL || head->next==NULL || k==0) return head;
         ListNode * temp = head;
-        int count=0;
-        while(temp){
+        int count=1;
+        while(temp->next){
             count++;
             temp = temp->next;
         }
-        if(k>count){
+        cout<<count;
+        if(k>=count){
             k=k%count;
             if(k==0) return head;
         }
+        temp->next = head;
+        int n = count-k;
         ListNode * first = head;
-        ListNode * second = head;
-        for(int i=0;i<k;i++){
-            second = second->next;
-            if(second==NULL) return head;
-        }
-        // cout<<second->val;
-        while(second->next){
-            second=second->next;
+        for(int i=1;i<n;i++){
             first = first->next;
         }
-        ListNode * newHead = first->next;
+        head = first->next;
         first->next = NULL;
-        second->next = head;
-        return newHead;
+        return head;
+        
+        
+        
+        
+        
+//         Mylogic 99% correct and working well, Just got one more optimization
+        // if(head==NULL || head->next==NULL || k==0) return head;
+        // ListNode * temp = head;
+        // int count=0;
+        // while(temp){
+        //     count++;
+        //     temp = temp->next;
+        // }
+        // if(k>count){
+        //     k=k%count;
+        //     if(k==0) return head;
+        // }
+        // ListNode * first = head;
+        // ListNode * second = head;
+        // for(int i=0;i<k;i++){
+        //     second = second->next;
+        //     if(second==NULL) return head;
+        // }
+        // // cout<<second->val;
+        // while(second->next){
+        //     second=second->next;
+        //     first = first->next;
+        // }
+        // ListNode * newHead = first->next;
+        // first->next = NULL;
+        // second->next = head;
+        // return newHead;
     }
 };
