4e9a0033e1a174f23ea5618d20e9ae3ceb8e3bfd
Akshay Soni <akshaysoni460@gmail.com>
2023-05-15 13:14:24 +0530

    Time: 3 ms (97.79%), Space: 11.3 MB (67.92%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,23 +17,53 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> m;
+//       O(1) Spcae Complexity logic, TC O(N)
+        // Node * dummy = new Node(-1);
         Node * temp = head;
-        Node * dummy = new Node(-1);
         while(temp){
-            Node * n = new Node(temp->val);
-            m[temp] = n;
-            temp = temp->next;
+            Node * node = new Node(temp->val);
+            node->next = temp->next;
+            temp->next = node;
+            temp = node->next;
         }
-        
         temp = head;
         while(temp){
-            m[temp]->next = m[temp->next];
-            m[temp]->random = m[temp->random];
+            if(temp->random==NULL) temp->next->random=NULL;
+            else temp->next->random = temp->random->next;
+            temp = temp->next->next;
+        }
+        temp = head;
+        Node * dummy = new Node(-1);
+        Node * newHead = dummy;
+        dummy->next = temp;
+        while(temp){
+            dummy->next = temp->next;
+            temp->next = temp->next->next;
             temp = temp->next;
+            dummy = dummy->next;            
         }
         
-        return m[head];
+        return newHead->next;
+        
+        
+// //         Brute Force (Stored Nodes in Map & then put its references) O(N) Spcae Complexity logic, TC O(N)
+//         unordered_map<Node*, Node*> m;
+//         Node * temp = head;
+//         Node * dummy = new Node(-1);
+//         while(temp){
+//             Node * n = new Node(temp->val);
+//             m[temp] = n;
+//             temp = temp->next;
+//         }
+        
+//         temp = head;
+//         while(temp){
+//             m[temp]->next = m[temp->next];
+//             m[temp]->random = m[temp->random];
+//             temp = temp->next;
+//         }
+        
+//         return m[head];
         
         
         
