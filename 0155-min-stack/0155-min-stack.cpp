c6738797da7a076e79207e6ec3413f969d5f1175
Akshay Soni <akshaysoni460@gmail.com>
2023-05-16 19:38:53 +0530

    Time: 26 ms (40.31%), Space: 16.2 MB (98.57%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,25 +1,42 @@
 class MinStack {
-private:
-    vector<pair<int,int>> st;
+    vector<long long> st;
+    long long mini = INT_MAX;
 public:
     MinStack() {
     }
     
-    void push(int val) {
-        if(st.empty()) st.push_back({val,val});
-        else st.push_back({val,min(val, st.back().second)});
+    void push(int val) {    
+        if(st.empty()){
+            st.push_back(val);
+            mini = val;
+        }
+        else{
+            if(val < mini){
+                st.push_back(1LL*2*val - mini);
+                mini = val;
+            }
+            else st.push_back(val);
+        }
+        
     }
     
     void pop() {
+        if(st.empty()) return;
+        long t = st.back();
         st.pop_back();
+        if(t<mini){
+            mini = 2*mini*1LL - t;
+        }
     }
     
     int top() {
-        return st.back().first;
+        long t1 = st.back();
+        if(t1 < mini) return mini;
+        return t1;
     }
     
     int getMin() {
-        return st.back().second;
+        return mini;
     }
 };
 
@@ -31,3 +48,31 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+
+// TC : O(1), SC : O(2N) Used vector of pair
+
+// class MinStack {
+// private:
+//     vector<pair<int,int>> st;
+// public:
+//     MinStack() {
+//     }
+    
+//     void push(int val) {
+//         if(st.empty()) st.push_back({val,val});
+//         else st.push_back({val,min(val, st.back().second)});
+//     }
+    
+//     void pop() {
+//         st.pop_back();
+//     }
+    
+//     int top() {
+//         return st.back().first;
+//     }
+    
+//     int getMin() {
+//         return st.back().second;
+//     }
+// };
