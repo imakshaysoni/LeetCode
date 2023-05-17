f4c97ef7561397938d0a79fd4f1f60af322cdaa3
Akshay Soni <akshaysoni460@gmail.com>
2023-05-17 23:27:06 +0530

    Time: 10 ms (82.31%), Space: 8.6 MB (85.75%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,33 +1,61 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        if(num.size()==k) return "0";
-        stack<int> st;
-        string s;
-        int i=0;
-        for(char ch: num){
-            
-            while(!st.empty() and k>0 and st.top() > ch-'0'){
-                st.pop();
+        
+        
+        
+        
+        string ans="";
+        for(char &c:num)
+        {
+            while(ans.size() && ans.back()>c &&k)
+            {
+                ans.pop_back();
                 k--;
             }
-            st.push(ch-'0');
-            if(st.size()==1 and st.top()==0) st.pop();
-            
+            if(ans.size()||c!='0')ans.push_back(c);
         }
+        while(ans.size()&&k--)           //<-------this look is to make sure we fulfil the deletion condition
+        {
+            ans.pop_back();
+        }
+        return (ans=="")?"0":ans;
         
-    while(k and !st.empty()){
         
-    st.pop(); k--;
-    }
-    if(st.empty()) return "0";
-    while(!st.empty()){
-        s += to_string(st.top());
-        st.pop();
-    }
         
-        reverse(s.begin(),s.end());
-        return s;
+        
+        
+        
+        
+        
+        
+//         if(num.size()==k) return "0";
+//         stack<int> st;
+//         string s;
+//         int i=0;
+//         for(char ch: num){
+            
+//             while(!st.empty() and k>0 and st.top() > ch-'0'){
+//                 st.pop();
+//                 k--;
+//             }
+//             st.push(ch-'0');
+//             if(st.size()==1 and st.top()==0) st.pop();
+            
+//         }
+        
+//     while(k and !st.empty()){
+        
+//     st.pop(); k--;
+//     }
+//     if(st.empty()) return "0";
+//     while(!st.empty()){
+//         s += to_string(st.top());
+//         st.pop();
+//     }
+        
+//         reverse(s.begin(),s.end());
+//         return s;
         
     }
 };
