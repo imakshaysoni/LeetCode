c3f0ee7a20809e7489629e923ea57eae1579a273
Akshay Soni <akshaysoni460@gmail.com>
2023-05-17 12:12:52 +0530

    Time: 20 ms (37.44%), Space: 19.7 MB (82.77%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,29 +1,73 @@
 class Solution {
 public:
     int trap(vector<int>& h) {
+        
         int n = h.size();
-        int leftMax = 0;
-        int rightMax = 0;
+        int l=0;
+        int r=n-1;
+        int lmax=0,rmax=0,res=0;
         
-        vector<int> left;
-        vector<int> right;
-        
-        for(int i=0;i<n;i++){
-            leftMax = max(leftMax, h[i]);
-            left.push_back(leftMax);
+        while(l<r){            
+            if(h[l]<h[r]){
+              
+                if(lmax > h[l])
+                {
+                    res += lmax-h[l];
+                }
+                else
+                {
+                    lmax = h[l];
+                }                
+                
+                l++;
+                
+            }
+            else{
+                
+                if(rmax > h[r])
+                {
+                    res += rmax-h[r];
+                }
+                else
+                    rmax = h[r];
+                
+                r--;
+            }
         }
-        for(int i=n-1;i>=0;i--){
-            rightMax = max(rightMax, h[i]);
-            right.push_back(rightMax);
-        }        
-        
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            ans += min(right[n-i-1], left[i]) - h[i];
-        }
-        
-        
-        return ans;
+        return res;
         
     }
 };
+
+
+
+//  Complexity TC: O(N) and SC: O(N+N)
+// class Solution {
+// public:
+//     int trap(vector<int>& h) {
+//         int n = h.size();
+//         int leftMax = 0;
+//         int rightMax = 0;
+        
+//         vector<int> left;
+//         vector<int> right;
+        
+//         for(int i=0;i<n;i++){
+//             leftMax = max(leftMax, h[i]);
+//             left.push_back(leftMax);
+//         }
+//         for(int i=n-1;i>=0;i--){
+//             rightMax = max(rightMax, h[i]);
+//             right.push_back(rightMax);
+//         }        
+        
+//         int ans = 0;
+//         for(int i=0;i<n;i++){
+//             ans += min(right[n-i-1], left[i]) - h[i];
+//         }
+        
+        
+//         return ans;
+        
+//     }
+// };
