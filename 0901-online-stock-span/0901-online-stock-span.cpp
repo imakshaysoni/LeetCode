2c75b20002bc63c5df220774cf605603d77e7f5f
Akshay Soni <akshaysoni460@gmail.com>
2023-05-17 19:24:04 +0530

    Time: 259 ms (11.34%), Space: 84.2 MB (68.69%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,20 +1,32 @@
 class StockSpanner {
     
         vector<int> arr;
+    stack<pair<int,int>> st;
 public:
     StockSpanner() {
         stack<int> s;
     }
     
-    int next(int price) {
-        arr.push_back(price);
-        int count=0;
-        for(int i=arr.size()-1;i>=0;i--){
-            if(arr[i] <= arr[arr.size()-1]) count++;
-            else break;
-        }
-        return count;
+//     int next(int price) {
+//         arr.push_back(price);
+//         int count=0;
+//         for(int i=arr.size()-1;i>=0;i--){
+//             if(arr[i] <= arr[arr.size()-1]) count++;
+//             else break;
+//         }
+//         return count;
         
+        
+//     }
+    int next(int price){
+        int span = 1;
+        while(!st.empty() and st.top().first <= price)
+        {
+            span += st.top().second;
+            st.pop();   
+        }
+        st.push({price, span});
+        return span;
         
     }
 };
