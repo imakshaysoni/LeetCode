ff2887ae8a72b6ca22e6250caaa4085414ebba7c
Akshay Soni <akshaysoni460@gmail.com>
2023-05-18 10:23:50 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -13,33 +13,69 @@ class Solution
     //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& M, int n) 
     {
-        // code here 
-        int row = M.size();
-        int col = M[0].size();
-        vector<int> poc;
-        // Find the person who might be celebrity
-        for(int i=0;i<row;i++){
-            int j=0;
-            while(j<col){
-                if(M[i][j]!=0) break;
-                j++;
-            }
-            if(j==col) poc.push_back(i);
-        }
-        if(poc.size()==row or poc.size()==0) return -1;
-        // for(int a: poc) cout<<a<<" ";
-        // if(poc.size()==row) return -1;
-        for(int x: poc){
-            int i=0;
-            for(;i<row;i++){
-                if(i==x) continue;
-                if(M[i][x]==0) break;
-            }
-            // cout<<i<<"->"<<row<<endl;
-            if(i==row) return x;
+        // Using Stack TC: O(N) SC:O(N)
+        
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            st.push(i);
         }
         
+        while(st.size()!=1){
+            int a = st.top(); st.pop();
+            int b = st.top(); st.pop();
+            if(M[a][b]==1) st.push(b);
+            else st.push(a);
+        }
+        
+        // while(!st.empty()){
+        //     cout<<st.top()<<" ";
+        //     st.pop();
+        // }
+        int x = st.top();
+        bool rowCheck=true;
+        for(int i=0;i<M.size();i++){
+            if(M[i][x]==0 and i!=x) {
+                rowCheck=false; break;
+                
+            }
+        }
+        bool colCheck=true;
+        for(int j=0;j<M[0].size();j++){
+            if(M[x][j]==1){
+                colCheck=false; break;
+            }
+        }
+        if(colCheck and rowCheck) return x;
         return -1;
+        
+        
+        // // Slef Brute Force TC: O(N) SC: O(N)
+        // int row = M.size();
+        // int col = M[0].size();
+        // vector<int> poc;
+        // // Find the person who might be celebrity
+        // for(int i=0;i<row;i++){
+        //     int j=0;
+        //     while(j<col){
+        //         if(M[i][j]!=0) break;
+        //         j++;
+        //     }
+        //     if(j==col) poc.push_back(i);
+        // }
+        // if(poc.size()==row or poc.size()==0) return -1;
+        // // for(int a: poc) cout<<a<<" ";
+        // // if(poc.size()==row) return -1;
+        // for(int x: poc){
+        //     int i=0;
+        //     for(;i<row;i++){
+        //         if(i==x) continue;
+        //         if(M[i][x]==0) break;
+        //     }
+        //     // cout<<i<<"->"<<row<<endl;
+        //     if(i==row) return x;
+        // }
+        
+        // return -1;
         
         
     }
