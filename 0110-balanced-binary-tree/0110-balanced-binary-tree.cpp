eed9b8843c82d9f0b6bcd41f1d61a450ed0b7084
Akshay Soni <akshaysoni460@gmail.com>
2023-05-24 00:30:35 +0530

    Time: 19 ms (26.23%), Space: 20.8 MB (79.61%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -14,7 +14,7 @@ public:
     bool isBalanced(TreeNode* root) {
         int nodeCount = 1;
         int height = findHeightOfBT(root);
-        if(height==INT_MAX) return false;
+        if(height==-1) return false;
         return true;
     }
     
@@ -23,9 +23,9 @@ public:
         if(root==NULL) return 0;
         int op1 = findHeightOfBT(root->left);
         int op2 = findHeightOfBT(root->right);
-        if(abs(op1-op2) > 1) return INT_MAX; 
+        if(abs(op1-op2) > 1) return -1; 
         
-        if(op1==INT_MAX || op2==INT_MAX) return INT_MAX;
+        if(op1==-1 || op2==-1) return -1;
         return 1 + max(op1,op2);
         
     }
