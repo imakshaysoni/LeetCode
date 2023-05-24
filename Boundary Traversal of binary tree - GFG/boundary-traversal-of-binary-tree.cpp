10f49d5beff5303dcd22950defd604f50756dffe
Akshay Soni <akshaysoni460@gmail.com>
2023-05-24 22:12:02 +0530

    Added solution - LeetHub

---
+++ Boundary Traversal of binary tree - GFG/boundary-traversal-of-binary-tree.cpp
@@ -0,0 +1,197 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+
+
+
+
+
+
+
+// } Driver Code Ends
+/* A binary tree Node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+class Solution {
+public:
+    vector <int> boundary(Node *root)
+    {
+        //Your code here
+        vector<int> result;
+        if(root==NULL) return result;
+        if(isLeafNode(root)) {
+            result.push_back(root->data);
+            return result;
+        }
+        result.push_back(root->data);
+        
+        boundaryTraversal(root, result);
+        return result;
+    }
+    
+    bool isLeafNode(Node * root){
+        if(root->left || root->right) return false;
+        return true;
+    }
+    
+    void addLeftBoundaryNodes(Node *root, vector<int> &result){
+        Node * curr = root->left;
+        while(curr and !isLeafNode(curr)){
+            result.push_back(curr->data);
+            if(curr->left) curr=curr->left;
+            else if(curr->right) curr=curr->right;
+        }
+        
+    }
+    
+    void addLeafNodes(Node * root, vector<int> &result){
+        Node * curr = root;
+        if(curr and isLeafNode(curr)){
+            result.push_back(curr->data);
+            return;
+        }
+        if(curr->left) addLeafNodes(curr->left, result);
+        if(curr->right) addLeafNodes(curr->right, result);
+        
+    }
+    
+    void addRightBoundaryNodes(Node * root, vector<int> &result){
+        Node * curr=root->right;
+        stack<int> st;
+        while(curr and !isLeafNode(curr)){
+            st.push(curr->data);
+            if(curr->right) curr= curr->right;
+            else if (curr->left) curr=curr->left;
+        }
+        
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+        
+    }
+    
+    void boundaryTraversal(Node* root, vector<int> &result){
+        
+        addLeftBoundaryNodes(root, result);
+        addLeafNodes(root, result);
+        addRightBoundaryNodes(root, result);
+        
+    }
+    
+    
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        vector <int> res = ob.boundary(root);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
