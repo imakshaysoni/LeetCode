4c6dd887769d89432b6b3b5c4e84a2353dd67992
Akshay Soni <akshaysoni460@gmail.com>
2023-05-25 22:52:21 +0530

    Time: 5 ms (42.76%), Space: 12.1 MB (41.48%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,21 +11,52 @@
  */
 class Solution {
 public:
+//     bard Optimized
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> result;
-        if(root==NULL) return result;
-//         Recurssive Solution TC:O(N), SC(H) Height Of Tree, max N when we get squeed tree
+  vector<int> result;
+  if (root == NULL) {
+    return result;
+  }
+
+  // Level order traversal using a deque
+  std::deque<TreeNode*> q;
+  q.push_back(root);
+  while (!q.empty()) {
+    int size = q.size();
+    for (int i = 0; i < size; i++) {
+      TreeNode* temp = q.front();
+      q.pop_front();
+      if (i == 0) {
+        result.push_back(temp->val);
+      }
+      if (temp->right != NULL) {
+        q.push_back(temp->right);
+      }
+      if (temp->left != NULL) {
+        q.push_back(temp->left);
+      }
+    }
+  }
+
+  return result;
+}
+    
+    
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int> result;
+//         if(root==NULL) return result;
+// //         Recurssive Solution TC:O(N), SC(H) Height Of Tree, max N when we get squeed tree
         
-//         rightView
-        rightView(root, result, 0);
-        return result;
+// //         rightView
+//         rightView(root, result, 0);
+//         return result;
         
-//         LeftView
-        leftView(root, result, 0);
-        return result;
+// //         LeftView
+//         leftView(root, result, 0);
+//         return result;
         
         
-//         Level Order Traversing TC O(N) SC O(N)
+//         // Level Order Traversing TC O(N) SC O(N)
 //         queue<TreeNode*> q;        
 //         q.push(root);
 //         while(!q.empty()){
@@ -42,23 +73,23 @@ public:
         
 //         return result;
         
-    }
+//     }
     
-    void rightView(TreeNode* root, vector<int> &res, int level){
-        if(root==NULL) return;
+//     void rightView(TreeNode* root, vector<int> &res, int level){
+//         if(root==NULL) return;
         
-        if(res.size()==level) res.push_back(root->val);
-        // rightView =right then Left
-        rightView(root->right, res, level+1);
-        rightView(root->left, res, level+1);
-    }
+//         if(res.size()==level) res.push_back(root->val);
+//         // rightView =right then Left
+//         rightView(root->right, res, level+1);
+//         rightView(root->left, res, level+1);
+//     }
     
-    void leftView(TreeNode* root, vector<int> &res, int level){
-        if(root==NULL) return;
+//     void leftView(TreeNode* root, vector<int> &res, int level){
+//         if(root==NULL) return;
         
-        if(res.size()==level) res.push_back(root->val);
-        // leftView =Left then Right
-        leftView(root->left, res, level+1);
-        leftView(root->right, res, level+1);
-    }
+//         if(res.size()==level) res.push_back(root->val);
+//         // leftView =Left then Right
+//         leftView(root->left, res, level+1);
+//         leftView(root->right, res, level+1);
+//     }
 };
