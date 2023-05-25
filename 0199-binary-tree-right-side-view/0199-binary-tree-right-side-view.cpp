937fb8debd76e982a8dda8a2f6117972a487f550
Akshay Soni <akshaysoni460@gmail.com>
2023-05-25 15:21:55 +0530

    Time: 5 ms (42.76%), Space: 11.8 MB (86.96%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -14,22 +14,51 @@ public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> result;
         if(root==NULL) return result;
+//         Recurssive Solution TC:O(N), SC(H) Height Of Tree, max N when we get squeed tree
         
-        queue<TreeNode*> q;        
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size();
-            for(int i=0;i<size;i++){                
-                TreeNode * temp = q.front();
-                q.pop();
-                if(temp->right) q.push(temp->right);
-                if(temp->left) q.push(temp->left);
-                if(i==0)
-                    result.push_back(temp->val);
-            }
-        }
-        
+//         rightView
+        rightView(root, result, 0);
         return result;
         
+//         LeftView
+        leftView(root, result, 0);
+        return result;
+        
+        
+//         Level Order Traversing TC O(N) SC O(N)
+//         queue<TreeNode*> q;        
+//         q.push(root);
+//         while(!q.empty()){
+//             int size = q.size();
+//             for(int i=0;i<size;i++){                
+//                 TreeNode * temp = q.front();
+//                 q.pop();
+//                 if(temp->right) q.push(temp->right);
+//                 if(temp->left) q.push(temp->left);
+//                 if(i==0)
+//                     result.push_back(temp->val);
+//             }
+//         }
+        
+//         return result;
+        
+    }
+    
+    void rightView(TreeNode* root, vector<int> &res, int level){
+        if(root==NULL) return;
+        
+        if(res.size()==level) res.push_back(root->val);
+        // rightView =right then Left
+        rightView(root->right, res, level+1);
+        rightView(root->left, res, level+1);
+    }
+    
+    void leftView(TreeNode* root, vector<int> &res, int level){
+        if(root==NULL) return;
+        
+        if(res.size()==level) res.push_back(root->val);
+        // leftView =Left then Right
+        leftView(root->left, res, level+1);
+        leftView(root->right, res, level+1);
     }
 };
