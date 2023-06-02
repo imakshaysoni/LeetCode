b59a67d776d9793ad83b3be1b7db30ad730d3d0e
Akshay Soni <akshaysoni460@gmail.com>
2023-06-02 20:39:14 +0530

    Time: 29 ms (9.69%), Space: 24.2 MB (55.76%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,23 +11,45 @@
  */
 class Solution {
 public:
+    int ans=-1;
     int kthSmallest(TreeNode* root, int k) {
         
-        stack<TreeNode*> st;
-        TreeNode * curr = root;
-        while(curr || !st.empty())
-        {            
-            while(curr){
-                st.push(curr);
-                curr = curr->left;  
-            }            
-            curr = st.top();
-            st.pop();
-            k--;
-            if(k==0) return curr->val;
-            curr = curr->right;               
+        int cnt=0;
+        inorder(root, k, cnt);
+        return ans; 
+        
+        // Stack Based solution
+        // stack<TreeNode*> st;
+        // TreeNode * curr = root;
+        // while(curr || !st.empty())
+        // {            
+        //     while(curr){
+        //         st.push(curr);
+        //         curr = curr->left;  
+        //     }            
+        //     curr = st.top();
+        //     st.pop();
+        //     k--;
+        //     if(k==0) return curr->val;
+        //     curr = curr->right;               
+        // }
+        // return -1;
+        
+    }
+    
+    void inorder(TreeNode* root, int k, int &count){
+        
+        if(!root) return;       
+        inorder(root->left, k, count);
+   
+        // Process the current node
+        count++;
+        if (count == k) {
+            ans = root->val; // Found the kth smallest element
+            return;
         }
-        return -1;
+        
+        inorder(root->right, k, count);
         
     }
 };
