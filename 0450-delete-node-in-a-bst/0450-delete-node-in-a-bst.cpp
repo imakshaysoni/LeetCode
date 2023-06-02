e87d6c800640d47f9c4654a06a72cb45f88ce0b5
Akshay Soni <akshaysoni460@gmail.com>
2023-06-02 17:22:07 +0530

    Time: 35 ms (77.42%), Space: 32.6 MB (66.29%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -1,37 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if (root == nullptr) {
-            return nullptr;
-        }
         
-        if (key < root->val) {
-            root->left = deleteNode(root->left, key);
-        } else if (key > root->val) {
-            root->right = deleteNode(root->right, key);
-        } else {
-            if (root->left == nullptr) {
-                TreeNode* temp = root->right;
-                delete root;
-                return temp;
-            } else if (root->right == nullptr) {
-                TreeNode* temp = root->left;
-                delete root;
-                return temp;
+        if(!root) return nullptr;
+        
+        if(root->val==key) return helper(root);
+        
+        TreeNode* curr = root;
+        while(curr){
+            if(curr->val > key){
+                if(curr->left and curr->left->val==key){
+                    curr->left = helper(curr->left);
+                    break;
+                }
+                else{
+                    curr=curr->left;
+                }
             }
-            
-            TreeNode* successor = findMin(root->right);
-            root->val = successor->val;
-            root->right = deleteNode(root->right, successor->val);
-        }
-        
+            else{
+                if(curr->right and curr->right->val==key){
+                    curr->right = helper(curr->right);
+                    break;
+                }
+                else curr = curr->right;
+            }           
+        }        
         return root;
+        
     }
     
-    TreeNode* findMin(TreeNode* node) {
-        while (node->left) {
-            node = node->left;
-        }
-        return node;
+    TreeNode* helper(TreeNode* root){
+        if(root->left==NULL) return root->right;
+        if(root->right==NULL) return root->left;
+        
+        TreeNode * rightMost = findRightMostNode(root->left);
+        rightMost->right = root->right;
+        return root->left;
+    }
+    TreeNode* findRightMostNode(TreeNode * root){
+        while(root->right) root=root->right;
+        return root;
     }
 };
