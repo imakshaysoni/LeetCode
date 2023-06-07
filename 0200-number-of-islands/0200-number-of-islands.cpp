79350bb2f8df7c18eb3259989c97abd02cfbe6cd
Akshay Soni <akshaysoni460@gmail.com>
2023-06-07 23:01:40 +0530

    Time: 44 ms (41.77%), Space: 12.1 MB (96.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -19,10 +19,13 @@ public:
     void dfs(vector<vector<char>> &grid, int i, int j){
         if(i>=grid.size() || j>=grid[0].size() || i<0 || j<0 || grid[i][j]=='0' || grid[i][j]=='#') return;
         grid[i][j]='#';
-        vector<int> dx = {0, 0, 1, -1, 1, -1, 1, -1};
-        vector<int> dy = {1, -1, 0, 0, 1, -1, -1, 1};
-        for(int index=0;index<4;index++){
-            dfs(grid, i+dx[index], j+dy[index]);
-        }        
+        // vector<int> dx = {0, 0, 1, -1, 1, -1, 1, -1};
+        // vector<int> dy = {1, -1, 0, 0, 1, -1, -1, 1};
+        for(int delrow=-1;delrow<=1;delrow++){         
+            for(int delcol=-1;delcol<=1;delcol++){
+                if(abs(delcol)==abs(delrow)) continue; // Ingoring diagonals
+                dfs(grid, i+delrow, j+delcol);
+            }      
+        }     
     }
 };
