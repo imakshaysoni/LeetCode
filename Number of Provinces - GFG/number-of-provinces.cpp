b4cafec6b42a22a573108dde295c4d00a78f1572
Akshay Soni <akshaysoni460@gmail.com>
2023-06-08 11:13:43 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -38,6 +38,7 @@ class Solution {
         q.push(node);
         while(!q.empty()){
             int n = q.front();
+            vis[n]=1;
             q.pop();
             for(auto it: adj[n]){
                 if(!vis[it]){
