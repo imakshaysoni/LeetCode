2c48aaeef24b3c5a499813bf9c40c48d3f2b2b26
Akshay Soni <akshaysoni460@gmail.com>
2023-06-09 12:08:01 +0530

    Time: 13 ms (32.64%), Space: 14.2 MB (25.09%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -2,10 +2,38 @@ class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
        
-        int base_color = image[sr][sc];
-        helper(image, sr,sc, color, base_color);
-        return image;
+        // int base_color = image[sr][sc];
+        // helper(image, sr,sc, color, base_color);
+        // return image;
+        // BFS Approch
+        vector<vector<int>> result = image;
+        bfs(result, sr, sc, color);
+        return result;
         
+    }
+    
+     void bfs(vector<vector<int>>& grid, int i, int j, int newColor) {
+        int color = grid[i][j];
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        while(!q.empty()){
+            auto it = q.front(); q.pop();
+            vector<int> dx = {-1, 0, 1, 0};
+            vector<int> dy = {0, 1, 0, -1};
+            
+            grid[it.first][it.second]=newColor;
+            
+            for(int index=0; index<4; index++){
+                int nrow = it.first + dx[index];
+                int mcol = it.second + dy[index];
+                if(nrow<0 || mcol<0 || nrow>=grid.size() || mcol>=grid[0].size() || grid[nrow][mcol]==newColor)
+                    continue;
+                if(grid[nrow][mcol]==color){
+                    grid[nrow][mcol] = newColor;
+                    q.push({nrow, mcol});
+                }
+            }
+        }
     }
     
     
