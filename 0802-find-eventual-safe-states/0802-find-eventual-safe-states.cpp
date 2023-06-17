b5a95970e31825c2b5185102f22b74543371d047
Akshay Soni <akshaysoni460@gmail.com>
2023-06-17 16:12:42 +0530

    Time: 237 ms (22.56%), Space: 58.7 MB (46.76%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -2,49 +2,81 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         
+//         BFS Approch using topo sort
         
         int n = graph.size();
-        vector<int> visi(n,0);
-        vector<int> path(n,0);
-        vector<int> check(n,0);
-        vector<int> safeNodes;
+        vector<int> indegree(n,0);
+        vector<int> topo;
+        vector<int> adj[n];
         for(int i=0;i<n;i++){
-            if(visi[i]==0){
-                dfs(graph, i, visi, path, check);
+            for(int x: graph[i]){
+                adj[x].push_back(i);
+                indegree[i]++;
             }
         }
         
+        queue<int> q;
         for(int i=0;i<n;i++){
-            if(check[i]==1) safeNodes.push_back(i);
+            if(indegree[i]==0) q.push(i);
         }
         
-        return safeNodes;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(int x: adj[node]){
+                indegree[x]--;
+                if(indegree[x]==0) q.push(x);
+            }
+        }
+        sort(topo.begin(), topo.end());
+        return topo;
+    }  
         
-    }
+        
+//         dfs Approch
+//         int n = graph.size();
+//         vector<int> visi(n,0);
+//         vector<int> path(n,0);
+//         vector<int> check(n,0);
+//         vector<int> safeNodes;
+//         for(int i=0;i<n;i++){
+//             if(visi[i]==0){
+//                 dfs(graph, i, visi, path, check);
+//             }
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             if(check[i]==1) safeNodes.push_back(i);
+//         }
+        
+//         return safeNodes;
+        
+//     }
     
     
-    bool dfs(vector<vector<int>> & graph, int node, vector<int> &visi, vector<int> &path, vector<int> & check){
+//     bool dfs(vector<vector<int>> & graph, int node, vector<int> &visi, vector<int> &path, vector<int> & check){
         
-        visi[node]=1;
-        path[node]=1;
+//         visi[node]=1;
+//         path[node]=1;
         
-        for(int n: graph[node]){
+//         for(int n: graph[node]){
             
-            if(visi[n]==1 and path[n]==1){
-                return false;
-            }
+//             if(visi[n]==1 and path[n]==1){
+//                 return false;
+//             }
             
-            if(visi[n]==0){
-                if(dfs(graph, n, visi, path, check)==false){
-                    return false;
-                }
-            }
+//             if(visi[n]==0){
+//                 if(dfs(graph, n, visi, path, check)==false){
+//                     return false;
+//                 }
+//             }
             
             
-        }        
-        check[node]=1;
-        path[node]=0;
-        return true;
+//         }        
+//         check[node]=1;
+//         path[node]=0;
+//         return true;
         
-    }
+//     }
 };
