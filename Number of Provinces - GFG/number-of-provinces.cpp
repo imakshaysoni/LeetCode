c136086c90c730c197ac56668203e0afad409dea
Akshay Soni <akshaysoni460@gmail.com>
2023-06-22 01:12:05 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -6,10 +6,74 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
+class DisjointSet
+{
+private:
+	vector<int> parent, rank, size;
+
+public:
+	// Constructor to initialize the disjoint set with size n
+	DisjointSet(int n){
+		rank.resize(n+1, 0);
+		parent.resize(n+1);
+		size.resize(n+1, 1);
+		for(int i=0;i<=n;i++) parent[i]=i;
+	}
+
+	// Find the representative (root) of the set to which node belongs (with path compression)
+	int findUp(int node){
+		if(node == parent[node]) return node;
+		
+		// Path compression by updating the parent of the node
+		return parent[node] = findUp(parent[node]);
+	}
+
+	// Union two sets based on rank
+	int unionByRank(int u, int v){
+		if(parent[u] == parent[v]) return 0;
+
+		int usp = findUp(u); // Find representative of u's set
+		int vsp = findUp(v); // Find representative of v's set
+
+		if(rank[usp] > rank[vsp]){
+			parent[vsp] = usp; // Set v's representative's parent as u's representative
+		}
+		else if(rank[vsp] > rank[usp]){
+			parent[usp] = vsp; // Set u's representative's parent as v's representative
+		}
+		else{
+			parent[usp] = vsp; // Set u's representative's parent as v's representative
+			rank[vsp]++; // Increment the rank of v's representative
+		}
+		return 1;
+	}
+
+	// Union two sets based on size
+	void unionBySize(int u, int v){
+		if(parent[u] == parent[v]) return;
+
+		int usp = findUp(u); // Find representative of u's set
+		int vsp = findUp(v); // Find representative of v's set
+
+		if(size[usp] > size[vsp]){
+			parent[vsp] = usp; // Set v's representative's parent as u's representative
+			size[usp] += size[vsp]; // Update the size of u's set
+		}
+		else{
+			parent[usp] = vsp; // Set u's representative's parent as v's representative
+			size[vsp] += size[usp]; // Update the size of v's set
+		}
+	}
+};
+
 class Solution {
   public:
   int ans=0;
     int numProvinces(vector<vector<int>> isConnected, int n) {
+        
+        // Code Using DIsjointSet
+        return disJointSet(isConnected, n);
+        
         // code here
         vector<vector<int>> adj(n+1);
         vector<int> vis(n+1,0);
@@ -31,6 +95,28 @@ class Solution {
         }
         }
         return ans;
+    }
+    
+    int disJointSet(vector<vector<int>> isConnected, int n){
+        DisjointSet ds(n);
+        vector<vector<int>> adj(n+1);
+        vector<int> vis(n+1,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j and isConnected[i][j]==1){
+                    // adj[i].push_back(j);
+                    ds.unionBySize(i,j);
+                }
+            }
+        }
+        
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(ds.findUp(i) == i) count++;
+        }
+        
+        return count;
+        
     }
     
     void bfs(int node, vector<int> &vis, vector<vector<int>> &adj){
