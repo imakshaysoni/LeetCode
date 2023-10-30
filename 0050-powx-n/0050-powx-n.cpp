28d525e6a2d0667872fda6bc97f181e7d5d8ffef
Akshay Soni <akshaysoni460@gmail.com>
2023-10-30 21:10:16 +0530

    Time: 1 ms (42.65%), Space: 6.4 MB (43.44%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,30 +1,9 @@
-// class Solution {
-// public:
-//     double myPow(double x, int n) {
-//         vector<int> dp;
-        
-//         if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
-//         if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
-//         if (n< 0 ) {
-//             n = abs(n);
-//             x = 1/x;
-//         };
-//         return pow(x,n);    
-//     }
-    
-//     double pow(double x, int n){     
-//         if(n==0) return 1;
-//         double temp = pow(x,n/2);
-//         if(n%2==1) return x*temp*temp;
-//         return temp*temp;
-//     }
-// };
-
 class Solution {
 public:
     double myPow(double x, int n) {
+        
         if(n==0) return 1;
-        if(n<0) {
+        if(n<0){
             n = abs(n);
             x = 1/x;
         }
@@ -34,6 +13,6 @@ public:
         }
         else{
             return x*temp*temp;
-        }
+        }      
     }
 };
