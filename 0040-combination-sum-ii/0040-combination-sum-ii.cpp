8af8d210f924c29e3d1baa3f6d37e98a0f1c5a29
Akshay Soni <akshaysoni460@gmail.com>
2023-11-01 12:59:18 +0530

    Time: 10 ms (20.98%), Space: 11.3 MB (31.60%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
+    vector<vector<int>> result;
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         
-        
         sort(candidates.begin(), candidates.end());
-        vector<vector<int>> ans;
-        vector<int> subset;
-        helper(candidates,target,0,subset,ans);
-        return ans;
-        
+        vector<int> arr;
+        solve(candidates, target, arr, 0);
+        return result;
     }
-    
-    
-    void helper(vector<int> &c, int target, int i, vector<int> &subset, vector<vector<int>> &ans){
-                
-        
-        if(target==0){
-            ans.push_back(subset); return;
+    void solve(vector<int> &c, int target, vector<int> &arr, int index){
+        if(target==0) {
+            result.push_back(arr);
+            return;
         }
+        if(target<0 or index>=c.size()){ return; }
         
-        if(target<0) return;
-        
-        if(i>=c.size()) return;
-        
-        subset.push_back(c[i]);       
-        helper(c,target-c[i],i+1,subset,ans);
-        subset.pop_back();
-        
-        while(i<c.size()-1 && c[i]==c[i+1]){ i++; }
-        
-        helper(c,target,i+1,subset,ans);
+//         Select First
+        arr.push_back(c[index]);
+        solve(c, target-c[index], arr, index+1);
+        arr.pop_back();
+//             Not Select
+        while(index<c.size()-1 && c[index]==c[index+1]){ index++; }
+        solve(c, target, arr, index+1);
     }
+    
+    
 };
