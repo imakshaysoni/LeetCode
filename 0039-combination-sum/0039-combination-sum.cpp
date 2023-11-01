cdab8c3dc7c2ef2b8e88ba8db3cc57ea96eb8ebf
Akshay Soni <akshaysoni460@gmail.com>
2023-11-01 12:44:31 +0530

    Time: 3 ms (85.04%), Space: 11.4 MB (48.11%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
+    vector<vector<int>> result;
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
-        vector<vector<int>> ans;
-        vector<int> subSet;
-        helper(candidates, target, ans, subSet, 0);
-        return ans;
+        
+        vector<int> arr;
+        solve(candidates, target, arr, 0);
+        return result;
     }
-    
-    
-    void helper(vector<int> &c, int target, vector<vector<int>> &ans, vector<int> &subSet, int i){
-        
-        
-        if(target==0){
-            ans.push_back(subSet); return;
-        }
-        
-        
-         if(target < 0)
+    void solve(vector<int> &c, int target, vector<int> &arr, int index){
+        if(target==0) {
+            result.push_back(arr);
             return;
+        }
+        if(target<0 or index>=c.size()){ return; }
         
-         if(i >= c.size())
-            return;         
-        
-//         Include First Term again & again
-        subSet.push_back(c[i]);
-        helper(c,target-c[i],ans,subSet, i);
-            
-        subSet.pop_back();
-        helper(c, target, ans, subSet, i+1);
-        
-        
+//         Select First
+        arr.push_back(c[index]);
+        solve(c, target-c[index], arr, index);
+        arr.pop_back();
+//             Not Select
+        solve(c, target, arr, index+1);
     }
+    
+    
 };
