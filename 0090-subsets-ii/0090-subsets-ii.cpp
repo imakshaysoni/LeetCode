842526834cf43b881bff1b5380312f876b1d542f
Akshay Soni <akshaysoni460@gmail.com>
2023-11-03 14:04:18 +0530

    Time: 4 ms (54.24%), Space: 7.8 MB (70.74%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         vector<int> subset;
-        uniqueSubSets(nums,0, ans, subset);
-        return ans;    
-        
+        sort(nums.begin(), nums.end());
+        solve(nums, 0, ans, subset);
+        return ans;
     }
     
-    
-    void uniqueSubSets(vector<int> &v, int i, vector<vector<int>> &ans, vector<int> &subset){
-        
-        
-        // if(i>=v.size()){
-        //     ans.push_back(subset);
-        //     return;
-        // }
-        
-        ans.push_back(subset);
-        
-        for(int k=i;k<v.size();k++){
-            if(i == k || v[k]!=v[k-1])
-            {subset.push_back(v[k]);
-            uniqueSubSets(v,k+1,ans,subset);
-            subset.pop_back();}
-        }
-        
-        
-    }
-        
-    
+    void solve(vector<int> &num, int index, vector<vector<int>> &ans, vector<int> &subset){
+	if(index==num.size()){
+		ans.push_back(subset);
+		return;
+	}
+
+	// Pick First
+	subset.push_back(num[index]);
+	solve(num, index+1, ans, subset);
+	subset.pop_back();
+	// Not Picking
+    while(index<num.size()-1 and num[index]==num[index+1]) index++;
+	solve(num, index+1, ans, subset);
+
+}
+
 };
