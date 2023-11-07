6530c029b67f81e186619686c8d138838244db97
Akshay Soni <akshaysoni460@gmail.com>
2023-11-07 12:39:29 +0530

    Time: 2 ms (62.94%), Space: 7.9 MB (76.40%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-     
+        
         vector<vector<int>> ans;
-        vector<int> permuations;
-        solve(nums, 0, permuations, ans);
-        return ans;
+        
+        solve(nums, 0, ans);
+        return ans;      
+        
+        
     }
-    
-    
-    void solve(vector<int> nums, int index, vector<int> &p, vector<vector<int>> &ans){
+    void solve(vector<int> &nums, int index, vector<vector<int>> &ans){
+        
         
         if(index==nums.size()){
             ans.push_back(nums);
-            return;
-        }
+        } 
         
         for(int i=index;i<nums.size();i++){
             swap(nums[index], nums[i]);
-            solve(nums, index+1, p, ans);
+            solve(nums, index+1, ans);
             swap(nums[index], nums[i]);
-        }    
-        
-        
-        
+        }        
         
     }
 };
