e2c620f5ce2e7169c6f5a283a20f0a990a5d8ffe
Akshay Soni <akshaysoni460@gmail.com>
2023-11-11 18:21:04 +0530

    Time: 234 ms (24.31%), Space: 385.2 MB (5.58%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     bool canCross(vector<int>& stones) {
+        unordered_map<int,int>m;
+        for(int i=0;i<stones.size();i++){
+            m[stones[i]] = i;
+        }
         
         vector<vector<int>> dp(stones.size()+1, vector<int>(2001,-1));
-        return frog(stones, 0, 1, dp);
+        return frog(stones, 0, 1, dp, m);
     }
     
-    bool frog(vector<int>&stones, int index, int jump, vector<vector<int>> &dp){
+    bool frog(vector<int>&stones, int index, int jump, vector<vector<int>> &dp, unordered_map<int,int> &mapper){
         if(index==stones.size()-1) return true;
         if(index>=stones.size()) return false;     
         if(jump<=0) return false;
         
         if(dp[index][jump]!=-1) return dp[index][jump];
         
-        int new_index = getIdx(stones, index, stones[index]+jump);
+        // int new_index = getIdx(stones, index, stones[index]+jump); //O(N)
+        int new_index = getIdxMap(mapper, stones[index]+jump); //O(1)
         if(new_index==-1) return false;
         index=new_index;    
         
-        int op1 = frog(stones, index, jump, dp);
-        int op2 = frog(stones, index, jump + 1, dp );
-        int op3 = frog(stones, index, jump - 1, dp);
+        int op1 = frog(stones, index, jump, dp, mapper);
+        int op2 = frog(stones, index, jump + 1, dp, mapper );
+        int op3 = frog(stones, index, jump - 1, dp, mapper);
         dp[index][jump] = op1 || op2 ||op3;
         return dp[index][jump];
     }
@@ -28,6 +33,11 @@ public:
         for(int i=index;i<stones.size();i++){
             if(stones[i]==val) return i;
         }
+        return -1;
+    }
+    
+    int getIdxMap(unordered_map<int,int>&m, int val){
+        if(m.find(val)!=m.end()) return m[val];
         return -1;
     }
 };
