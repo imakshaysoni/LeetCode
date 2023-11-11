62c202272f45cc17ff49020d1e488b3d9cfccfe6
Akshay Soni <akshaysoni460@gmail.com>
2023-11-11 18:15:27 +0530

    Time: 723 ms (9.49%), Space: 382.5 MB (5.98%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
     bool canCross(vector<int>& stones) {
-    
         
-        
-        vector<vector<int>> dp(stones.size()+1, vector<int> (2001,-1));
-        return solve(stones, 0, 1, dp);
+        vector<vector<int>> dp(stones.size()+1, vector<int>(2001,-1));
+        return frog(stones, 0, 1, dp);
     }
     
-    
-    bool solve(vector<int> &s, int index, int jumps, vector<vector<int>> &dp){
+    bool frog(vector<int>&stones, int index, int jump, vector<vector<int>> &dp){
+        if(index==stones.size()-1) return true;
+        if(index>=stones.size()) return false;     
+        if(jump<=0) return false;
         
-        if(index==s.size()-1) return true;
+        if(dp[index][jump]!=-1) return dp[index][jump];
         
-        if(index<0 || index > s.size()) return false;
+        int new_index = getIdx(stones, index, stones[index]+jump);
+        if(new_index==-1) return false;
+        index=new_index;    
         
-        if(jumps <= 0) return false;
-        
-        if(dp[index][jumps]!=-1) return dp[index][jumps];
-        
-        int idx = getIdx(s, s[index]+jumps, index);
-        if(idx==-1) return dp[index][jumps] = false;
-        index = idx;
-        
-        
-        bool op1 = solve(s, index, jumps, dp);
-        bool op2 = solve(s, index, jumps-1, dp);
-        bool op3 = solve(s, index, jumps+1, dp);
-        
-        return dp[index][jumps] = op1 || op2 || op3;
-        
-        
+        int op1 = frog(stones, index, jump, dp);
+        int op2 = frog(stones, index, jump + 1, dp );
+        int op3 = frog(stones, index, jump - 1, dp);
+        dp[index][jump] = op1 || op2 ||op3;
+        return dp[index][jump];
     }
     
-    int getIdx(vector<int> &s, int val, int index){
-        
-        for(int i=index;i<s.size();i++){
-            if(s[i]==val) return i;
+    int getIdx(vector<int> &stones, int index, int val){
+        for(int i=index;i<stones.size();i++){
+            if(stones[i]==val) return i;
         }
-        
         return -1;
     }
 };
