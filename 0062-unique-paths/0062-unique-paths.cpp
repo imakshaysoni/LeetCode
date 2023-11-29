abd7cab7c6f73eefb6392ef22f401000ab50dcc4
Akshay Soni <akshaysoni460@gmail.com>
2023-11-29 12:36:59 +0530

    Time: 4 ms (6.98%), Space: 6.8 MB (38.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,29 +2,17 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>> board(m,vector<int>(n,0));
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        int ans = helper(board, 0, 0, dp);
-        // helper(board,0,0);
-        return ans;
+        vector<vector<int>> dp (m, vector<int>(n,0));
+        return solve(dp, 0, 0);
     }
     
-    int helper(vector<vector<int>> &board, int i, int j, vector<vector<int>> &dp){
-        int m = board.size();
-        int n = board[0].size();
-        
-        if(i>=m || j>=n) return 0;
-        
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
-        
+    int solve(vector<vector<int>> &dp, int i, int j){
+        if(i<0 || i>=dp.size() || j<0 || j>=dp[0].size()) return 0;
+        if(i==dp.size()-1 and j==dp[0].size()-1) return 1;
         if(dp[i][j]!=0) return dp[i][j];
-        
-        dp[i+1][j] = helper(board, i+1,j, dp);
-        dp[i][j+1] = helper(board, i, j+1, dp);
-        
-        return dp[i+1][j] + dp[i][j+1];
-        
+        int op1 = solve(dp,i+1,j); // move down
+        int op2 = solve(dp, i, j+1); // Move Right
+        dp[i][j] = op1 + op2;
+        return dp[i][j];
     }
 };
