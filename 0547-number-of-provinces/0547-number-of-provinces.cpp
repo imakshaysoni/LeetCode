709e02e64f45af919c564e2844b06e1415971ec2
Akshay Soni <akshaysoni460@gmail.com>
2023-12-09 17:16:34 +0530

    Time: 25 ms (39.91%), Space: 19.5 MB (6.20%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,56 +1,45 @@
 class Solution {
 public:
-    int ans=0;
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<vector<int>> adj(n+1);
-        vector<int> vis(n+1,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        int cities=isConnected.size();
+        vector<vector<int>> adj(cities+1,vector<int> (cities+1,0));
+        for(int i=0;i<cities;i++){
+            for(int j=0;j<cities;j++){
                 if(i!=j and isConnected[i][j]==1){
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
-        
-        
-        for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                ans++;
-                // dfs(i, vis, adj);
-                bfs(i, vis, adj);
-            }
-        }
-        return ans;
-    }
-    
-    void bfs(int node, vector<int> &vis, vector<vector<int>> &adj){
+        int ans=0;
+        vector<int> visited(cities+1,0);
         queue<int> q;
-        q.push(node);
-        while(!q.empty()){
-            int n = q.front();
-            // vis[n]=1;
+        
+        for(int i=0;i<cities;i++){
+            
+            if(visited[i]==0){
+                ans++;
+            q.push(i);
+            visited[i]=1;
+            while(!q.empty()){
+            int city = q.front();
             q.pop();
-            for(auto it: adj[n]){
-                if(!vis[it]){
+            
+            for(auto it: adj[city]){
+                if(visited[it]==0){
+                    visited[it]=1;
                     q.push(it);
-                    vis[it] = 1;
                 }
+            }
+        }    
             }
             
         }
         
-    }
-    
-    void dfs(int node, vector<int> &vis, vector<vector<int>> &adj){
+        return ans;
         
-        vis[node]=1;
-        for(auto n: adj[node]){
-            if(vis[n]==0){
-                dfs(n, vis, adj);
-            }
-        }
-        return;
+        
+        
+        
     }
 };
