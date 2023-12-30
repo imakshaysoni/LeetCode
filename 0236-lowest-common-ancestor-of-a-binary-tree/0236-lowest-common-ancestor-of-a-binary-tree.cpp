e856851218a2a19727445b1a4401327df206647f
Akshay Soni <akshaysoni460@gmail.com>
2023-12-30 16:49:43 +0530

    Time: 29 ms (5.85%), Space: 14.5 MB (90.95%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,64 +10,20 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-//         More Optimized Solution
         
         return lca(root, p, q);
-     
-        
-//         vector<TreeNode*> path1;
-//         vector<TreeNode*> path2;
-//         path(root, p, path1);
-//         path(root, q, path2);
-        
-//         int len1 = path1.size();
-//         int len2 = path2.size();
-        
-//         for(int i=1;i<min(len1, len2);i++){
-//             if(path1[i]!=path2[i])
-//                 return path1[i-1];
-//         }
-//         return path1[min(len1,len2)-1];
-        
-//         return root;
     }
     
-    TreeNode* lca(TreeNode * root, TreeNode* p, TreeNode* q){
+    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q){
         
-        if(root==NULL or root==p or root==q)
-            return root;
+        if(root==NULL || root==p || root==q) return root;
         
         
-        TreeNode * left = lca(root->left, p, q);
-        TreeNode * right = lca(root->right, p, q);   
-        if(left and right) {
-            return root;
-        }
-        if(left!=NULL) return left;
-        return right;        
+        TreeNode* left = lca(root->left, p, q);
+        TreeNode* right = lca(root->right, p, q);
         
-    }
-    
-    bool path(TreeNode* root, TreeNode* p, vector<TreeNode*> &res){
-        
-        if(root==NULL) return false;        
-        
-        res.push_back(root);
-        
-        if(root==p){
-            return true;
-        }
-        
-        bool op1 = path(root->left, p, res);
-        bool op2 = false;
-        if(op1==false)
-            op2 = path(root->right, p, res);
-        if(op1==false and op2==false)
-        {
-            res.pop_back();     
-            return false;
-        }
-            
-        return true;
+        if(left==NULL) return right;
+        else if(right==NULL) return left;
+        else return root;
     }
 };
