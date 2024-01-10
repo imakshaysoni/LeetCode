179db0735de2385738af4829c176dbca80876a48
Akshay Soni <akshaysoni460@gmail.com>
2024-01-10 14:49:43 +0530

    Time: 8 ms (77.60%), Space: 9.4 MB (47.04%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,109 +2,33 @@ class Solution {
 public:
     int minDistance(string word1, string word2) {
         
-        int m = word1.size();
-        int n = word2.size();
-        
-        // Recursive 
-        // vector<vector<int>> dp(m, vector<int>(n, -1));
-        // return helper(word1, word2, m-1, n-1, dp);
-        
-//         Converting into bottom up DP
-        // int DP2 = bottom_up_2D(word1, word2);
-        // return DP2;
-        
-//        1D 2 Array Space Optimized
-        int DP1 = bottom_up_1D(word1, word2);
-        return DP1;
-        
-        
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return solve(word1, word2, n, m, dp);
     }
     
-// Recursive Approch
-    int helper(string &s1, string &s2, int i, int j, vector<vector<int>> &dp){
+    int solve(string &w1, string &w2, int i, int j, vector<vector<int>> &dp){
         
-//         Base Case
-//         If S1 Get Exsaust
-        if(i<0) return j+1;
-//         If S2 Get Exsaust
-        if(j<0) return i+1;      
+        if(j==0) return i;
+        if(i==0) return j;        
         
         if(dp[i][j]!=-1) return dp[i][j];
         
-        if(s1[i]==s2[j]) {
-            int op = helper(s1,s2,i-1,j-1, dp);
-            return dp[i][j] = op;
-            }
-            else{
-            
-//             Option1 Insert   
-                int op1 = 1 + helper(s1,s2,i,j-1, dp);
-            
-//             Option2 Delete
-            int op2 = 1 + helper(s1,s2,i-1, j, dp);
-            
-            
-//             Option3 Replace
-            int op3 = 1 + helper(s1,s2,i-1,j-1, dp);
-            
-            return dp[i][j] = min(op1, min(op2,op3));
+        if(w1[i-1]==w2[j-1]){
+            return dp[i][j] = solve(w1, w2, i-1, j-1, dp);
         }
-        
-        
-    }
-    
-    
-//     2D Array
-    int bottom_up_2D(string &word1, string &word2){
-        int m = word1.size();
-        int n = word2.size();
-        vector<vector<int>> dp(m+1, vector<int> (n+1,0));
-        
-        for(int j=0;j<=n;j++) dp[0][j] = j;
-        for(int i=0;i<=m;i++) dp[i][0] = i;
-        
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] =  1 + min(dp[i][j-1],
-                                   min(dp[i-1][j],
-                                       dp[i-1][j-1]));
-                }
-            }
+        else{            
+//             Insert            
+            int insert_op = 1 + solve(w1, w2, i, j-1, dp);
+//             Replace
+            int replace_op =  1 + solve(w1, w2, i-1, j-1, dp);
+//             Delete
+            int delete_op = 1 + solve(w1, w2, i-1, j, dp);
+            
+            return dp[i][j] = min(insert_op, min(replace_op, delete_op));            
         }
-        
-        return dp[m][n];
-    }
-    
-        
-//     1D Array More Space Optimized
-    int bottom_up_1D(string &word1, string &word2){
-        int m = word1.size();
-        int n = word2.size();
-        vector<int> prev(n+1,0);
-        vector<int> curr(n+1,0);
-        
-        for(int j=0;j<=n;j++) prev[j] = j;
-        // for(int i=0;i<=m;i++) dp[i][0] = i; Not Required
-        
-        for(int i=1;i<=m;i++){
-            curr[0] = i;
-            for(int j=1;j<=n;j++){
-                if(word1[i-1]==word2[j-1]){
-                    curr[j] = prev[j-1];
-                }
-                else{
-                    curr[j] =  1 + min(curr[j-1],
-                                   min(prev[j],
-                                       prev[j-1]));
-                }
-            }
-            prev = curr;
-        }
-        
-        return prev[n];
     }
 };
+
+
