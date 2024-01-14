fae43dace2e69acd058cbc343e325acda4aca8a6
Akshay Soni <akshaysoni460@gmail.com>
2024-01-14 13:30:01 +0530

    Time: 4 ms (62.41%), Space: 12.9 MB (70.94%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -12,62 +12,24 @@
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        if (!root) return;
         
-        TreeNode* prev = nullptr;
-        
-        // Approach 1: Recursive Approach
-        // Time Complexity: O(N), where N is the number of nodes in the binary tree
-        // Space Complexity: O(N) due to the recursive call stack
-        
+
+        TreeNode* prev = NULL;
         flatBT(root, prev);
-        
-        // Approach 2: Stack Approach
-        // Time Complexity: O(N), where N is the number of nodes in the binary tree
-        // Space Complexity: O(N) due to the stack usage
-        stack<TreeNode*> st;
-        st.push(root);
-        while (!st.empty()) {
-            TreeNode* curr = st.top();
-            st.pop();
-            if (curr->right) st.push(curr->right);
-            if (curr->left) st.push(curr->left);
-            curr->left = nullptr;
-            if (!st.empty())
-                curr->right = st.top();
-            else
-                curr->right = nullptr;
-        }
-        
-        // Approach 3: Morris Traversal
-        // Time Complexity: O(N), where N is the number of nodes in the binary tree
-        // Space Complexity: O(1) as no additional space is used
-        TreeNode* node = root;
-        while (node) {
-            if (node->left) {
-                TreeNode* prev = node->left;
-                while (prev->right) {
-                    prev = prev->right;
-                }
-                prev->right = node->right;
-                node->right = node->left;
-                node->left = nullptr;
-                node = node->right;
-            } else {
-                node = node->right; 
-            }          
-        }   
     }
     
-    // Recursive function to flatten the binary tree
-    void flatBT(TreeNode* root, TreeNode*& prev) {
-        if (!root) return;
+    void flatBT(TreeNode* root, TreeNode* &prev){
+        
+        if(root==NULL) return;
         
         flatBT(root->right, prev);
         flatBT(root->left, prev);
         
-        root->left = nullptr;
+        root->left = NULL;
         root->right = prev;
-        prev = root;      
+        prev=root;
+        
+        
+        
     }
 };
