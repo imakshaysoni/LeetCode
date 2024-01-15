8fb13138ef845913794fdb38fe338f8cc514ece6
Akshay Soni <akshaysoni460@gmail.com>
2024-01-15 21:41:31 +0530

    Time: 161 ms (67.10%), Space: 75.8 MB (76.59%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -4,37 +4,56 @@ public:
         int n = prices.size();
         // vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(2, -1)));
         // return get_profit(prices, 0, 0, dp, 0);
-        return tabulation_code(prices, n);    
-//         return space_optimized(prices, n);
+        // return tabulation_code(prices, n);    
+        return space_optimized(prices, n);
         
     }
     
     int space_optimized(vector<int>&prices, int n){
         //         Space Optimized
-      
-        vector<int> ahead(2, 0);
-        ahead[0] = 0;// dp[n][0]  = 0;
-        ahead[1] = 0;// dp[n][1] = 0;
+        // vector<vector<vector<int>>>dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+        
+        vector<vector<int>> ahead(2, vector<int>(3, 0));
+        vector<vector<int>> curr(2, vector<int>(3, 0));
+//         First Base case; When k == 2, whatever idx & buy, it shoudl be 0
+//         for(int idx=0;idx<n;idx++){
+//             for(int buy=0;buy<2;buy++){
+//                 dp[idx][buy][2] = 0;
+//             }
+//         }
+        
+// //         SecondBase Case; When idx==n, whatever buy & cap, it should be 0
+//         for(int idx=0;idx<n;idx++){
+//             for(int buy=0;buy<2;buy++){
+//             for(int k=0;k<=2;k++){
+//                 dp[idx][buy][k] = 0;
+//                 }
+//             }    
+//         }
+        
+        
+        
         
         for(int i=n-1;i>=0;i--){
-            vector<int> curr(2,0);
             for(int j=0;j<2;j++){
-                if(j==0){
-                    int op1 = -prices[i] + ahead[1]; //  get_profit(prices, idx+1, 1, dp);
-                    int op2 = ahead[0]; //get_profit(prices, idx+1, 0, dp);
-                    curr[j] = max(op1, op2);
+                for(int k=0;k<2;k++){
+                    if(j==0){
+                    int op1 = -prices[i] + ahead[1][k]; //  get_profit(prices, idx+1, 1, dp, k);
+                    int op2 = ahead[0][k]; //get_profit(prices, idx+1, 0, dp, k);
+                    curr[j][k] = max(op1, op2);
+                    }
+                    else{
+                        int op3 = prices[i] + ahead[0][k+1]; //get_profit(prices, idx+1, 0, dp, k+1);
+                        int op4 = ahead[1][k]; //get_profit(prices, idx+1, 1, dp, k);            
+                        curr[j][k] = max(op3, op4);
+                    } 
                 }
-                else{
-                    int op3 = prices[i] + ahead[0]; //get_profit(prices, idx+1, 0, dp);
-                    int op4 = ahead[1]; //get_profit(prices, idx+1, 1, dp);            
-                    curr[j] = max(op3, op4);
-                }
+                
             }
             ahead = curr;
-            
         }
         
-        return ahead[0];
+        return ahead[0][0];
 
     }
     
