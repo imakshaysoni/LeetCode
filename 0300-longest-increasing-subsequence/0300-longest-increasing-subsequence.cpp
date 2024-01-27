26cf81ee9af8c500137796584e40c72cb4415fef
Akshay Soni <akshaysoni460@gmail.com>
2024-01-27 23:37:56 +0530

    Time: 505 ms (20.02%), Space: 294.9 MB (6.08%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,57 +1,26 @@
 class Solution {
 public:
-    int maxLen = INT_MIN;
-    int lengthOfLIS(vector<int>& nums) {
-        // int n = nums.size();
-        // vector<int> subset;
-        vector<int> dp(nums.size()+1, -1);
-        int ans = LenHelper(nums, 0,-1, dp);
-        return ans;
+    int lengthOfLIS(vector<int>& nums) {           
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return solve(nums, 0, -1, dp);
     }
     
-    int LenHelper(vector<int> &nums, int index, int prev, vector<int> &dp){
+    int solve(vector<int>&nums, int idx, int prev, vector<vector<int>>&dp){
         
-        if(index==nums.size()) return 0;
+        if(idx==nums.size()) return 0;
         
-        if(dp[prev+1] != -1) return dp[prev+1];
+        if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
         
-//        not Take it
-        int op1 =LenHelper(nums, index+1, prev, dp);
-        
-//         Take it
-        int op2=0;
-        if(prev == -1 || nums[index] > nums[prev])
-        {
-            op2 = 1 + LenHelper(nums, index+1,index, dp);
+//         Option1
+        int op1 = 0 + solve(nums, idx+1, prev, dp);
+        int op2 = 0;
+        if(prev==-1 || nums[idx] > nums[prev]){
+            op2 = 1 + solve(nums, idx+1, idx, dp);
         }
+        return dp[idx][prev+1] = max(op1, op2);
         
-        return dp[prev+1]= max(op1,op2);
     }
     
-//     void helper(vector<int> &nums, vector<int> &subset, int i){
-        
-//         // if(i>nums.size()) return;
-//         if(i==nums.size()){
-//             for(auto x: subset) cout<<x<<" ";
-//             int len = subset.size();
-//             maxLen = max(len,maxLen);
-//             cout<<endl;
-//             return;
-//         }
-        
-//         if(subset.size()>0 && nums[i] > subset.back()){
-// //         Take it
-//             subset.push_back(nums[i]);
-//             helper(nums,subset,i+1);    
-            
-//         }        
-        
-//         if(i==0){ subset.push_back(nums[i]); helper(nums,subset,i+1); }
-        
-//         // leave it
-//         if(subset.size()>0)  subset.pop_back();
-//         helper(nums,subset,i+1);
-//         return;
-        
-//     }
+    
 };
