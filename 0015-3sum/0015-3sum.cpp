1f6c55e948ac55ff0f36148a93baa5c7c194164a
Akshay Soni <akshaysoni460@gmail.com>
2024-01-31 09:54:12 +0530

    Time: 61 ms (95.48%), Space: 26.8 MB (51.63%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,45 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        int n=nums.size();
-        vector<vector<int>> v;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n-2;i++){
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<n;i++){
             int target = -nums[i];
-            int s=i+1;
-            int e=n-1;
-            while(s<e){
-                
-                if(nums[s]+nums[e] == target){
-                    v.push_back({nums[i],nums[s],nums[e]});
-                    while(s<e && nums[s]==nums[s+1])s++;
-                while(e > s&&nums[e]==nums[e-1])e--;
-                    s++;e--;
+            int left = i+1;
+            int right = n-1;
+            
+            while(left<right){
+                if(nums[left]+nums[right]==target){
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    while(left<right and nums[left]==nums[left+1]) left++;
+                    while(right>left and nums[right]==nums[right-1]) right--;
+                    left++;
+                    right--;
                 }
-                else if (nums[s]+nums[e]>target)e--;
-                else s++;
+                else if (nums[left]+nums[right]<target){
+                    left++;
+                }
+                else{
+                    right--;
+                }
             }
-            while(i<n-1 && nums[i]==nums[i+1])i++;
+            while(i<n-1 && nums[i]==nums[i+1]) i++;
+            
             
         }
-        return v;
-//          Brute Force TLE
-        // sort(nums.begin(),nums.end());
-        // vector<vector<int>> v;
-        // int n=nums.size();
-        // for(int i=0;i<n-2;i++){
-        //     for(int j=i+1;j<n-1;j++){
-        //         for(int k=j+1;k<n;k++){
-        //             if(nums[i]+nums[j]+nums[k]==0){
-        //                 v.push_back({nums[i],nums[j],nums[k]});   
-        //             }
-        //             while(k<n-1 && nums[k]==nums[k+1])k++;
-        //         }
-        //         while(j<n-1 && nums[j]==nums[j+1])j++;
-        //     }
-        //     while(i<n-2 && nums[i]==nums[i+1])i++;
-        // }
-        // return v;
+        
+        return ans;
+            
     }
 };
