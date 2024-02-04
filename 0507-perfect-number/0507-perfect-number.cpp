0165a478bdef46a5a991876ef4735ce5528e7279
Akshay Soni <akshaysoni460@gmail.com>
2024-02-04 15:19:04 +0530

    Time: 1054 ms (46.22%), Space: 6.8 MB (28.32%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -3,7 +3,7 @@ public:
     bool checkPerfectNumber(int num) {
         if(num==1) return false;
         int sum=1;
-        for(int i=2;i<num;i++){
+        for(int i=2;i<=num/2;i++){
             if(num%i==0) sum+=i;
         }
         if(sum==num) return true;
