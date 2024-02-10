2a5dc369a7ed7782348b3f0427cea431a8872ed1
Akshay Soni <akshaysoni460@gmail.com>
2024-02-10 12:07:59 +0530

    Time: 43 ms (69.53%), Space: 44.8 MB (22.16%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -4,22 +4,21 @@ public:
         int totSum = 0;
         for(auto it: cardPoints) totSum+=it;
         int n = cardPoints.size();
+        
+        if(k==n) return totSum;
         int left = 0;
         int right = 0;
         int sum = 0;     
         int ans = 0;
         while(right<n){
-            sum += cardPoints[right];
-            
+            sum += cardPoints[right];            
             if(right-left+1 == n-k){
                 ans = max(totSum-sum, ans);
                 sum -= cardPoints[left];
                 left++;
             }
-            right++;
-            
+            right++;            
         }
-        if(ans==0) return totSum;
         return ans;
     }
 };
