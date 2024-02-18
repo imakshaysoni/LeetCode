8acefbd8f81a34076897f6c707fecd2e05d81ece
Akshay Soni <akshaysoni460@gmail.com>
2024-02-18 11:40:28 +0530

    Time: 12 ms (62.25%), Space: 21.8 MB (5.40%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
-    
-        intervals.push_back(newInterval);
-        
         int n = intervals.size();
-        
-        sort(intervals.begin(), intervals.end());
-        
-        vector<vector<int>> ans;
-        
+        vector<vector<int>> new_intervals;
+        bool inserted=false;
         for(int i=0;i<n;i++){
+            if(newInterval[0] < intervals[i][0] and inserted==false){
+                new_intervals.push_back(newInterval);
+                inserted=true;
+            }
+            new_intervals.push_back(intervals[i]);            
+        }
+        if(new_intervals.size()==intervals.size()) new_intervals.push_back(newInterval);
+        vector<vector<int>> ans;
+        int newn = new_intervals.size();
+        for(int i=0;i<newn;i++){
             
-            if(ans.empty() || intervals[i][0] > ans.back()[1]){
-                ans.push_back(intervals[i]);
+            if(ans.empty() || new_intervals[i][0] > ans.back()[1]){
+                ans.push_back(new_intervals[i]);
             }
             else{
-                ans.back()[1] = max(ans.back()[1] ,  intervals[i][1]);
+                ans.back()[1] = max(ans.back()[1] ,  new_intervals[i][1]);
             }
             
             
         }
         
-        return ans;
-        
-        
-        
-        
+        return ans;       
         
     }
 };
