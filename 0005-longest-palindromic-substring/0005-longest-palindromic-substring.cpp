2351bfcedc31e3d79d613cd853637a17f6231fa0
Akshay Soni <akshaysoni460@gmail.com>
2024-02-24 13:17:35 +0530

    Time: 38 ms (59.61%), Space: 174.1 MB (17.90%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string twoPointer(string s, int left, int right){
+        while(left>=0 && right<s.size() && s[left]==s[right]){
+            left--;
+            right++;
+        }
+        return s.substr(left+1, right-left-1);
+        
+    }
+    string longestPalindrome(string s) {
+        if(s.size()<=1) return s;
+        string ans = "";
+        int n = s.size();
+        for(int i=0;i<n;i++){
+                string even = twoPointer(s, i, i);
+                string odd = twoPointer(s, i, i+1);
+                
+                if(even.size()>ans.size()){
+                    ans=even;
+                }
+                if(odd.size()>ans.size()){
+                    ans = odd;
+                }
+        }
+        
+        return ans;
+        
+    }
+};
