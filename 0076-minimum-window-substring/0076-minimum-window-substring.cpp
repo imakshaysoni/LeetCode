736f1e5dc0fdc0472610e41b8426476b966e7fcc
Akshay Soni <akshaysoni460@gmail.com>
2025-01-25 11:53:36 +0530

    Time: 4 ms (75.31%), Space: 11.7 MB (25.99%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,45 +2,35 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         
-        unordered_map<char, int> mapp;
-        // Count Freq Of each word in t
-        for(char ch: t){
-            mapp[ch]++;
-        }
-        int mini=INT_MAX;
-        int startIdx = -1;
-        int left=0;
-        int right=0;
-        int n=s.size();
-        int cnt = 0;
-        while(right<n){
+        vector<int> ft(128, 0);
+        vector<int> fs(128,0);
+        int min_ans = INT_MAX;
+        for(auto it: t) ft[it]++;
+
+        int left =0, right=0;
+        int start=0, end=0;
+        int count = 0;
+        while(right<s.size()){
             char ch = s[right];
-            if(mapp.find(ch)!=mapp.end() and mapp[ch]>0){
-                cnt++;
-                mapp[ch]--;
-            }
-            else{
-                if(mapp.find(ch)!=mapp.end()){
-                    mapp[ch]--;
+            fs[ch]++;
+
+            if(fs[ch]<=ft[ch]) count++;
+
+            while(count==t.size()){
+                if(min_ans>=right-left+1){
+                    start=left;
+                    min_ans = right-left+1;
                 }
-                else{
-                    mapp[ch]=-1;
-                }
-            }
-            
-            while(cnt==t.size()){
-                if(right-left+1 < mini){
-                    mini = right-left+1;
-                    startIdx = left;
-                }
-                char ch = s[left];
-                mapp[ch]++;
+
+                char ch1 = s[left];
+                fs[ch1]--;
+                if(fs[ch1]<ft[ch1]) count--;
                 left++;
-                if(mapp[ch]>0) cnt--;
             }
             right++;
         }
-        if(startIdx==-1) return "";
-        return s.substr(startIdx, mini);
+        return (min_ans==INT_MAX) ? "" : s.substr(start, min_ans);
+
+
     }
 };
