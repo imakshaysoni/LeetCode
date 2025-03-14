a0408508faf916004bd8bbc6a845be02205494d0
Akshay Soni <akshaysoni460@gmail.com>
2025-03-14 18:55:08 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.1%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -2,32 +2,29 @@ class Solution {
 public:
     bool checkValidString(string s) {
         
-        stack<int> left;
-        stack<int> star;
-        
-        for(int i=0;i<s.size();i++) 
-        {
-            char ch = s[i];
-            if(ch=='(') left.push(i);
+        stack<char> open;
+        stack<char> star;
+
+        for (int i=0;i<s.size();i++){
+            char ch  = s[i];
+
+            if(ch=='(') open.push(i);
             else if(ch=='*') star.push(i);
             else{
-                if(!left.empty()) left.pop();
-                else if(!star.empty()) star.pop();
+                if(open.size()>0) open.pop();
+                else if(star.size()>0) star.pop();
                 else return false;
             }
-        }
         
-        while(left.size()>0){
+        }
+        while(open.size()>0){
             if(star.size()==0) return false;
-            if(star.top()>left.top()){
+            if(star.top() > open.top()) {
+                open.pop();
                 star.pop();
-                left.pop();
             }
             else return false;
         }
-        
         return true;
-        
-        
     }
 };
