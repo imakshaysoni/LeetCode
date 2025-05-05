fdef21c6acc7d8ad04cc919af0babfe76b2c9231
Akshay Soni <akshaysoni460@gmail.com>
2025-05-05 15:33:30 +0530

    Time: 54 ms (44.97%), Space: 21 MB (27.6%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -6,11 +6,38 @@ public:
         int cols = grid[0].size();
         vector<vector<vector<int>>> dp (
             rows, vector<vector<int>> (
-                cols, vector<int>(cols, -1)
+                cols, vector<int>(cols, 0)
             )
         );
-        return solve(grid, rows, cols, 0, 0, cols-1, dp);
+        // return solve(grid, rows, cols, 0, 0, cols-1, dp);
+        // Tabulation
+        for(int c1=0;c1<cols;c1++){
+            for(int c2=0;c2<cols;c2++){
+                if(c1==c2) dp[rows-1][c1][c2] = grid[rows-1][c1];
+                else dp[rows-1][c1][c2] = grid[rows-1][c1] + grid[rows-1][c2];
+            }
+        }
+        for(int r1=rows-2;r1>=0;r1--){
+            for(int c1=cols-1;c1>=0;c1--){
+                for(int c2=cols-1;c2>=0;c2--){
+                    int cherries = 0;
+                    if(c1==c2) cherries = grid[r1][c1];
+                    else cherries = grid[r1][c1] + grid[r1][c2];
+                    int maxi = 0;
+                    for(int dx=-1;dx<=1;dx++){
+                        for(int dy=-1;dy<=1;dy++){
+                            if(c1+dx >= 0 && c1+dx < cols && c2+dy >= 0 && c2+dy < cols){
+                                maxi = max(maxi, dp[r1+1][c1+dx][c2+dy] + cherries);
+                            }
+                        }
+                    }
+                    dp[r1][c1][c2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][cols-1];
     }
+    
 
     int solve(vector<vector<int>>&grid, int rows, int cols, int r1, int c1, int c2, vector<vector<vector<int>>> &dp){
         int r2 = r1;
