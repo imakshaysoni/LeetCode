a33f6db1cca8365f287502b8368fc670154bbe79
Akshay Soni <akshaysoni460@gmail.com>
2025-05-05 14:42:53 +0530

    Time: 39 ms (83.06%), Space: 21.1 MB (27.6%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-        int r = grid.size();
-        int c = grid[0].size();
-        vector<vector<vector<int>>>dp(r,vector<vector<int>>(c, vector<int>(c,-1)));
-        return dfs(grid, r, c, 0, 0, c-1, dp);
-    }
-    
-    int dfs(vector<vector<int>>&grid, int rows, int cols, int row, int col1, int col2, vector<vector<vector<int>>> &dp){
-    if(col1<0 || col1>=cols || col2<0 || col2>=cols) return -1e8;
-    if(row==rows-1){
-        if(col1==col2) return grid[row][col1];
-        else return grid[row][col1]+grid[row][col2];
+        
+        int rows= grid.size();
+        int cols = grid[0].size();
+        vector<vector<vector<int>>> dp (
+            rows, vector<vector<int>> (
+                cols, vector<int>(cols, -1)
+            )
+        );
+        return solve(grid, rows, cols, 0, 0, cols-1, dp);
     }
 
-    if(dp[row][col1][col2]!=-1) return dp[row][col1][col2];
-    int maxi=0;
-    for(int dx=-1;dx<=1;dx++){
-        for(int dy=-1;dy<=1;dy++){
-            int result=0;
-            if(col1==col2){
-                result = grid[row][col1] + dfs(grid, rows, cols, row+1, col1+dx, col2+dy, dp);
-            }
-            else{
-                result = grid[row][col1] + grid[row][col2] + dfs(grid, rows, cols, row+1, col1+dx, col2+dy, dp);
-            }            
-            maxi = max(maxi, result);
-            dp[row][col1][col2] = maxi;
+    int solve(vector<vector<int>>&grid, int rows, int cols, int r1, int c1, int c2, vector<vector<vector<int>>> &dp){
+        int r2 = r1;
+        if(r1>=rows || r1<0 || r2>=rows || r2<0 ||
+        c1>=cols || c2>=cols || c1<0 || c2<0 ) return 0;
+
+        if(r1==rows-1 and r2==rows-1){
+            if(c1==c2) return grid[r1][c1];
+            return grid[r1][c1]+grid[r2][c2];
         }
-    }
 
-    return dp[row][col1][col2];    
-}
+        if(dp[r1][c1][c2]!=-1) return dp[r1][c1][c2];
+
+        int cherries = 0;
+        if(r1==r2 and c1==c2) cherries = grid[r1][c1];
+        else cherries = grid[r1][c1]+grid[r2][c2];
+        int maxi = 0;
+        for(int dx=-1;dx<=1;dx++){
+            for(int dy=-1;dy<=1;dy++){
+                maxi = max(maxi, 
+                solve(grid, rows, cols, r1+1, c1+dx, c2+dy, dp)
+                + cherries
+                );
+                
+            }
+        }
+        return dp[r1][c1][c2] = maxi;
+
+
+    }
 };
