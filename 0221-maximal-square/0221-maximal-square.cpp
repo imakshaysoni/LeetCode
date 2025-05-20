25096732355c4e80ca749fd0298526ec148e8187
Akshay Soni <akshaysoni460@gmail.com>
2025-05-20 11:56:02 +0530

    Time: 22 ms (18.86%), Space: 31.2 MB (14.02%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,117 +1,44 @@
-// class Solution {
-// public:
-//     int maximalSquare(vector<vector<char>>& m) {
-//         int maxCount = 0;
-//         for(int row=0;row<m.size();row++){
-//             for(int col=0;col<m[0].size();col++){
-//                 if(m[row][col]=='1'){
-//                     int area = 0;
-//                     area = helper(m, row, col);
-//                     maxCount = max(maxCount,area);
-//                 }
-//             }
-//         }
-//         return maxCount*maxCount;        
-//     }
-    
-//     int helper(vector<vector<char>> &m, int row, int col){
-        
-//         if(row<0 || row>=m.size() || col<0 || col>=m[0].size() || m[row][col]=='0') return 0;
-// //         
-//         int count =0;
-//         count++;
-        
-// //         right Count
-//         int rightCnt=0;
-//         for(int j=col;j<m[0].size();j++){
-//             if(m[row][j]=='1') rightCnt++;
-//             else break;
-//         }
-        
-//         int downCnt=0;
-//         for(int i=row;i<m.size();i++){
-//             if(m[i][col]=='1') downCnt++;
-//             else break;
-//         }
-        
-//         int digCnt=0;
-//         for(int i=row,j=col;i<m.size() && j<m[0].size();j++,i++){
-//             if(m[i][j]=='1') digCnt++;
-//             else break;
-//         }
-        
-//         int minCount = min(min(rightCnt, downCnt), digCnt);
-//         int cnt = 0;
-//         for(int i=row+1;i<row+minCount;i++){
-//             for(int j=col+1;j<col+minCount;j++){
-//                 // cout<<i<<" "<<j<<endl;
-//                 if(m[i][j]=='0'){
-//                     // return cnt*cnt;
-//                     return i-row+1;
-//                 }
-//             }
-//         }        
-//         // minCount = min(cnt)
-//         return minCount;
-//         return minCount*minCount;
-        
-//     }
-    
-    
-// };
-
 class Solution {
 public:
-    int maxAns = 0;
+    int maxArea = 0;
     int maximalSquare(vector<vector<char>>& matrix) {
-        vector<vector<int>> dp(matrix.size(), vector<int>(matrix[0].size(),-1));
         
-        // int x = helper(matrix,0,0, dp);
-        int x = dpBottomUp(matrix);
-        return maxAns*maxAns;
-    }
-    
-    int helper(vector<vector<char>> &mat, int i, int j, vector<vector<int>> &dp){
-        
-        if(i>=mat.size() || j>=mat[0].size()) return 0;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        int rightCnt = helper(mat, i, j+1, dp);
-        int digCnt = helper(mat, i+1, j+1, dp);
-        int downCnt = helper(mat, i+1, j, dp);
-        
-        if(mat[i][j]=='1'){
-            int ans = 1+min(rightCnt,min(downCnt,digCnt));
-            dp[i][j]=ans;
-            maxAns = max(maxAns, ans);
-            return ans;
-        }
-        else{
-            return dp[i][j]=0;
-        }          
-        
-        
-    }
-    
-    int dpBottomUp(vector<vector<char>> &mat){
-        int row = mat.size();
-        int col = mat[0].size();
-        vector<vector<int>> dp(row+1,vector<int> (col+1,0));
-        for(int i=row-1;i>=0;i--){
-            for(int j=col-1;j>=0;j--){
-                
-                int rightCnt = dp[i][j+1];
-                int digCnt = dp[i+1][j+1];
-                int downCnt = dp[i+1][j];
-                
-                if(mat[i][j]=='1'){
-                    dp[i][j]=1+min(rightCnt,min(digCnt,downCnt));
-                    maxAns = max(maxAns, dp[i][j]);
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<vector<int>> dp(rows, vector<int>(cols, -1));
+        // int maxArea = -1e9;
+        for(int row=0;row<rows;row++){
+            for(int col=0;col<cols;col++){
+                if(matrix[row][col]=='1'){
+                    solve(matrix, row, col, rows, cols, dp);
+                    //maxArea = max(maxArea, area);
                 }
-                else dp[i][j]=0;
             }
         }
-        return 0;
-        
+
+        return maxArea;
+
+    }
+
+    int solve(vector<vector<char>> &matrix, int row, int col, int rows, int cols
+    , vector<vector<int>>&dp){
+
+            //base conditions
+            if(row < 0 || row>=rows ||
+            col < 0 || col >=cols || matrix[row][col]=='0') return 0;
+
+            if(dp[row][col]!=-1) return dp[row][col];
+            // Options
+            int op1 = 1 + solve(matrix, row, col+1, rows, cols, dp);
+            int op2 = 1 + solve(matrix, row+1, col, rows, cols, dp);
+            int op3 = 1 + solve(matrix, row+1, col+1, rows, cols, dp);
+
+            int mini = min(op1, min(op2, op3));
+            dp[row][col] = mini;
+            maxArea = max(maxArea, mini*mini);
+            
+            return mini;
+
+
     }
 };
