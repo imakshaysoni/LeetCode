2b7912fc3035053bb7aa806fd039b779cdd33a30
Akshay Soni <akshaysoni460@gmail.com>
2025-05-21 10:32:36 +0530

    Time: 0 ms (100%), Space: 12.1 MB (34.58%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,19 +3,31 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
 
         sort(strs.begin(), strs.end());
-        int n = strs[0].size();
-        int n1 = strs.size();
-        string ans = "";
-        for(int i=0;i<n;i++){
-            int j;
-            for(j=0;j<n1;j++){
-                if(strs[j][i]!=strs[0][i]) break;
-            }
-            if(j==n1) ans+=strs[0][i];
-            else break;
+        // int n = strs[0].size();
+        // int n1 = strs.size();
+        // string ans = "";
+        // for(int i=0;i<n;i++){
+        //     int j;
+        //     for(j=0;j<n1;j++){
+        //         if(strs[j][i]!=strs[0][i]) break;
+        //     }
+        //     if(j==n1) ans+=strs[0][i];
+        //     else break;
 
-        }
+        // }
+        // return ans;
+
+        // Another approch
+        int n = strs.size();
+        string first = strs[0];
+        string last = strs[n-1];
+        string ans = "";
+        for(int i=0;i<first.size();i++){
+            if(first[i]==last[i]) ans += first[i];
+            else break;
+        } 
         return ans;
+
         
     }
 };
