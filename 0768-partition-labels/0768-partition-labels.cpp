ee2a85abb157cfa1fa21c60a9af4dd487d2d84a2
Akshay Soni <akshaysoni460@gmail.com>
2025-05-24 15:53:08 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.06%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -1,57 +1,87 @@
 class Solution {
 public:
-    static bool comp(vector<int> a, vector<int> b){
-        return a[0] < b[0];
-    }
-    vector<int> mergeIntervals(vector<vector<int>> intervals){
-            vector<vector<int>> result;
-            for(auto it: intervals){
-                if(result.empty() || it[0] > result.back()[1]){
-                    result.push_back(it);
-                }
-                else{
-                    result.back()[1] = max(it[1], result.back()[1]);
-                }
+    static bool comp(vector<int> a, vector<int> b) { return a[0] < b[0]; }
+    vector<int> mergeIntervals(vector<vector<int>> intervals) {
+        vector<vector<int>> result;
+        for (auto it : intervals) {
+            if (result.empty() || it[0] > result.back()[1]) {
+                result.push_back(it);
+            } else {
+                result.back()[1] = max(it[1], result.back()[1]);
             }
-            cout<<endl;
-            for(auto it: result){
-            for(auto val: it) cout<<val<<",";
-            cout<<endl;
+        }
+        cout << endl;
+        for (auto it : result) {
+            for (auto val : it)
+                cout << val << ",";
+            cout << endl;
         }
 
-            vector<int> ans;
-            for(auto it: result){
-                if(it[1]==-1) ans.push_back(1);
-                else ans.push_back(it[1]-it[0]+1);
-            }
-            return ans;
+        vector<int> ans;
+        for (auto it : result) {
+            if (it[1] == -1)
+                ans.push_back(1);
+            else
+                ans.push_back(it[1] - it[0] + 1);
+        }
+        return ans;
     }
-            
+
+    vector<int> two_pointer(string s){
+
+        vector<int> mapp(26, 0);
+        for(int i=0;i<s.size();i++){
+            mapp[s[i]-'a'] = i; // Store last index of each char
+        }
+        vector<int> ans;
+        int start = 0;
+        int end = -1;
+        int i = 0;
+        while(i<s.size()){
+
+            end = max(end, mapp[s[i]-'a']);
+
+            if(i==end){
+                ans.push_back(end-start+1);
+                start = i+1;
+            }
+            i++;
+
+
+
+        }
+        return ans;
+
+
+
+
+    }
+
     vector<int> partitionLabels(string s) {
 
-        unordered_map<char, pair<int,int>> mapp;
-        for(int i=0;i<s.size();i++){
+        return two_pointer(s);
+
+        unordered_map<char, pair<int, int>> mapp;
+        for (int i = 0; i < s.size(); i++) {
             char ch = s[i];
-            if(mapp.find(ch)==mapp.end()){
+            if (mapp.find(ch) == mapp.end()) {
                 mapp[ch] = {i, -1};
+            } else {
+                mapp[ch].second = i;
             }
-            else{
-                mapp[ch].second=i;
-            }
-        }   
+        }
         vector<vector<int>> intervals;
-        for(auto it: mapp){
+        for (auto it : mapp) {
             intervals.push_back({it.second.first, it.second.second});
         }
         sort(intervals.begin(), intervals.end(), comp);
 
-        for(auto it: intervals){
-            for(auto val: it) cout<<val<<",";
-            cout<<endl;
+        for (auto it : intervals) {
+            for (auto val : it)
+                cout << val << ",";
+            cout << endl;
         }
-        
-        return mergeIntervals(intervals);
-        return {};
 
+        return mergeIntervals(intervals);
     }
 };
