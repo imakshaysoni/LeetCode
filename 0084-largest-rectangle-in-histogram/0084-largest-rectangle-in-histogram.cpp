b71d9b009a800752f0cdfec6661cc549bf6b5436
Akshay Soni <akshaysoni460@gmail.com>
2025-05-28 18:31:51 +0530

    Time: 19 ms (75.89%), Space: 85.9 MB (29.54%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,58 +1,56 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        
-        int maxArea=0;
         int n = heights.size();
-        vector<int> next;
-        vector<int> prev;
-        next = nextSmaller(heights);
-        prev = prevSmaller(heights);
-        // for(int x: next) cout<<x<<" ";
-        // for(int y: prev) cout<<y<<" ";
-        // cout<<endl;
-        for(int i=0;i<n;i++){
-            if(next[i] == -1) next[i]=n;
-            int area = (next[i]-prev[i]-1) * heights[i];
-            maxArea = max(area, maxArea);
-        }
+        return largestAreaInHistogram(heights, n);
         
+    }
+
+    vector<int> prev_smaller(vector<int> &hist){
+        stack<int> st;
+        vector<int> prevSmaller(hist.size(), -1);
+
+        for(int i=0;i<hist.size();i++){
+            while(!st.empty() and hist[st.top()] >= hist[i]){
+                st.pop();
+            }
+            if(st.empty()) prevSmaller[i] = -1;
+            if(!st.empty()) prevSmaller[i]=st.top();
+            st.push(i);
+        }
+        return prevSmaller;
+    }
+
+    vector<int> next_smaller(vector<int> &hist){
+
+        stack<int> st;
+        vector<int> nextSmaller(hist.size(), -1);
+        for(int i=hist.size()-1;i>=0;i--){
+            while(!st.empty() and hist[st.top()]  >= hist[i]){
+                st.pop();
+            }
+            if(st.empty()) nextSmaller[i] = hist.size();
+            else nextSmaller[i] = st.top();
+            st.push(i);
+        }
+        return nextSmaller;
+    }
+
+    int largestAreaInHistogram(vector<int>& hist, int n) {
+
+        int maxArea = 0;
+        vector<int> prevSmaller = prev_smaller(hist);
+        vector<int> nextSmaller = next_smaller(hist);
+
+        for (int i = 0; i < n; i++) {
+
+            int leftIdx = prevSmaller[i];
+            int rightIdx = nextSmaller[i];
+
+            int width = rightIdx -leftIdx - 1;
+            // if(i==2) cout<<leftIdx<<":"<<rightIdx<<endl;
+            maxArea = max(maxArea, width * hist[i]);
+        }
         return maxArea;
-    }
-    
-    vector<int> nextSmaller(vector<int> &heights){
-        int n = heights.size();
-        vector<int> ans(n);
-        stack<int> s;
-        s.push(-1);
-        
-        for(int i=n-1;i>=0;i--){
-            int currVal = heights[i];
-            while(s.top()!=-1 and heights[s.top()] >= currVal){
-                s.pop();
-            }
-            ans[i]= s.top();
-            s.push(i);            
-            
-        }
-        return ans;
-    }
-    
-    vector<int> prevSmaller(vector<int> &heights){
-        int n = heights.size();
-        vector<int> ans(n);
-        stack<int> s;
-        s.push(-1);
-        
-        for(int i=0;i<n;i++){
-            int currVal = heights[i];
-            while(s.top() !=-1 and heights[s.top()] >= currVal){
-                s.pop();
-            }
-            ans[i] = s.top();
-            s.push(i);            
-            
-        }
-        return ans;
     }
 };
