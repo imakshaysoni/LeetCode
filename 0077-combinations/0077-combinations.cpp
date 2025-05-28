348517dd7454813d1e20c2454977cae2803bd29e
Akshay Soni <akshaysoni460@gmail.com>
2025-05-28 16:14:32 +0530

    Time: 112 ms (18.77%), Space: 131.2 MB (18.44%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    set<vector<int>> stt;
     vector<vector<int>> combine(int n, int k) {
-        vector<int> nums;
-        for(int i=1;i<=n;i++){
-            nums.push_back(i);
-        }
         vector<int> st;
-        // for(int i=0;i<nums.size();i++) {
-        //     st.clear();
-        //     solve(nums, i, st, k);
-        // }
-        solve(nums, 0, st, k);
+        solve(1, n, st, k);
         return res;
     }
 
-    void solve(vector<int> &nums, int i, vector<int> &st, int k){
+    void solve(int i, int n, vector<int> &st, int k){
             if(st.size()==k) {
                 res.push_back(st);
                 return;
             }
-            if(i>=nums.size()) return;
+            if(i>n) return;
             
-
             // pick
-            st.push_back(nums[i]);
-            solve(nums, i+1, st, k);
+            st.push_back(i);
+            solve(i+1, n, st, k);
             // not pick
             st.pop_back();
-            solve(nums, i+1, st, k);
+            solve(i+1, n, st, k);
     }
 };
