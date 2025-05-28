45c6a40ed0d46900c06f14cf8070c5020376ff1f
Akshay Soni <akshaysoni460@gmail.com>
2025-05-28 16:05:13 +0530

    Time: 75 ms (32.84%), Space: 82.5 MB (40.87%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,51 +1,34 @@
 class Solution {
 public:
+    vector<vector<int>> res;
+    set<vector<int>> stt;
     vector<vector<int>> combine(int n, int k) {
-        
-        vector<int> subset;
-        vector<vector<int>> ans;
-        // vector<int> subset;
-        helper(1,n,k,ans, subset);
-        
-        return ans;
-    }
-    
-    
-    void helper(int i, int n, int k, vector<vector<int>> &ans, vector<int> &subSets){
-        
-        
-        if(k==0){
-            ans.push_back(subSets);
-            return;
+        vector<int> nums;
+        for(int i=1;i<=n;i++){
+            nums.push_back(i);
         }
-        if(k > n - i +1) return;
-        if(i>n) return;
-                
-//         Taking the first term
-        subSets.push_back(i);
-        helper(i+1,n,k-1,ans,subSets);
-        
-        
-//         Ignorering the value
-        subSets.pop_back();
-        helper(i+1,n,k,ans,subSets);
-        
-        
+        vector<int> st;
+        // for(int i=0;i<nums.size();i++) {
+        //     st.clear();
+        //     solve(nums, i, st, k);
+        // }
+        solve(nums, 0, st, k);
+        return res;
     }
-    
-     
+
+    void solve(vector<int> &nums, int i, vector<int> &st, int k){
+            if(st.size()==k) {
+                res.push_back(st);
+                return;
+            }
+            if(i>=nums.size()) return;
+            
+
+            // pick
+            st.push_back(nums[i]);
+            solve(nums, i+1, st, k);
+            // not pick
+            st.pop_back();
+            solve(nums, i+1, st, k);
+    }
 };
-
-
-//  Mehtod whihc i figure out on my own, 80% correct
-// void helper(int i ,int n, int k, vector<vector<int>> &ans, vector<int> &subset){
-        
-        
-//         if(subset.size()==k){ ans.push_back(subset); return;}
-        
-//         for(int j=i;j<n+1;j++){
-//             subset.push_back(j);
-//             helper(j+1,n,k,ans,subset);
-//             subset.pop_back();
-//         }
-//     }
