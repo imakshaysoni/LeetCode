a66be56ffb5e5afce6a0c03755ab3db1a6daee1a
Akshay Soni <akshaysoni460@gmail.com>
2025-05-29 10:02:36 +0530

    Time: 0 ms (100%), Space: 10.6 MB (63.5%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
+
     int rob(vector<int>& nums) {
-        
-       
-        // vector<int> dp(nums.size()+1, -1);
-        // int ans = helper(nums,0, dp);
-        // return ans;
-        
-        vector<int> dp(nums.size()+2,0);
-        for(int i=nums.size()-1;i>=0;i--){
-            int op1 = nums[i]+dp[i+2];
-            int op2 = dp[i+1];
-            dp[i] = max(op1,op2);
-        }
-        
-        return dp[0];
+        vector<int> dp(nums.size(), -1);
+        return solve(nums, 0 , dp);
     }
-    
-    int helper(vector<int> &nums, int i, vector<int> &dp){
-        
+
+    int solve(vector<int> &nums, int i, vector<int> &dp){
+
         if(i>=nums.size()) return 0;
         
         if(dp[i]!=-1) return dp[i];
         
-        int op1, op2;
-        
-        op1 = nums[i] + helper(nums, i+2, dp);
-        op2 = helper(nums,i+1, dp);
-        dp[i] = max(op1,op2);
-        return dp[i];      
-        
+        //pick
+        int op1 = nums[i] + solve(nums, i+2, dp);
+        int op2 = solve(nums, i+1, dp);
+        return dp[i] = max(op1, op2);
+
     }
-    
-    
 };
