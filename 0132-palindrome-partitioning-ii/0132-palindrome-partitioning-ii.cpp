8d948a19e3cc40dad7d7cf81a7499eaae63a689f
Akshay Soni <akshaysoni460@gmail.com>
2025-05-31 16:11:35 +0530

    Time: 1840 ms (5.86%), Space: 32.1 MB (22.74%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -6,7 +6,7 @@ public:
             return 0;
         
         vector<int> dp(s.size(), -1);
-        return solve(s, 0, dp);
+        return solve(s, 0, dp) - 1;
     }
     bool checkPalindrom(string &s, int left, int right) {
         while (left <= right) {
@@ -29,11 +29,7 @@ public:
         for (int index = idx; index < s.size(); index++) {
             curr += s[index];
             if (checkPalindrom(s, idx, index))
-                    if(index==s.size()-1) {
-                        mini = min(mini, 0); 
-                    }
-                    else { 
-                        mini = min(mini, 1 + solve(s, index + 1, dp)); }
+                mini = min(mini, 1 + solve(s, index + 1, dp));
         }
         return dp[idx] = mini;
     }
