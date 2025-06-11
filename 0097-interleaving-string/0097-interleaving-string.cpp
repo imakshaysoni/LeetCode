12cab95bc70cf9cd0818ab10e3eff87968b93809
Akshay Soni <akshaysoni460@gmail.com>
2025-06-11 10:03:48 +0530

    Time: 0 ms (100%), Space: 9.8 MB (41.28%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -3,46 +3,34 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size() + s2.size() != s3.size()) return false;
 
-        int i = 0;
         int first = 0;
         int second = 0;
-        vector<vector<vector<int>>> dp (s3.size()+1, vector<vector<int>> (s1.size()+1, vector<int> (s2.size()+1, -1)));
-        return solve(s1,s2,s3,i, first, second, dp);
-
-        // while(i<s3.size()){
-        //     char ch = s3[i];
-        //     if(ch == s1[first]) first++;
-        //     else if(ch == s2[second]) second++;
-        //     else return false;
-        //     i++;
-        // }
-
-        // if(i==s3.size() and first==s1.size() and second==s2.size()) return true;
-        // return false;
+        vector<vector<int>> dp (s1.size()+1, vector<int> (s2.size()+1, -1));
+        return solve(s1,s2,s3,first, second, dp);
 
     }
 
-    bool solve(string &s1, string &s2, string &s3, int i, int first,  int second, vector<vector<vector<int>>> &dp ){
+    bool solve(string &s1, string &s2, string &s3, int first,  int second, vector<vector<int>> &dp ){
         
-        if(i==s3.size() and first==s1.size() and second==s2.size()) return true;
+        if(first==s1.size() and second==s2.size()) return true;
         
-        if(i>=s3.size()) return false;
+        if(first+second >=s3.size()) return false;
         
-        if(i<s3.size() and first >= s1.size() and second>=s2.size()) return false;
+        if(first + second <s3.size() and first >= s1.size() and second>=s2.size()) return false;
 
-        if(dp[i][first][second]!=-1) return dp[i][first][second];
+        if(dp[first][second]!=-1) return dp[first][second];
 
-        char ch = s3[i];
+        char ch = s3[first+second];
         if(first < s1.size() and second<s2.size() and ch!=s1[first] and ch!=s2[second]) return false;
 
         if(first < s1.size() and second<s2.size() and ch == s1[first] and ch == s2[second]){
-            bool op1 = solve(s1,s2,s3,i+1, first+1, second, dp);
-            bool op2 = solve(s1,s2,s3, i+1, first, second+1, dp);
-            return dp[i][first][second] = op1 or op2;
+            bool op1 = solve(s1,s2,s3, first+1, second, dp);
+            bool op2 = solve(s1,s2,s3, first, second+1, dp);
+            return dp[first][second] = op1 or op2;
         } 
-        if(first<s1.size() and ch==s1[first]) return dp[i][first][second] = solve(s1,s2,s3, i+1, first+1, second, dp);
-        if(second<s2.size() and ch==s2[second]) return dp[i][first][second]  = solve(s1,s2,s3, i+1, first, second+1, dp);
-        cout<<i<<first<<second;
+        if(first<s1.size() and ch==s1[first]) return dp[first][second] = solve(s1,s2,s3, first+1, second, dp);
+        if(second<s2.size() and ch==s2[second]) return dp[first][second]  = solve(s1,s2,s3, first, second+1, dp);
+        // cout<<i<<first<<second;
         return false;
 
     }
