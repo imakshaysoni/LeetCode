0aa6e8188e337a2282114d9fd650e6db71088343
Akshay Soni <akshaysoni460@gmail.com>
2025-06-14 18:13:32 +0530

    Time: 0 ms (100%), Space: 15 MB (95.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -9,16 +9,15 @@ public:
 
         while(left<=right){
             int mid = left + (right-left)/2;
-            
             if(nums[mid]==target) return mid;
-            
+            if(nums[left]==target) return left;
+            if(nums[right]==target) return right;
+
             if(nums[left] <= nums[mid]){
-                if(target >= nums[left] and target < nums[mid]){
+                if(target >= nums[left] and target<nums[mid]){
                     right = mid-1;
                 }
-                else{
-                    left = mid+1;
-                }
+                else left = mid+1;
             }
             else{
                 if(target > nums[mid] and target <= nums[right]){
@@ -28,9 +27,10 @@ public:
                     right = mid-1;
                 }
             }
-            
+
         }
         return -1;
+
 
     }
 };
