aff0fe537366d338fb5700a28713f97e8b868bae
Akshay Soni <akshaysoni460@gmail.com>
2025-06-16 10:50:19 +0530

    Time: 13 ms (5.01%), Space: 95 MB (79.54%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,33 +2,47 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-        vector<int> res;
         int first=0;
         int second = 0;
 
+        int n = nums1.size() + nums2.size();
+
+        int idx1 = n/2;
+        int idx2 = n/2-1;
+        int val1= -1;
+        int val2 = -1;
+        int idx= 0;
         while(first < nums1.size() and second <nums2.size()){
             if(nums1[first] < nums2[second] ){
-                res.push_back(nums1[first]);
+                if(idx==idx1) val1=nums1[first];
+                if(idx==idx2) val2=nums1[first];
                 first++;
             }
             else{
-                res.push_back(nums2[second]);
+                if(idx==idx1) val1=nums2[second];
+                if(idx==idx2) val2=nums2[second];
                 second++;
             }
+            idx++;
+            if(val1!=-1 and val2!=-1) break;
         }
-        while(first < nums1.size()){
-            res.push_back(nums1[first]);
-            first++;
-        }
-        while(second < nums2.size()){
-            res.push_back(nums2[second]);
-            second++;
-        }
+        while(first < nums1.size() and (val1==-1 or val2==-1)){
 
-        int n = res.size();
-        int mid = n/2;
-        if(n%2==0) return (res[mid] + res[mid-1] )/2.0;
-        return res[mid];
+            if(idx==idx1) val1=nums1[first];
+            if(idx==idx2) val2=nums1[first];
+            first++;
+            idx++;
+        }
+        while(second < nums2.size() and  (val1==-1 or val2==-1)){
+            if(idx==idx1) val1=nums2[second];
+            if(idx==idx2) val2=nums2[second];
+
+            second++;
+            idx++;
+        }
+        cout<<val1<<" "<<val2;
+        if(n%2==0) return (val1+val2)/2.0;
+        return val1;
 
 
         
