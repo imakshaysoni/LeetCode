63e860980fd15e63d7fcc1e675cd048c849d0a0e
Akshay Soni <akshaysoni460@gmail.com>
2025-06-16 22:11:25 +0530

    Time: 0 ms (100%), Space: 95.1 MB (61.13%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,6 +2,50 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
+        //
+
+        return binarySearch(nums1, nums2);
+
+
+    }
+
+
+    double binarySearch(vector<int> &nums1, vector<int> &nums2){
+
+
+        // for even
+        if(nums1.size() > nums2.size()) return binarySearch(nums2, nums1);
+
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int n = n1+n2;
+        int maxPos = n1;
+        int minPos = 0;
+        int left = (n1+n2+1)/2;
+        while(minPos <= maxPos){
+            int mid = (minPos + maxPos )/2;
+            int mid2 = left - mid;
+            int l1 = INT_MIN, r1=INT_MAX;
+            int l2 = INT_MIN, r2=INT_MAX;
+            if(mid-1 >=0 ) l1 = nums1[mid-1];
+            if(mid2-1 >= 0) l2 = nums2[mid2-1];
+
+            if(mid < n1 ) r1 = nums1[mid];
+            if(mid2 < n2 ) r2 = nums2[mid2];
+            if(l1 <= r2 and l2 <= r1){
+                if(n%2==0)  return (max(l1, l2) + min(r1, r2) ) / 2.0;
+                return max(l1, l2);
+            }
+            else{
+                if(l1 > r2) maxPos = mid-1;
+                else minPos = mid+1;
+            }
+        }
+
+    return 0;
+    }
+
+    double intitive_sol(vector<int> &nums1, vector<int> &nums2){
         int first=0;
         int second = 0;
 
@@ -43,10 +87,6 @@ public:
         cout<<val1<<" "<<val2;
         if(n%2==0) return (val1+val2)/2.0;
         return val1;
-
-
-        
-
         
     }
 };
