bfe2a6429b16805326c3c1038ad62f564f46ba96
Akshay Soni <akshaysoni460@gmail.com>
2025-07-02 01:41:51 +0530

    Time: 0 ms (100%), Space: 18.4 MB (12.69%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,21 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> res;
-        if (root == NULL) return res;
-        dfs(root, to_string(root->val), res);
-        return res;
+        vector<string> ans;
+        string s;
+        path(root, s, ans);
+        return ans;
     }
-    
-    void dfs(TreeNode* root, string path, vector<string>& res) {
-        if (root->left == NULL && root->right == NULL) {
-            res.push_back(path);
+
+    void path(TreeNode* root, string p, vector<string> &ans){
+        if(root==NULL) return;
+        if(root->left==root->right){
+            p = p+to_string(root->val);
+            ans.push_back(p);
+            return;
         }
-        
-        if (root->left != NULL)
-            dfs(root->left, path + "->" + to_string(root->left->val), res);
-        
-        if (root->right != NULL)
-            dfs(root->right, path + "->" + to_string(root->right->val), res);
+
+        p = p+to_string(root->val) + "->";
+        path(root->left, p, ans);
+        path(root->right, p, ans);
+        return;
+
     }
+
+    
+
 };
