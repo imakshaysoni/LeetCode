7bd2cd42360dd131a3258a226e4cba7ec103fc74
Akshay Soni <akshaysoni460@gmail.com>
2025-07-10 10:55:39 +0530

    Time: 5 ms (45.66%), Space: 27.2 MB (73.8%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,81 +1,40 @@
 /**
-* Definition for a binary tree node.
-* struct TreeNode {
-* int val;
-* TreeNode *left;
-* TreeNode *right;
-* TreeNode() : val(0), left(nullptr), right(nullptr) {}
-* TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-* TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-* };
-*/
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-unordered_map<int, int> inOrderIdx;
-int inOrderLen = inorder.size();
-for(int i=0;i<inOrderLen;i++){
-inOrderIdx[inorder[i]]=i;
-}
-int preOrderLen = preorder.size();
-TreeNode * root = buildUniqueBT(inorder, 0, inorder.size()-1, preorder,0, preorder.size()-1, inOrderIdx);
-return root;
-}
-TreeNode * buildUniqueBT(vector<int> &in, int inStart, int inEnd, vector<int> &pre, int preStart, int preEnd, unordered_map<int, int> &iMap)
-{
-if(preStart>preEnd || inStart>inEnd) return nullptr;
-int rootVal = pre[preStart];
-int rootIdx = iMap[rootVal];
-int numsLeft = rootIdx - inStart;
-TreeNode * root = new TreeNode(rootVal);
-root->left = buildUniqueBT(in,inStart, rootIdx-1, pre, preStart+1, preStart+numsLeft , iMap);
-root->right = buildUniqueBT(in, rootIdx+1, inEnd, pre, preStart+numsLeft+1, preEnd, iMap);
-return root;
-}
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+
+        int start = 0;
+        int end = inorder.size();
+        int idx = 0;
+        return solve(preorder, inorder, idx, start, end-1);
+        
+    }
+
+    TreeNode* solve(vector<int> &preorder, vector<int> &inorder, int &idx, int start, int end){
+
+        if(start > end or idx>=preorder.size()) return nullptr;
+
+        int rootVal = preorder[idx];
+        idx++;
+        int i=start;
+        while(i<=end){
+            if(inorder[i]==rootVal) break;
+            i++;
+        } 
+        TreeNode* root = new TreeNode(rootVal);
+        root->left = solve(preorder, inorder, idx, start, i-1);
+        root->right = solve(preorder, inorder, idx, i+1, end);
+
+        return root;
+    }
 };
-
-// /**
-//  * Definition for a binary tree node.
-//  * struct TreeNode {
-//  *     int val;
-//  *     TreeNode *left;
-//  *     TreeNode *right;
-//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//  * };
-//  */
-// class Solution {
-// public:
-//    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-//     if (preorder.empty() || inorder.empty() || preorder.size() != inorder.size()) {
-//         return nullptr;
-//     }
-    
-//     unordered_map<int, int> inOrderIdx;
-//     int inOrderLen = inorder.size();
-//     for (int i = 0; i < inOrderLen; i++) {
-//         inOrderIdx[inorder[i]] = i;
-//     }
-//     int preOrderLen = preorder.size();
-//     TreeNode* root = buildUniqueBT(inorder, 0, inOrderLen - 1, preorder, 0, preOrderLen - 1, inOrderIdx);
-    
-//     return root;
-// }
-
-// TreeNode* buildUniqueBT(vector<int>& in, int inStart, int inEnd, vector<int>& pre, int preStart, int preEnd, unordered_map<int, int>& iMap) {
-//     if (preStart > preEnd || inStart > inEnd) return nullptr;
-
-//     int rootVal = pre[preStart];
-//     int rootIdx = iMap[rootVal];
-
-//     int numsLeft = rootIdx - inStart;
-
-//     TreeNode* root = new TreeNode(rootVal);
-//     root->left = buildUniqueBT(in, inStart, rootIdx - 1, pre, preStart + 1, preStart + numsLeft, iMap);
-//     root->right = buildUniqueBT(in, rootIdx + 1, inEnd, pre, preStart + numsLeft + 1, preEnd, iMap);
-
-//     return root;
-// }
-
-// };
