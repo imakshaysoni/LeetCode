a929e6a37dea03d6ade800c64be1a06b67093887
Akshay Soni <akshaysoni460@gmail.com>
2025-07-19 10:21:01 +0530

    Time: 0 ms (100%), Space: 62.5 MB (5.16%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -9,42 +9,54 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+class BSTIterator {
 public:
-        TreeNode* first=nullptr;
-        TreeNode* mid=nullptr;
-        TreeNode* last=nullptr;
-        TreeNode* prev = nullptr;
-    void recoverTree(TreeNode* root) {
-        inorder(root);
-        if(first&&last) swap(first->val, last->val);
-        else swap(first->val, mid->val);
-        
+    stack<TreeNode*> st;
+    BSTIterator(TreeNode* root) {
+        pushAll(root);
+    }
+    
+    TreeNode* next() {
+
+        TreeNode* node = st.top();
+        st.pop();
+
+        if(node->right){
+            pushAll(node->right);
+        }
+
+        return node;
         
     }
     
-    void inorder(TreeNode* root){
-        
-        if(!root) return;
-        
-        inorder(root->left);
-        
-        if(prev and prev->val > root->val)
-        {
-            if(first==nullptr){
-                first=prev;
-                mid = root;
+    bool hasNext() {
+        return !st.empty();
+    }
+
+    void pushAll(TreeNode* root){
+        while(root) {
+            st.push(root);
+            root = root->left;
+        }
+    }
+};
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        BSTIterator *bst = new BSTIterator(root);
+        TreeNode* prev = nullptr;
+        TreeNode* first=nullptr;
+        TreeNode* second= nullptr;
+        while(bst->hasNext()){
+            TreeNode* node = bst->next();
+            if(prev!=nullptr and node->val < prev->val){
+                if(first==nullptr) first = prev;
+                second = node;
             }
-            else{
-                last = root;
-                return;
-            }
-        } 
-        prev = root;
-        inorder(root->right);
-        
-        
-        
-        
+            prev = node;
+        }
+        if(first and second) swap(first->val, second->val);
+        return;
     }
 };
