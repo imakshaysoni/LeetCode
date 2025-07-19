3f69b7237edd083af49b3856a5c8ed5334c3eed1
Akshay Soni <akshaysoni460@gmail.com>
2025-07-19 19:40:03 +0530

    Time: 4 ms (19.69%), Space: 28.3 MB (6.79%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
-        // Brute force
+
+        unordered_map<int,int> mapper;
         int n = nums.size();
         for(int i=0;i<n;i++){
-            int number = nums[i];
-            int count = 1;
-
-            for(int j=i+1;j<n;j++){
-                if(nums[j]==number) count++;
+            if(mapper.find(nums[i])!=mapper.end()){
+                mapper[nums[i]]++;
             }
+            else{
+                mapper[nums[i]] = 1;
+            }
+        }
 
-            if(count > n/2) return number;
-
+        for(auto it: mapper){
+            int number = it.first;
+            int count = it.second;
+            if(count>n/2) return number;
         }
         return -1;
+        // Brute force
+        // int n = nums.size();
+        // for(int i=0;i<n;i++){
+        //     int number = nums[i];
+        //     int count = 1;
+
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[j]==number) count++;
+        //     }
+
+        //     if(count > n/2) return number;
+
+        // }
+        // return -1;
+        // TC O(n*n)
     }
 };
