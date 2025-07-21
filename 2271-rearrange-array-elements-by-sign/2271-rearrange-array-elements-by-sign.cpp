a7c6eda9c6a944e4335e57d93a0e28babbbb84af
Akshay Soni <akshaysoni460@gmail.com>
2025-07-21 10:11:03 +0530

    Time: 15 ms (14.07%), Space: 139.1 MB (20.54%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,51 +1,29 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        
         int n = nums.size();
-        vector<int> ans(n, 0);
-        int posIdx = 0;
-        int negIdx = 1;
+        vector<int> pos;
+        vector<int> neg;
 
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>0) {
-                ans[posIdx] = nums[i];
-                posIdx+=2;
-            }
-            else {
-                ans[negIdx] = nums[i];
-                negIdx+=2;
-            }
-        
+        for(int i=0;i<n;i++){
+            if(nums[i]>=0) pos.push_back(nums[i]);
+            else neg.push_back(nums[i]);
         }
+
+        vector<int> ans(n, 0);
+        int idx = 0;
+        int posIdx = 0;
+        int negIdx = 0;
+        while(idx<n-1){
+            ans[idx] = pos[posIdx];
+            posIdx++;
+            ans[idx+1] = neg[negIdx];
+            negIdx++;
+
+            idx += 2;
+        }
+
         return ans;
-        
-        // vector<int> pos;
-        // vector<int> neg;
-        
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i] >= 0) pos.push_back(nums[i]);
-        //     else neg.push_back(nums[i]);
-        // }
-
-        // int n = nums.size();
-        // int posIdx=0;
-        // int negIdx=0;
-        // int idx=0;
-        // while(idx<n-1 and posIdx < pos.size() and negIdx < neg.size()){
-        //     nums[idx] = pos[posIdx];
-        //     nums[idx+1] = neg[negIdx];
-        //     idx+=2;
-        //     posIdx++;
-        //     negIdx++;
-        // }
-        // while(posIdx<pos.size()){
-        //     nums[idx] = pos[posIdx];
-        // }
-
-        // while(negIdx<neg.size()){
-        //     nums[idx] = pos[negIdx];
-        // }
-        // return nums;
-
     }
 };
