37e1d3c22d41b7bfd019a284da0c13fe356a33ac
Akshay Soni <akshaysoni460@gmail.com>
2025-07-21 10:13:25 +0530

    Time: 0 ms (100%), Space: 128.4 MB (57.59%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,29 +1,51 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        
-        int n = nums.size();
-        vector<int> pos;
-        vector<int> neg;
 
-        for(int i=0;i<n;i++){
-            if(nums[i]>=0) pos.push_back(nums[i]);
-            else neg.push_back(nums[i]);
-        }
 
-        vector<int> ans(n, 0);
-        int idx = 0;
+        // Optimal
         int posIdx = 0;
-        int negIdx = 0;
-        while(idx<n-1){
-            ans[idx] = pos[posIdx];
-            posIdx++;
-            ans[idx+1] = neg[negIdx];
-            negIdx++;
+        int negIdx=1;
 
-            idx += 2;
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        for(int i=0;i<n;i++){
+            if(nums[i]>=0) {
+                ans[posIdx] = nums[i];
+                posIdx+=2;
+            }
+            else{
+                ans[negIdx] = nums[i];
+                negIdx +=2;
+            }
         }
-
         return ans;
+
+        
+        // int n = nums.size();
+        // vector<int> pos;
+        // vector<int> neg;
+
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>=0) pos.push_back(nums[i]);
+        //     else neg.push_back(nums[i]);
+        // }
+
+        // vector<int> ans(n, 0);
+        // int idx = 0;
+        // int posIdx = 0;
+        // int negIdx = 0;
+        // while(idx<n-1){
+        //     ans[idx] = pos[posIdx];
+        //     posIdx++;
+        //     ans[idx+1] = neg[negIdx];
+        //     negIdx++;
+
+        //     idx += 2;
+        // }
+
+        // return ans;
+        // TC: O(N) + o(N)
+        // SC: O(N/2) + O(N/2)
     }
 };
