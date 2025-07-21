6baeebc681468c702701994a1cd4a16d8db4eec2
Akshay Soni <akshaysoni460@gmail.com>
2025-07-21 09:19:53 +0530

    Time: 9 ms (40.21%), Space: 128.4 MB (57.59%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,34 +1,51 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        
-        vector<int> pos;
-        vector<int> neg;
-        
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] >= 0) pos.push_back(nums[i]);
-            else neg.push_back(nums[i]);
-        }
-
         int n = nums.size();
-        int posIdx=0;
-        int negIdx=0;
-        int idx=0;
-        while(idx<n-1 and posIdx < pos.size() and negIdx < neg.size()){
-            nums[idx] = pos[posIdx];
-            nums[idx+1] = neg[negIdx];
-            idx+=2;
-            posIdx++;
-            negIdx++;
-        }
-        while(posIdx<pos.size()){
-            nums[idx] = pos[posIdx];
-        }
+        vector<int> ans(n, 0);
+        int posIdx = 0;
+        int negIdx = 1;
 
-        while(negIdx<neg.size()){
-            nums[idx] = pos[negIdx];
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0) {
+                ans[posIdx] = nums[i];
+                posIdx+=2;
+            }
+            else {
+                ans[negIdx] = nums[i];
+                negIdx+=2;
+            }
+        
         }
-        return nums;
+        return ans;
+        
+        // vector<int> pos;
+        // vector<int> neg;
+        
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i] >= 0) pos.push_back(nums[i]);
+        //     else neg.push_back(nums[i]);
+        // }
+
+        // int n = nums.size();
+        // int posIdx=0;
+        // int negIdx=0;
+        // int idx=0;
+        // while(idx<n-1 and posIdx < pos.size() and negIdx < neg.size()){
+        //     nums[idx] = pos[posIdx];
+        //     nums[idx+1] = neg[negIdx];
+        //     idx+=2;
+        //     posIdx++;
+        //     negIdx++;
+        // }
+        // while(posIdx<pos.size()){
+        //     nums[idx] = pos[posIdx];
+        // }
+
+        // while(negIdx<neg.size()){
+        //     nums[idx] = pos[negIdx];
+        // }
+        // return nums;
 
     }
 };
