e2c6efe0529b53ba0df45f61944e086e635c0c9d
Akshay Soni <akshaysoni460@gmail.com>
2025-07-22 09:21:54 +0530

    Time: 0 ms (100%), Space: 17.6 MB (74.84%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
+        
+        // BOttmon Up Approch
         int n = cost.size();
-        vector<int> dp(cost.size(), 0);
+        vector<int> dp(n, 0);
 
         dp[0] = cost[0];
         dp[1] = cost[1];
-        for(int i=2;i<cost.size();i++){
-            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        for (int i=2;i<n;i++){
+            dp[i] = cost[i] + min(dp[i-1] , dp[i-2]);
         }
+
         return min(dp[n-1], dp[n-2]);
+        
+
     }
 };
