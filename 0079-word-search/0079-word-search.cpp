34e441ebde2af5c63e5e3fd02ae50e29082dd052
Akshay Soni <akshaysoni460@gmail.com>
2025-10-01 09:44:45 +0530

    Time: 185 ms (91.77%), Space: 10.4 MB (85.59%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -4,58 +4,39 @@ public:
         
         int rows = board.size();
         int cols = board[0].size();
-//         If word size greater then total words present in board.
-        if(word.size()>rows*cols) return false;
-        
-//         check the frequency of each words.
-        unordered_map<char, int> m;
-        for(auto &v: board){
-            for(auto &ch: v){
-                m[ch]++;
-            }
-        }
-        
-        for(auto ch: word){
-            if(m[ch]==0) return false;
-            m[ch]--;
-        }
-        
-        int left_pref = word.find_first_not_of(word[0]);
-        int right_pref = word.size() - word.find_last_not_of(word.back());
-        if(left_pref > right_pref) {
-            reverse(word.begin(), word.end());
-        }
-        
         for(int row=0;row<rows;row++){
             for(int col=0;col<cols;col++){
                 if(board[row][col]==word[0]){
-                    if(solve(board, row, col, word, 0)){
-                        return true;
-                    }
+                    bool ans = solve(row, col, rows, cols, word, 0, board);
+                    if(ans==true) return true;
                 }
             }
         }
         return false;
-        
     }
-    
-    bool solve(vector<vector<char>>&b, int row, int col, string word, int w_idx){
-        if(w_idx>=word.size()) return true;
-        if(row>=b.size() || row<0 || col>=b[0].size() || col<0 || b[row][col]=='#') return false;
-        if(word[w_idx]!=b[row][col]) return false;
+
+    bool solve(int row, int col, int rows, int cols, string &word, int idx, vector<vector<char>>&board){
+
+        if(row>=rows or row<0
+            or col>=cols or col<0
+            or board[row][col]!=word[idx]) return false;
         
-        b[row][col] = '#';
-        bool exist = false;    
-    
+        if(idx==word.size()-1) return true;
+        board[row][col]='#';
+
+
         for(int dx=-1;dx<=1;dx++){
             for(int dy=-1;dy<=1;dy++){
-                if(abs(dx)==abs(dy)) continue; //Ignore diagonals
-                exist = exist || solve(b, row+dx, col+dy, word, w_idx+1);
+                if(abs(dx)==abs(dy)) continue;
+                bool ans = solve(row+dx, col+dy, rows, cols, word, idx+1, board);
+                if(ans) return true;
             }
         }
-        b[row][col]=word[w_idx];
-        return exist;
-        
-        
+        board[row][col] = word[idx];
+
+        return false;
+
+
+
     }
 };
