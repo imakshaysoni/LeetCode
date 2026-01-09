9747301cd66e50f3db458e548d645c837ed4e256
Akshay Soni <akshaysoni460@gmail.com>
2026-01-09 19:34:37 +0530

    Time: 7 ms (65.41%), Space: 37.2 MB (48.62%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,47 +1,54 @@
-class Solution {
-public:
-    long long mod = 1e9+7;
-    int countPaths(int V, vector<vector<int>>& edges) {
-                vector<vector<pair<int,int>>> adjList(V);
-        
-        for(auto iter: edges){
-            int u = iter[0];
-            int v = iter[1];
-            int time = iter[2];
-            
-            adjList[u].push_back({v, time});
-            adjList[v].push_back({u, time});
-        }
-        
-        priority_queue<pair<int,int>,
-        vector<pair<int,int>>,
-        greater<pair<int,int>>> pq;
-        
-        pq.push({0, 0});
-        vector<long long> dist(V, 1e9);
-        dist[0]=0;
-        vector<long long> paths(V, 0);
-        paths[0]=1;
-        
-        while(!pq.empty()){
-            long long time = pq.top().first;
-            long long u = pq.top().second;
-            pq.pop();
-            
-            for(auto it: adjList[u]){
-                long long v = it.first;
-                long long t = it.second;
-                
-                if(dist[v] > time + t){
-                    dist[v] = dist[u] + t;
-                    paths[v] = paths[u];
-                    pq.push({dist[v], v});
-                }
-                else if(dist[v]==time+t){
-                    paths[v] = (paths[v]+paths[u])%mod;
+class Solution
+{
+    public:
+        int countPaths(int n, vector<vector < int>> &roads)
+        {
+            vector<pair<int, int>> adj[n];
+
+            for (auto it: roads)
+            {
+                adj[it[0]].push_back({ it[1],
+                    it[2] });
+                adj[it[1]].push_back({ it[0],
+                    it[2] });
+            }
+
+            vector < long long > dist(n, LONG_MAX);
+            vector < long long > ways(n, 0);
+            priority_queue<pair < long long, long long >, vector< pair < long long, long long >>, greater<pair < long long, long long>>> q;
+
+            dist[0] = 0;
+            ways[0] = 1;
+            long long mod = 1e9 + 7;
+            q.push({ 0,
+                0 });
+
+            while (!q.empty())
+            {
+                auto it = q.top();
+                q.pop();
+                long long time = it.first;
+                long long node = it.second;
+
+                for (auto it: adj[node])
+                {
+                    long long neighbor_node = it.first;
+                    long long neighbor_time = it.second;
+
+                    if (time + neighbor_time < dist[neighbor_node])
+                    {
+                        dist[neighbor_node] = time + neighbor_time;
+                        ways[neighbor_node] = ways[node];
+                        q.push({ dist[neighbor_node],
+                            neighbor_node });
+                    }
+                    else if (time + neighbor_time == dist[neighbor_node])
+                    {
+                        ways[neighbor_node] = (ways[neighbor_node] + ways[node]) % mod;
+                    }
                 }
             }
+
+            return ways[n - 1] % mod;
         }
-        return paths[V-1]%mod;
-    }
 };
