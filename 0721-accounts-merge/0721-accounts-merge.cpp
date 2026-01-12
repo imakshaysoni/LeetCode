086b02f80ad5e3163351960d7db0f0d349d89d3a
Akshay Soni <akshaysoni460@gmail.com>
2026-01-12 22:26:53 +0530

    Time: 27 ms (81.21%), Space: 40.3 MB (59.88%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,105 +1,64 @@
-class DisjointSet
-{
-private:
-	vector<int> parent, rank, size;
-
-public:
-	// Constructor to initialize the disjoint set with size n
-	DisjointSet(int n){
-		rank.resize(n+1, 0);
-		parent.resize(n+1);
-		size.resize(n+1, 1);
-		for(int i=0;i<=n;i++) parent[i]=i;
-	}
-
-	// Find the representative (root) of the set to which node belongs (with path compression)
-	int findUp(int node){
-		if(node == parent[node]) return node;
-		
-		// Path compression by updating the parent of the node
-		return parent[node] = findUp(parent[node]);
-	}
-
-	// Union two sets based on rank
-	int unionByRank(int u, int v){
-		if(parent[u] == parent[v]) return 0;
-
-		int usp = findUp(u); // Find representative of u's set
-		int vsp = findUp(v); // Find representative of v's set
-
-		if(rank[usp] > rank[vsp]){
-			parent[vsp] = usp; // Set v's representative's parent as u's representative
-		}
-		else if(rank[vsp] > rank[usp]){
-			parent[usp] = vsp; // Set u's representative's parent as v's representative
-		}
-		else{
-			parent[usp] = vsp; // Set u's representative's parent as v's representative
-			rank[vsp]++; // Increment the rank of v's representative
-		}
-		return 1;
-	}
-
-	// Union two sets based on size
-	void unionBySize(int u, int v){
-		if(parent[u] == parent[v]) return;
-
-		int usp = findUp(u); // Find representative of u's set
-		int vsp = findUp(v); // Find representative of v's set
-
-		if(size[usp] > size[vsp]){
-			parent[vsp] = usp; // Set v's representative's parent as u's representative
-			size[usp] += size[vsp]; // Update the size of u's set
-		}
-		else{
-			parent[usp] = vsp; // Set u's representative's parent as v's representative
-			size[vsp] += size[usp]; // Update the size of v's set
-		}
-	}
-};
-
-
 class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+int dsu_find(int node, vector<int> & parent){
+        if(node==parent[node]) return node;
+        return parent[node] = dsu_find(parent[node], parent);
+    }
+    
+    void dsu_union(int u, int v, vector<int> &parent){
+        int parent_u = dsu_find(u, parent);
+        int parent_v = dsu_find(v, parent);
         
-        unordered_map<string, int> map;
-        DisjointSet ds(accounts.size());
-        for(int i=0;i<accounts.size();i++){
-            for(int j=1;j<accounts[i].size();j++){
-                
-                string email = accounts[i][j];
-                if(map.find(email)!=map.end()){
-                    int firstNode = i;
-                    int secondNode = map[email];
-                    ds.unionByRank(firstNode, secondNode);
+        if(parent_u==parent_v) return;
+        
+        parent[parent_u] = parent_v;
+        return;
+    }
+    vector<vector<string>> accountsMerge(vector<vector<string>>& details) {
+        int n = details.size();
+        vector<int> parent(n);
+        for(int i=0;i<n;i++) parent[i]=i;
+        unordered_map<string,int> mapp;
+        
+        for(int i=0;i<details.size();i++){
+            for(int j=1;j<details[i].size();j++){
+                string email = details[i][j];
+                if(mapp.count(email)){
+                    int firstNode=i;
+                    int secondNode= mapp[email];
+                    dsu_union(firstNode, secondNode, parent);
                 }
                 else{
-                    map[email] = i; 
-                }                
-                
+                    mapp[email] = i;
+                }
             }
+            
+        }
+        vector<vector<string>> mergedNodes(n);
+        
+        for(auto iter: mapp){
+            string email = iter.first;
+            int node = iter.second;
+            int parentNode = dsu_find(node, parent);
+            mergedNodes[parentNode].push_back(email);
         }
         
-        vector<string> mergedMails[accounts.size()];
-        for(auto it: map){
-            string mail  = it.first;
-            int node  = it.second;
-            int pNode = ds.findUp(node);
-            mergedMails[pNode].push_back(mail);
-        }    
-        
-        vector<vector<string>> ans;
-        for(int i=0;i<accounts.size();i++){
-            if(mergedMails[i].size()==0) continue;
-            sort(mergedMails[i].begin(), mergedMails[i].end());
+        vector<vector<string>> result;
+        for(int i=0;i<mergedNodes.size();i++){
+            if(mergedNodes[i].size() == 0) continue;
+            string name = details[i][0]; 
+            
             vector<string> temp;
-            temp.push_back(accounts[i][0]);
-            for(auto it: mergedMails[i]) temp.push_back(it);
-            ans.push_back(temp);
+            temp.push_back(name);
+            
+            sort(mergedNodes[i].begin(), mergedNodes[i].end());
+            
+            for(int j=0;j<mergedNodes[i].size();j++){
+                temp.push_back(mergedNodes[i][j]);
+            }
+            result.push_back(temp);
         }
         
-        return ans;
-        
+        return result;
     }
 };
