7d967954469084e6dab59d985e955bec3526e156
Akshay Soni <akshaysoni460@gmail.com>
2026-01-12 12:54:42 +0530

    Time: 23 ms (44.17%), Space: 46.6 MB (61.38%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,84 +1,54 @@
-class DisjointSet
-{
-private:
-	vector<int> parent, rank, size;
-
-public:
-	// Constructor to initialize the disjoint set with size n
-	DisjointSet(int n){
-		rank.resize(n+1, 0);
-		parent.resize(n+1);
-		size.resize(n+1, 1);
-		for(int i=0;i<=n;i++) parent[i]=i;
-	}
-
-	// Find the representative (root) of the set to which node belongs (with path compression)
-	int findUp(int node){
-		if(node == parent[node]) return node;
-		
-		// Path compression by updating the parent of the node
-		return parent[node] = findUp(parent[node]);
-	}
-
-	// Union two sets based on rank
-	int unionByRank(int u, int v){
-		if(parent[u] == parent[v]) return 0;
-
-		int usp = findUp(u); // Find representative of u's set
-		int vsp = findUp(v); // Find representative of v's set
-
-		if(rank[usp] > rank[vsp]){
-			parent[vsp] = usp; // Set v's representative's parent as u's representative
-		}
-		else if(rank[vsp] > rank[usp]){
-			parent[usp] = vsp; // Set u's representative's parent as v's representative
-		}
-		else{
-			parent[usp] = vsp; // Set u's representative's parent as v's representative
-			rank[vsp]++; // Increment the rank of v's representative
-		}
-		return 1;
-	}
-
-	// Union two sets based on size
-	void unionBySize(int u, int v){
-		if(parent[u] == parent[v]) return;
-
-		int usp = findUp(u); // Find representative of u's set
-		int vsp = findUp(v); // Find representative of v's set
-
-		if(size[usp] > size[vsp]){
-			parent[vsp] = usp; // Set v's representative's parent as u's representative
-			size[usp] += size[vsp]; // Update the size of u's set
-		}
-		else{
-			parent[usp] = vsp; // Set u's representative's parent as v's representative
-			size[vsp] = size[vsp] + size[usp]; // Update the size of v's set
-		}
-	}
-};
 
 class Solution {
 public:
+
+    int dsu_find(int node, vector<int> &parent){
+        if(node==parent[node]) return node;
+        return parent[node] = dsu_find(parent[node], parent); // Path Compression
+    }
+
+    void dsu_union(int a, int b, vector<int> & parent, vector<int> &rank){
+        int a_parent = dsu_find(a, parent);
+        int b_parent = dsu_find(b, parent);
+        if(a_parent==b_parent) return;
+
+        if(rank[a_parent] > rank[b_parent]){
+            parent[b_parent] = a_parent;
+        }
+        else if(rank[b_parent] > rank[a_parent]){
+            parent[a_parent] = b_parent;
+        }
+        else{
+            parent[b_parent] = a_parent;
+            rank[a_parent] += 1;
+        }
+        return;
+    }
+
+
+
     int makeConnected(int n, vector<vector<int>>& connections) {
-        // code here
-        int initial_edges = connections.size();
-        if(n > initial_edges + 1) return -1;
-        DisjointSet ds(n-1);
-        int cntExtras=0;
-        for(auto it: connections){
-            int u = it[0];
-            int v = it[1];
-            if(ds.findUp(u)== ds.findUp(v)) cntExtras++;
-            else ds.unionBySize(u,v);
-            initial_edges++;
-        }
+
+        if(connections.size()<n-1) return -1;
         
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(ds.findUp(i) == i) count++;
+        vector<int> parent(n);
+        for(int i=0;i<n;i++) parent[i] = i;
+
+        vector<int> rank(n, 0);
+
+        for(auto iter: connections){
+            dsu_union(iter[0], iter[1], parent, rank);
         }
-        if(cntExtras>=count-1) return count-1;
-        return -1;
+
+        int count =0;
+        for(int i=0;i<n-1;i++){
+            if(dsu_find(i, parent) != dsu_find(i+1, parent)){
+                dsu_union(i, i+1, parent, rank);
+                count++;
+            }
+        }
+
+        return count;
+
     }
 };
