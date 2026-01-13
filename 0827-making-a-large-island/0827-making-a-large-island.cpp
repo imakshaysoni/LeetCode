1a9390569b4a24c63adb3736aa2f453d5904c8c9
Akshay Soni <akshaysoni460@gmail.com>
2026-01-13 13:08:49 +0530

    Time: 176 ms (83.13%), Space: 115.5 MB (70.79%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,162 +1,150 @@
-class DisjointSet
-{
-
+class DisJointSet{
     public:
-
+        
         vector<int> parent, rank, size;
-   	// Constructor to initialize the disjoint set with size n
-    DisjointSet(int n)
-    {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1, 1);
-        for (int i = 0; i <= n; i++) parent[i] = i;
+        DisJointSet(int n){
+            parent.resize(n);
+            rank.resize(n, 0);
+            size.resize(n, 1);
+        }
+
+        int DisJointFind(int node){
+            if(node==parent[node]) return node;
+
+            return parent[node] = DisJointFind(parent[node]);
+        }
+
+        void DisJointUnion(int u, int v){
+            
+            int parent_u = DisJointFind(u);
+            int parent_v = DisJointFind(v);
+
+            if(parent_u == parent_v) return;
+
+            parent[parent_v] = parent_u;
+            return;
+
+        }
+
+
+        void DisJointUnionByRank(int u, int v){
+            int parent_u = DisJointFind(u);
+            int parent_v = DisJointFind(v);
+
+            if(parent_u == parent_v) return;
+
+            if(rank[parent_u] > rank[parent_v]){
+                parent[parent_v] = parent_u;
+            }
+            else if( rank[parent_v] > rank[parent_u]){
+                parent[parent_u] = parent_v;
+            }
+            else{
+                parent[parent_v] = parent_u;
+                rank[parent_u] += 1;
+            }
+            return;
+        }
+
+        void DisJointUnionBySize(int u, int v){
+            int parent_u = DisJointFind(u);
+            int parent_v = DisJointFind(v);
+
+            if(parent_u == parent_v) return;
+
+            if(size[parent_u] > size[parent_v]){
+                parent[parent_v] = parent_u;
+                size[parent_u] += size[parent_v];
+            }
+            else if( size[parent_v] > size[parent_u]){
+                parent[parent_u] = parent_v;
+                size[parent_v] += size[parent_u];
+            }
+            else{
+                parent[parent_v] = parent_u;
+                size[parent_u] += size[parent_v];
+            }
+            return;
+        }
+    };
+
+class Solution {
+public:
+    bool isValid(int row, int col, vector<vector<int>> &grid){
+        if(row <0 || row>=grid.size() 
+    || col <0 || col >= grid[0].size() || grid[row][col]==0) return false;
+         return true;
     }
 
-   	// Find the representative (root) of the set to which node belongs (with path compression)
-    int findUp(int node)
-    {
-        if (node == parent[node]) return node;
+    void dsu_join(vector<vector<int>> &grid, int row, int col, DisJointSet &ds){
+        int firstNode = row * grid[0].size() + col;
+        for(int dx=-1;dx<=1;dx++){
+            for(int dy=-1;dy<=1;dy++){
+                if(abs(dx)==abs(dy)) continue;
 
-       	// Path compression by updating the parent of the node
-        return parent[node] = findUp(parent[node]);
+                int nrow = row + dx;
+                int ncol = col + dy;
+                if(isValid(nrow, ncol, grid)){
+                    int secondNode = nrow * grid[0].size() + ncol;
+                    ds.DisJointUnionBySize(firstNode, secondNode);
+                }
+            }
+        }
+
+
     }
+    int largestIsland(vector<vector<int>>& grid) {
 
-   	// Union two sets based on rank
-    int unionByRank(int u, int v)
-    {
-        if (parent[u] == parent[v]) return 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
 
-        int usp = findUp(u);	// Find representative of u's set
-        int vsp = findUp(v);	// Find representative of v's set
-
-        if (rank[usp] > rank[vsp])
-        {
-            parent[vsp] = usp;	// Set v's representative's parent as u's representative
+        DisJointSet ds(rows*cols);
+        for(int i=0;i<rows*cols;i++) ds.parent[i]=i;     
+        
+        for(int row=0;row<rows;row++){
+            for(int col=0;col<cols;col++){
+                if(grid[row][col]==1){
+                    dsu_join(grid, row, col, ds);
+                }
+            }
         }
-        else if (rank[vsp] > rank[usp])
-        {
-            parent[usp] = vsp;	// Set u's representative's parent as v's representative
-        }
-        else
-        {
-            parent[usp] = vsp;	// Set u's representative's parent as v's representative
-            rank[vsp]++;	// Increment the rank of v's representative
-        }
-        return 1;
-    }
-
-   	// Union two sets based on size
-    void unionBySize(int u, int v)
-    {
-        if (parent[u] == parent[v]) return;
-
-        int usp = findUp(u);	// Find representative of u's set
-        int vsp = findUp(v);	// Find representative of v's set
-
-        if (size[usp] > size[vsp])
-        {
-            parent[vsp] = usp;	// Set v's representative's parent as u's representative
-            size[usp] += size[vsp];	// Update the size of u's set
-        }
-        else
-        {
-            parent[usp] = vsp;	// Set u's representative's parent as v's representative
-            size[vsp] += size[usp];	// Update the size of v's set
-        }
-    }
-};
-
-class Solution
-{
-    private:
-        bool isValid(int row, int col, int m, int n)
-        {
-            return row >= 0 && col >= 0 && row < m && col < n;
-        }
-    public:
-        int largestIsland(vector<vector < int>> &grid)
-        {
-           	// code here
-            int n = grid.size();
-            int m = grid[0].size();
-            DisjointSet ds(n *m);
-            vector<int> dx = { -1, 0, 1, 0 };
-            vector<int> dy = { 0, 1, 0, -1};
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    int nodeNo = i *m + j;
-                    if (grid[i][j] == 1)
-                    {
-                        for (int index = 0; index < 4; index++)
-                        {
-                            int adjr = i + dx[index];
-                            int adjc = j + dy[index];
-
-                            if (isValid(adjr, adjc, n, m) and grid[adjr][adjc] == 1)
-                            {
-                                int adjNodeNo = adjr *m + adjc;
-                                if (ds.findUp(adjNodeNo) != ds.findUp(nodeNo))
-                                {
-                                    ds.unionBySize(nodeNo, adjNodeNo);
-                                }
+        
+        set<int> st;
+        int ans = -1;
+        for(int row=0;row<rows;row++){
+            for(int col=0;col<cols;col++){
+                if(grid[row][col]==0){
+                    for(int dx=-1;dx<=1;dx++){
+                        for(int dy=-1;dy<=1;dy++){
+                            if(abs(dx)==abs(dy)) continue;
+                            int nrow = row + dx;
+                            int ncol = col + dy;
+                            if(isValid(nrow, ncol, grid)){                                
+                                int node = nrow * cols + ncol;
+                                int parentNode = ds.DisJointFind(node);
+                                st.insert(parentNode);
                             }
                         }
                     }
-                }
-            }
 
-           	// Check is all node is already 1
-           	// int flag = false;
-           	// for(int i=0;i < n;i++){
-           	//     for(int j=0;j < m;j++){
-           	//         if(grid[i][j]==0) {
-           	//             flag = true;
-           	//             break;
-           	//         }
-           	//     }
-           	// }
-
-           	// if(flag==false) return n*m;
-
-           	//  Make Disjoint Set
-            int maxSize = -1;
-            set<int> s;
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-
-                    if (grid[i][j] == 0)
-                    {
-                        for (int index = 0; index < 4; index++)
-                        {
-                            int adjr = i + dx[index];
-                            int adjc = j + dy[index];
-                            if (isValid(adjr, adjc, n, m))
-                            {
-                                if (grid[adjr][adjc] == 1)
-                                {
-                                    int adjNodeNo = adjr *m + adjc;
-                                    int ulp = ds.findUp(adjNodeNo);
-                                    s.insert(ulp);
-                                }
-                            }
-                        }
-
-                        int localSize = 0;
-                        for (auto it: s)
-                        {
-                            localSize += ds.size[it];
-                        }
-                        maxSize = max(maxSize, localSize + 1);
-                        s.clear();
+                    int temp=1;
+                    for(auto it: st){
+                        temp += ds.size[it];
                     }
+                    ans = max(ans, temp);
+                    st.clear();
+
                 }
+                
             }
-            if (maxSize == -1) return n * m;
-            return maxSize;
         }
+
+        if(ans==-1) return rows*cols;
+
+        return ans;
+        
+
+
+        
+    }
 };
