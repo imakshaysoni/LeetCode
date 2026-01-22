8763be75fa790aaa0d1f1c0432ed9f37203f0a74
Akshay Soni <akshaysoni460@gmail.com>
2026-01-22 17:44:30 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.02%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,35 +2,36 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         
-        return solve(nums, target, 0, nums.size()-1);
+        return binarySearch(nums, 0, nums.size()-1, target);
     }
 
-    int solve(vector<int> &nums, int target, int left, int right){
+    int binarySearch(vector<int>&nums, int low, int high, int target){
 
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            if(nums[mid]==target) return mid;
-            if(nums[left]==target) return left;
-            if(nums[right]==target) return right;
+        if(low > high) return -1; // not found
+        
+        int mid = low +(high-low)/2;
 
-            if(nums[left] <= nums[mid]){
-                if(target >= nums[left] and target<nums[mid]){
-                    right = mid-1;
-                }
-                else left = mid+1;
+        if(nums[mid]==target) return mid;
+        // if(nums[low]==target) return low;
+        // if(nums[high]==target) return high;
+
+        if(nums[low] <= nums[mid]){
+            //left half is sorted
+            if(target >= nums[low] and target <= nums[mid]){
+                high = mid-1;
             }
             else{
-                if(target > nums[mid] and target <= nums[right]){
-                    left = mid+1;
-                }
-                else{
-                    right = mid-1;
-                }
+                low=mid+1;
             }
-
         }
-        return -1;
-
-
+        else{
+            if(target >= nums[mid] and target <= nums[high]){
+                low=mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return binarySearch(nums, low, high, target);
     }
 };
