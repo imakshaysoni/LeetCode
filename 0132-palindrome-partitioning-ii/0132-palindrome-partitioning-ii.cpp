1781285c8dcf39c07156c6227af175a98fe92a18
Akshay Soni <akshaysoni460@gmail.com>
2026-02-26 19:37:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,55 +1,38 @@
 class Solution {
 public:
     int minCut(string s) {
-        vector<vector<int>> memo(s.size(), vector<int> (s.size(), -1));
-        if (checkPalindrom(s, 0, s.size() - 1, memo))
-            return 0;
-        
-        // vector<int> dp(s.size(), -1);
-        // return solve(s, 0, dp) - 1;
 
-        vector<int> dp(s.size()+1, 0);
-        int n = s.size();
-        for(int idx=n-1;idx>=0;idx--){
-            string curr = "";
-            int mini = INT_MAX;
-            for (int index = idx; index < s.size(); index++) {
-                curr += s[index];
-                if (checkPalindrom(s, idx, index, memo))
-                    mini = min(mini, 1 + dp[index+1]);
-            }
-            dp[idx] = mini;
-        }
-        return dp[0]-1;
+
+        return solve(s, 0)-1;
         
+    }
+    int solve(string s, int i){
+
+        if(i>=s.size()) return 0;
+        string str = "";
+        int mini = 1e8;
+        for(int j=i;j<s.size();j++){
+            str+=s[j];
+            if(isPalindrome(str)){
+                int result = 1 + solve(s, j+1);
+
+                mini = min(mini, result);           
+            }    
+
+        }
+        return mini;
+
 
     }
-    bool checkPalindrom(string &s, int left, int right, vector<vector<int>> &memo) {
-        int ldx =left;
-        int rdx = right;
-        if(memo[left][right]!=-1) return memo[ldx][rdx];
-        while (left <= right) {
-            if (s[left] != s[right]) {
-                return memo[ldx][rdx] = false;
-            }
-            right--;
-            left++;
+
+    bool isPalindrome(string s){
+        int start = 0;
+        int end = s.size()-1;
+        while(start <= end){
+            if(s[start]!=s[end]) return false;
+            start++;
+            end--;
         }
-        return memo[ldx][rdx] = true;
+        return true;
     }
-    // int solve(string &s, int idx, vector<int> &dp) {
-
-    //     if (idx >= s.size())
-    //         return 0;
-    //     if(dp[idx]!=-1) return dp[idx];
-
-    //     string curr = "";
-    //     int mini = INT_MAX;
-    //     for (int index = idx; index < s.size(); index++) {
-    //         curr += s[index];
-    //         if (checkPalindrom(s, idx, index))
-    //             mini = min(mini, 1 + solve(s, index + 1, dp));
-    //     }
-    //     return dp[idx] = mini;
-    // }
 };
